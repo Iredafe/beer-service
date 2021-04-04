2920a788318052a6b02312678ac8720689c9be14
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-04-04 10:49:42 -0600

    add data model

--- src/main/java/dafe/springframework/beerservice/BeerServiceApplication.java
+++ src/main/java/dafe/springframework/beerservice/BeerServiceApplication.java
@@ -3,11 +3,15 @@ package dafe.springframework.beerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class BeerServiceApplication {
 
+    private static final Logger LOGGER = Logger.getLogger(BeerServiceApplication.class.getName());
     public static void main(String[] args) {
         SpringApplication.run(BeerServiceApplication.class, args);
-    }
 
+        LOGGER.info("HEY DAFE, THE SYSTEM LAUNCHED SUCCESSFULLY...");
+    }
 }
---
+++ src/main/java/dafe/springframework/beerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,7 @@
+package dafe.springframework.beerservice.web.model;
+
+public enum BeerStyleEnum {
+LAGER,STOUT, ALE, PILSNER,
+        GOSE, PORTER, WHEAT, IPA, PALE_ALE, SAISON
+}
+
