85e8103f76e59defedbe3484390a29db520bbc21
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-04-17 21:05:36 -0600

    another algo for summing unique numbers

---
+++ src/main/java/dafe/springframework/beerservice/Solution7.java
@@ -0,0 +1,37 @@
+package dafe.springframework.beerservice;
+
+public class Solution7 {
+    public static int sumOfUnique(int [] nums){
+        //problem statement:
+        //input is an array of numbers
+        //output is an integer sum of the unique elements, i.e that appear once in the array
+
+        //solution statement:
+        //loop through the array with 2 pointers: O(n^2) runtime
+        //do a lookup on the array to see if the value exists in the array
+        //if the value in a given index exists(is not unique), break out of the loop
+        //if the value in a given index does not exist, sum it up with others
+        //return the sum of unoique values
+
+        int sum=0;
+        for(int i=0; i<nums.length; i++){
+            boolean isUnique = true;
+            for(int j=0; j< nums.length; j++){
+                if(nums[i]==nums[j] && i!=j){
+                    isUnique=false;
+                    break;
+                }
+            }
+        if(isUnique){
+            sum+= nums[i];
+        }
+        }
+        return sum;
+    }
+
+    public static void main(String [] args){
+
+      int [] myNum = {2,1,3,4,2,6,8};
+        System.out.println("This is the sum of unique numbers : " + sumOfUnique(myNum));
+    }
+}
