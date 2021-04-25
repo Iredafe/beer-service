98c701de4d3b5d9fa7cd9d32e313e03cdb2da90a
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-04-25 16:39:42 -0600

    hashmap impl for algo

--- src/main/java/dafe/springframework/beerservice/NumberOfGoodPairs.java
+++ src/main/java/dafe/springframework/beerservice/NumberOfGoodPairs.java
@@ -1,5 +1,9 @@
 package dafe.springframework.beerservice;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class NumberOfGoodPairs {
     public static int numIdenticalPairs(int[] nums) {
         //problem statement
@@ -24,12 +28,28 @@ public class NumberOfGoodPairs {
         return counter;
     }
 
-    public static void main(String[] args) {
+    public static int numIdenticalPairsHash(int[] nums) {
+
+        int counter=0;
+        Map<Integer,Integer> myHashMap = new HashMap<>();
+        for(int num:nums){
+            Integer frequency = myHashMap.get(num);
+            if(frequency ==null){
+                myHashMap.put(num,1);
+            }else{
+                counter+=frequency;
+                myHashMap.put(num, frequency+1);
+            }
+        }
+    return counter;
+    }
+
+        public static void main(String[] args) {
         int testArray []= {9,2,4,2,5,7,5,3,4,1,0,3,2,5};
         int testArray2 [] = {1,2,3,1,1,3};
-        int result = numIdenticalPairs(testArray);
-        int result2=numIdenticalPairs(testArray2);
-        System.out.println("This is the number of good pairs : " + result);
-        System.out.println("This is the number of good pairs : " + result2);
-    }
+        System.out.println("This is the number of good pairs : " + numIdenticalPairs(testArray));
+        System.out.println("This is the number of good pairs : " + numIdenticalPairs(testArray2));
+        System.out.println("This is the number of good pair in hashmap " + numIdenticalPairsHash(testArray));
+        System.out.println("This is the number of good pair in hashmap " + numIdenticalPairsHash(testArray2));
+        }
 }
