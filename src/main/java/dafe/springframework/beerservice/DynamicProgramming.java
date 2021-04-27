beead6f8cf6c3805b0ba24861ff7a504c8921477
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-04-26 20:53:45 -0600

    climbing stairs algo

---
+++ src/main/java/dafe/springframework/beerservice/DynamicProgramming.java
@@ -0,0 +1,23 @@
+package dafe.springframework.beerservice;
+
+public class DynamicProgramming {
+
+    public static int climbStairs(int stepNumber){
+
+        if(stepNumber>=0 && stepNumber<=2) return stepNumber;
+
+       int cache[] = new int[stepNumber+1];
+       cache[1]=1;
+       cache[2]=2;
+
+       for (int i=3; i<=stepNumber; i++){
+           cache[i]= cache[i-1] + cache[i-2];
+       }
+       return cache[stepNumber];
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("The number of steps are : " + climbStairs(3));
+    }
+}
