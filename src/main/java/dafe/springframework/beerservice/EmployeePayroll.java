7f21fd308e88eece4dae1ea9f26acf03a674e16f
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-05-01 17:10:19 -0600

    begin work on new system

---
+++ src/main/java/dafe/springframework/beerservice/EmployeePayroll.java
@@ -0,0 +1,13 @@
+package dafe.springframework.beerservice;
+
+import java.util.Date;
+
+public class EmployeePayroll {
+    private String employeeName;
+    private String employeeAddress;
+    String title;
+    String department;
+    Date paymentDate;
+    String paymentCycle;
+    
+}
