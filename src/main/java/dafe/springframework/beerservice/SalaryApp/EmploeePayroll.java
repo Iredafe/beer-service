3d60f787ad2bfe2ba6b3fb56b97c8937d4ce8e69
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-05-03 16:35:12 -0600

    create an app with different pattern

---
+++ src/main/java/dafe/springframework/beerservice/SalaryApp/EmploeePayroll.java
@@ -0,0 +1,4 @@
+package dafe.springframework.beerservice.SalaryApp;
+
+public abstract class EmploeePayroll {
+}
---
+++ src/main/java/dafe/springframework/beerservice/SalaryApp/PartTimePayrollFactory.java
@@ -0,0 +1,4 @@
+package dafe.springframework.beerservice.SalaryApp;
+
+public class PartTimePayrollFactory extends EmployeePayrollFactory{
+}
