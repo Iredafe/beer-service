332ee31ad31a5768004579ae44ae44ad1c71b9c9
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-05-03 17:41:12 -0600

    finished app

--- src/main/java/dafe/springframework/beerservice/SalaryApp/ClientSalaryApp.java
+++ src/main/java/dafe/springframework/beerservice/SalaryApp/ClientSalaryApp.java
@@ -5,6 +5,29 @@ public class ClientSalaryApp {
 
     public static void main(String[] args) {
 
+
+        EmployeePayrollFactory fullTimefactory = new FullTimeEmployeeFactory();
+
+        EmployeePayrollFactory partTimeFactory=new PartTimePayrollFactory();
+
+        EmployeePayrollFactory contractFactory=new ContractEmployeeFactory();
+
+        EmployeePayroll payroll = fullTimefactory.createEmployeePayroll("PDF");
+        System.out.println("Creating fulltime payroll in PDF for " + payroll.employeeName);
+        payroll = fullTimefactory.createEmployeePayroll("JSON");
+        System.out.println("Creating full time payroll in JSON for " + payroll.employeeName);
+
+        payroll = partTimeFactory.createEmployeePayroll("PDF");
+        System.out.println("Creating Part time payroll in PDF for " + payroll.employeeName);
+        payroll = fullTimefactory.createEmployeePayroll("JSON");
+        System.out.println("Creating part time payroll in JSON for " + payroll.employeeName);
+
+        payroll = contractFactory.createEmployeePayroll("PDF");
+        System.out.println("Creating contract payroll in PDF for " + payroll.employeeName);
+        payroll = contractFactory.createEmployeePayroll("JSON");
+        System.out.println("Creating contract payroll in JSON for " + payroll.employeeName);
+
+
     }
 
 }
