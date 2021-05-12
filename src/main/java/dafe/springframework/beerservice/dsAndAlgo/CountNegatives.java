7be927a25cc1c20449b4cb295080a3cbf722daab
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-05-11 21:05:29 -0600

    solve count negative wit hbinary search algo

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/CountNegatives.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/CountNegatives.java
@@ -21,10 +21,31 @@ public static int countNegatives(int[][] grid){
     return count;
 }
 
+public static int countNegativesInMatrix(int [][] grid){
+
+    int counter = 0;
+    for(int i=0; i<grid.length; i++){
+        int start =0, end = grid[i].length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(grid[i][mid]<0){
+                end = mid-1;
+            }else {
+                start = mid+1;
+            }
+        }
+        counter = counter + grid[i].length - start;
+        end = start -1;
+    }
+
+    return counter;
+}
+
     public static void main(String[] args) {
 
     int[][] test1= {{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}};
 
         System.out.println("This is the counter of negatives in the sorted matrix : " + countNegatives(test1));
+        System.out.println("This is the counter of negatives in the sorted matrix : " + countNegativesInMatrix(test1));
     }
 }
