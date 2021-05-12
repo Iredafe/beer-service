b4a06c27363c4ce8c81079e1f72049118bc6b9a1
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-05-11 19:07:38 -0600

    algo for count negatives

---
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/CountNegatives.java
@@ -0,0 +1,30 @@
+package dafe.springframework.beerservice.dsAndAlgo;
+
+/*
+Given a m x n matrix grid which is sorted in non-increasing order
+both row-wise and column-wise, return the number of negative numbers in grid.
+ */
+
+public class CountNegatives {
+
+
+public static int countNegatives(int[][] grid){
+
+    int count =0;
+    for(int i=0; i<grid.length; i++){
+        for(int j=0; j<grid[i].length; j++){
+            if(grid[i][j] < 0)
+                count++;
+        }
+    }
+
+    return count;
+}
+
+    public static void main(String[] args) {
+
+    int[][] test1= {{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}};
+
+        System.out.println("This is the counter of negatives in the sorted matrix : " + countNegatives(test1));
+    }
+}
