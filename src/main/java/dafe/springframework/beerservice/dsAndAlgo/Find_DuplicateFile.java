cb89d084dbe206417649e2938d0b244ae5a24bab
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-06-23 22:47:36 -0600

    built algo with hashmap. closes #11

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/Find_DuplicateFile.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/Find_DuplicateFile.java
@@ -1,20 +1,35 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class Find_DuplicateFile {
 
-    public static List <List<String>> findDuplicate(String[] path){
+    public static List <List<String>> findDuplicate(String[] paths){
 
         HashMap <String, List<String>> myHashStore = new HashMap<>();
-        
+        for(String path: paths){
+            String [] values = path.split(" ");
+            for(int i=1; i< values.length; i++){
+                String [] fileName = values[i].split("\\(");
+                fileName[1]= fileName[1].replace(")","");
+         List<String> list = myHashStore.getOrDefault(fileName[1], new ArrayList<String>());
+         list.add(values[0]+ "/" + fileName[0]);
+         myHashStore.put(fileName[1], list);
+            }
+        }
 
-
+        List<List<String>> result = new ArrayList<>();
+        for(String key: myHashStore.keySet()){
+            if(myHashStore.get(key).size()>1)
+                result.add(myHashStore.get(key));
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        String [] input ={"root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)","root 4.txt(efgh)"};
+        String [] input ={"root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)","root 4.txt(efg)"};
 
         System.out.println("The answer is : " + findDuplicate(input));
     }
