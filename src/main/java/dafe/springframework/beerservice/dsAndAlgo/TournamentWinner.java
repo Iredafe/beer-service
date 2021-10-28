661360f37dd0d3d9ece11f6feba3593f8d01df9e
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-27 23:06:00 -0600

    tournament winner game WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/TournamentWinner.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/TournamentWinner.java
@@ -1,4 +1,14 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+
 public class TournamentWinner {
+
+   public String tournamentWinner(ArrayList<ArrayList<String>> competitions,ArrayList<Integer> results){
+       String winner ="";
+
+
+       return winner;
+   }
+
 }
