b095c15d865edafbe1a509777d3ff6ce775bc1fa
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-28 22:46:12 -0600

    tournament winner WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/TournamentWinner.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/TournamentWinner.java
@@ -1,13 +1,19 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TournamentWinner {
 
+    public int HOME_TEAM_WON = 1;
    public String tournamentWinner(ArrayList<ArrayList<String>> competitions,ArrayList<Integer> results){
        String winner ="";
+    Map<String, Integer> scores = new HashMap<>();
+    for(int i=0; i<= results.size(); i++){
+        int result = results.get(i);
 
-
+    }
        return winner;
    }
 
