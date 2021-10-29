2b14cf6bf2bd7c0c110f995f44f8c307d97e43e5
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-29 15:26:25 -0600

    tournament winner WIP change arraylist to list for testing

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/TournamentWinner.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/TournamentWinner.java
@@ -1,18 +1,15 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class TournamentWinner {
 
     public static int HOME_TEAM_WON = 1;
-   public static String tournamentWinner(ArrayList<ArrayList<String>> competitions,ArrayList<Integer> results){
+   public static String tournamentWinner(List<List<String>> competitions,ArrayList<Integer> results){
        String currentBestTeam ="";
     Map<String, Integer> scores = new HashMap<>();
     for(int index=0; index<= results.size(); index++){
-        ArrayList<String> competition = competitions.get(index);
+        List<String> competition = competitions.get(index);
         scores.put(currentBestTeam, 0);
         int result = results.get(index);
         String homeTeam = competition.get(0);
@@ -28,13 +25,29 @@ public class TournamentWinner {
    }
 
     private static void updateScores(String team, int points, Map<String, Integer> scores) {
-
+    //this line of code needs more clarity... what if team does not exist in scores but just won 3 points? would we go ahead and put 0 as ?
        if(!scores.containsKey(team)){
            scores.put(team, 0);
        }
         scores.put(team, scores.get(team) +points);
 
    }
+
+
     public static void main(String[] args) {
-    }
+   List<List<String>> list = new ArrayList<List<String>>();
+   list.add(List.of("HTML", "C#"));
+    list.add(List.of("C#", "Python"));
+    list.add(List.of("Python", "HTML"));
+
+
+        ArrayList<Integer> results = new ArrayList<>();
+        results.add(0);
+        results.add(0);
+        results.add(1);
+
+        System.out.println(tournamentWinner(list, results));
+   }
+
+
 }
