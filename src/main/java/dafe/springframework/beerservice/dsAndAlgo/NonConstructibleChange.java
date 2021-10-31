c995003a0a4a542c9e16bfc7d648d6d94f173bc5
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-30 22:59:54 -0600

    non-constructible change done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NonConstructibleChange.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NonConstructibleChange.java
@@ -1,15 +1,28 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.Arrays;
+
 public class NonConstructibleChange {
 
     public static int nonConstructibleChange(int[] coins) {
         // Write your code here.
-        return -1;
+        int change = 0;
+//sort input
+        Arrays.sort(coins);
+        //loop through the sorted input and add the inputs
+     for(int coin:coins){
+         //if the change + 1 is < a given number in an index, then we cannot make the change, return change+1
+         if(coin> change+1){
+                change+=1;
+             //if the change is less than or equal to the given number in an index, we can make the change, add the coin to change
+         }else if(coin <= change+1){
+             change+=coin;
+         }
+     }
+        return change;
     }
-
     public static void main(String[] args) {
         int[] input ={5, 7, 1, 1, 2, 3, 22};
-
         System.out.println("The minimum change is : " + nonConstructibleChange(input));
     }
 }
