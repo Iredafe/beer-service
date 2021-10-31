53331d6a1983db92ad0793cee346425f072f7864
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-30 23:22:42 -0600

    palindrome check started

---
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/PalindromeCheck.java
@@ -0,0 +1,12 @@
+package dafe.springframework.beerservice.dsAndAlgo;
+
+public class PalindromeCheck {
+
+    public static boolean isPalindrome(String str){
+
+        boolean isPalindrome=true;
+
+
+        return isPalindrome;
+    }
+}
