ff34f8940a095cc9249b1a2c975f05c0559b271c
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-30 22:25:04 -0600

    non-constructible change WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NonConstructibleChange.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NonConstructibleChange.java
@@ -2,8 +2,14 @@ package dafe.springframework.beerservice.dsAndAlgo;
 
 public class NonConstructibleChange {
 
-    public int nonConstructibleChange(int[] coins) {
+    public static int nonConstructibleChange(int[] coins) {
         // Write your code here.
         return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] input ={5, 7, 1, 1, 2, 3, 22};
+
+        System.out.println("The minimum change is : " + nonConstructibleChange(input));
     }
 }
