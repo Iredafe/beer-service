ed9d3ed50cb1e5b8f9640d17f2ceb34e19ffabb1
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-30 23:38:35 -0600

    palindrome check using string concatenation

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/PalindromeCheck.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/PalindromeCheck.java
@@ -3,11 +3,17 @@ package dafe.springframework.beerservice.dsAndAlgo;
 public class PalindromeCheck {
 
     public static boolean isPalindrome(String str){
+        String reverse ="";
+        char[] characters = str.toCharArray();
+        for(int i=characters.length-1; i>=0; i--){
+            reverse+=characters[i];
+        }
+        if(reverse.equalsIgnoreCase(str)){
+            return true;
+        }
+        System.out.println(reverse);
 
-        boolean isPalindrome=true;
-
-
-        return isPalindrome;
+        return false;
     }
 
     public static void main(String[] args) {
