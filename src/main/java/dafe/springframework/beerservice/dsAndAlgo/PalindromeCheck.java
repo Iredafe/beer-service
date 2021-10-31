d79a6429ed941f0c9c9f8750f372e213e9039d92
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-30 23:51:30 -0600

    palindrome check I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/PalindromeCheck.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/PalindromeCheck.java
@@ -8,12 +8,7 @@ public class PalindromeCheck {
         for(int i=characters.length-1; i>=0; i--){
             reverse+=characters[i];
         }
-        if(reverse.equalsIgnoreCase(str)){
-            return true;
-        }
-        System.out.println(reverse);
-
-        return false;
+        return reverse.equals(str);
     }
 
     public static void main(String[] args) {
