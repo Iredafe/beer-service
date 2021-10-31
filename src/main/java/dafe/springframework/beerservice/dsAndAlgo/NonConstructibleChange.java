7622cd0d7999ce9aec55985bd02520a104aee85b
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-30 23:15:46 -0600

    non-constructible change done III best solution

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NonConstructibleChange.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NonConstructibleChange.java
@@ -10,16 +10,16 @@ public class NonConstructibleChange {
 //sort input
         Arrays.sort(coins);
         //loop through the sorted input and add the inputs
-     for(int index=0; index<coins.length; index++){
+     for(int coin:coins){
          //if the change + 1 is < a given number in an index, then we cannot make the change, return change+1
-         if((change+1)< coins[index]){
-                change+=1;
-             //if the change is greater than or equal to the given number in an index, we can make the change, add the coin to change
-         }else if((change+1) >= coins[index]){
-             change+=coins[index];
+         if(coin> change+1){
+                return change+1;
+             //if the change is less than or equal to the given number in an index, we can make the change, add the coin to change
          }
+             change+=coin;
+
      }
-        return change;
+        return change+1;
     }
     public static void main(String[] args) {
         int[] input ={5, 7, 1, 1, 2, 3, 22};
