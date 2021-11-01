09c9e09a02876a01f469b6c8519b037bbf051158
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-31 21:17:13 -0600

    palindrome check III done properly(add comment)

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/PalindromeCheck.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/PalindromeCheck.java
@@ -2,7 +2,7 @@ package dafe.springframework.beerservice.dsAndAlgo;
 
 public class PalindromeCheck {
 
-    public static boolean isPalindrome(String str){
+    public static boolean isPalindrome(String str){ //O(n^2) time
         String reverse ="";
         char[] characters = str.toCharArray();
         for(int i=characters.length-1; i>=0; i--){
@@ -11,7 +11,7 @@ public class PalindromeCheck {
         return reverse.equals(str);
     }
 
-    public static boolean isItPalindrome(String str){
+    public static boolean isItPalindrome(String str){ //O(n) time
         StringBuilder newString = new StringBuilder();
 
         char[] characters = str.toCharArray();
@@ -21,7 +21,7 @@ public class PalindromeCheck {
         return str.equals(newString.toString());
     }
 
-    public static boolean isPalindromeUsingPointers(String str){
+    public static boolean isPalindromeUsingPointers(String str){ //O(n) time
         int left = 0, right= str.length()-1;
         while(left<right && left!=right){
             if(str.charAt(left) != str.charAt(right)){
@@ -30,6 +30,10 @@ public class PalindromeCheck {
             left++;
             right--;
         }
+        return true;
+    }
+    public static boolean isPalindromeRecursive(String str){
+
         return true;
     }
 
