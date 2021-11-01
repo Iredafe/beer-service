2a27c6e99c57cfc17f9359db3da7d00cc827b7e7
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-31 19:37:37 -0600

    palindrome check II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NonConstructibleChange.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NonConstructibleChange.java
@@ -14,15 +14,16 @@ public class NonConstructibleChange {
          //if the change + 1 is < a given number in an index, then we cannot make the change, return change+1
          if(coin> change+1){
                 return change+1;
-             //if the change is less than or equal to the given number in an index, we can make the change, add the coin to change
+             //if the change is less than or equal to the given number in an index, we can make the change,
          }
+         //add the coin to change
              change+=coin;
 
      }
         return change+1;
     }
     public static void main(String[] args) {
-        int[] input ={5, 7, 1, 1, 2, 3, 22};
+        int[] input ={5, 7, 1, 1, 2, 3, 22}; //sorted = {1,1,2,3,5,7,22}
         System.out.println("The minimum change is : " + nonConstructibleChange(input));
     }
 }
--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/PalindromeCheck.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/PalindromeCheck.java
@@ -11,8 +11,18 @@ public class PalindromeCheck {
         return reverse.equals(str);
     }
 
+    public static boolean isItPalindrome(String str){
+        StringBuilder newString = new StringBuilder();
+
+        char[] characters = str.toCharArray();
+        for(int i = characters.length-1; i>=0; i--){
+            newString.append(characters[i]);
+        }
+        return str.equals(newString.toString());
+    }
+
     public static void main(String[] args) {
         String input = "abcdcba";
-        System.out.println("This String is a palindrome : " + isPalindrome(input));
+        System.out.println("This String is a palindrome : " + isItPalindrome(input));
     }
 }
