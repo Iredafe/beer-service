689393ffeba1de6c344d54edb4ec7865a7d44a5e
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-01 21:42:33 -0600

    nth fibonacci I WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NthFibonacci.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NthFibonacci.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class NthFibonacci {
+
+    public static int getNthFib(int n){
+
+        return n;
+    }
+
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println("The Nth fibonacci number is : " + getNthFib(n));
+    }
+
 }
