76aa3d4eba0ef2149c2891709ead371762f07f8c
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-01 23:07:25 -0600

    nth fibonacci using DP started

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NthFibonacci.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NthFibonacci.java
@@ -12,6 +12,10 @@ public class NthFibonacci {
         return getNthFib(n-1) + getNthFib(n-2);
     }
 
+    public static int getNthFibDP(int n){
+
+        return n;
+    }
 
     public static void main(String[] args) {
         int n = 6;
