c7512a2e08da2cc5bed57b79bad2dafa2d630ab9
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-03 21:12:29 -0600

    caesar cypher encryptor WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/CeasarCypherEncryptor.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/CeasarCypherEncryptor.java
@@ -1,4 +1,18 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class CeasarCypherEncryptor {
+    public static String caesarCypherEncryptor(String str, int key){
+        char[] charcters = str.toCharArray();
+        for(int i=0; i< charcters.length; i++){
+
+        }
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+        String str="xyz";
+        int key =2;
+        System.out.println("This is the new string : " + caesarCypherEncryptor(str, key));
+    }
 }
