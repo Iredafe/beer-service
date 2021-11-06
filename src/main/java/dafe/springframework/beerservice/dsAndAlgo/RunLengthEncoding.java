84a30042cf2a0e0656507bc4072267135fc7057e
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-05 21:42:56 -0600

    run length encoding WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/RunLengthEncoding.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/RunLengthEncoding.java
@@ -1,4 +1,14 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class RunLengthEncoding {
+
+    public static String runLengthEncoding(String string){
+
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
