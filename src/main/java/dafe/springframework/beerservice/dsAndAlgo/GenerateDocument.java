b76e9d59d5f3f2e1e61e50eb739a3ee6434ec8a9
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-06 20:27:43 -0600

    generate document algo WIP II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/GenerateDocument.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/GenerateDocument.java
@@ -1,9 +1,21 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GenerateDocument {
 
     public static boolean generateDoc(String characters, String document) {
+    //initialize a variable to store the elements in the available characters
+        //loop through the characters and check if each letter in the document is contained therein
+        //if all the letters in the document are found stored uniquely in the character store, then return true
+        //else return false
 
+        Map<Integer, String> characterMap = new HashMap<>();
+        for(int i=0; i<characters.length(); i++){
+            characterMap.put(i,characters);
+
+        }
         return false;
     }
 
