b797055bbc9348a66c88cddc866d5f8ce13f62cd
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-06 22:49:51 -0600

    first non-repeating character WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FirstNonRepeatingCharacter.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FirstNonRepeatingCharacter.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class FirstNonRepeatingCharacter {
+
+    public static int firstNonRepeatingCharacter(String string){
+
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+
+        String input="abcdcaf";
+        System.out.println("This is the index of first non- repeating character : " + firstNonRepeatingCharacter(input));
+    }
 }
