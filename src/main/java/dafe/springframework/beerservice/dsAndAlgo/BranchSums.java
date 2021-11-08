cc08c0dc67158102b1c4b133eb81f8347e64d7b8
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-07 21:46:01 -0700

    branch sum binary tree class created

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BranchSums.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BranchSums.java
@@ -1,4 +1,15 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class BranchSums {
+    public static class BinaryTree {
+        int value;
+        BinaryTree left;
+        BinaryTree right;
+
+        BinaryTree(int value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
 }
