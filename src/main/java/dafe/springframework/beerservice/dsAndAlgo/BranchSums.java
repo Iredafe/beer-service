b29389f55234925fcee7dd05eeb96d9001ec74a1
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-07 21:48:57 -0700

    branch sum algo WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BranchSums.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BranchSums.java
@@ -1,15 +1,25 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
-public class BranchSums {
-    public static class BinaryTree {
-        int value;
-        BinaryTree left;
-        BinaryTree right;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.ArrayList;
+import java.util.List;
 
-        BinaryTree(int value) {
-            this.value = value;
-            this.left = null;
-            this.right = null;
-        }
+public class BranchSums {
+
+    public static class BinaryTree{
+    int value;
+    BinaryTree left;
+    BinaryTree right;
+
+    public BinaryTree(int value){
+        this.value=value;
+        this.left=null;
+        this.right=null;
+    }
+    }
+
+    public static List<Integer> branchSums(BinaryTree){
+
+        return new ArrayList<Integer>();
     }
 }
