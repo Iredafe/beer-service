5f3d34adc2e3809bdbb057a5b7cc8985f2747b68
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-08 23:03:12 -0700

    remove duplicate from linked list WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/RemoveDuplicatesFromLinkedList.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/RemoveDuplicatesFromLinkedList.java
@@ -1,4 +1,14 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class RemoveDuplicatesFromLinkedList {
+
+    public static class LinkedList{
+        public int value;
+        public LinkedList next;
+
+        public LinkedList(int value){
+            this.value=value;
+            this.next = null;
+        }
+    }
 }
