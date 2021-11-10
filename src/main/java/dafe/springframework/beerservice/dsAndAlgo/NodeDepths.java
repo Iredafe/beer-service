a45b966dd3237d473a36e33ba4587c0f57715644
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-09 21:39:13 -0700

    create node

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NodeDepths.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NodeDepths.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class NodeDepths {
+
+    public static class BinaryTree{
+        int value; BinaryTree left, right;
+        public BinaryTree(int value){
+            this.value = value;
+            this.left=null;
+            this.right=null;
+        }
+    }
 }
