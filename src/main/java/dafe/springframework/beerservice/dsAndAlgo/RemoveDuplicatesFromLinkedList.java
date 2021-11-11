5fcd7c359f3de5e3dbe2bb70b5b6488babc6db38
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-10 23:15:24 -0700

    remove duplicates WIP II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/RemoveDuplicatesFromLinkedList.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/RemoveDuplicatesFromLinkedList.java
@@ -3,7 +3,7 @@ package dafe.springframework.beerservice.dsAndAlgo;
 import org.springframework.boot.actuate.endpoint.web.Link;
 
 public class RemoveDuplicatesFromLinkedList {
-
+    LinkedList head;
     public static class LinkedList{
         public int value;
         public LinkedList next;
@@ -12,6 +12,19 @@ public class RemoveDuplicatesFromLinkedList {
             this.value=value;
             this.next = null;
         }
+    }
+
+    //utility function
+    //insert a new node at the front of the list
+    public void push(int new_value) {
+        //allocate the node and put in the value
+        LinkedList newNode = new LinkedList(new_value);
+
+        //make next of new LinkedList node as head
+        newNode.next = head;
+
+        //move the head to point to the new node
+    head = newNode;
     }
 
     public static LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList){
