880e2243b11617aad5b98b8dccb09b5e748e54fb
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-11 20:26:45 -0700

    three sum WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ThreeSum.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ThreeSum.java
@@ -1,4 +1,12 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreeSum {
+
+    public static List<Integer> threeNumberSum(int array[] ,int target){
+
+        return new ArrayList<>();
+    }
 }
