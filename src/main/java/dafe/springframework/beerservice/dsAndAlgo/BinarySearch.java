26f922d0c6c22cfaf247dc501895416c1b131515
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-12 21:35:03 -0700

    binary search done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BinarySearch.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BinarySearch.java
@@ -4,6 +4,21 @@ public class BinarySearch {
 
     public static int binarySearch(int [] array , int target){
 
+      return binarySearchHelper(array, target, 0, array.length-1);
+    }
+
+    private static int binarySearchHelper(int[] array, int target, int left, int right) {
+        while(left<=right){
+            int mid=(right+left)/2;
+            int searchResult = array[mid];
+            if(target==searchResult){
+                return mid;
+            }else if(target>searchResult){
+                left= mid+1;
+            }else {
+                right=mid-1;
+            }
+        }
         return -1;
     }
 
@@ -12,6 +27,6 @@ public class BinarySearch {
         int [] input = {0, 1, 21, 33, 45, 45, 61, 71, 72, 73};
         int target = 33;
 
-        System.out.println("This is the index for the number you searched for " + binarySearch(input, target));
+        System.out.println("The index for the number you searched for is : " + binarySearch(input, target));
     }
 }
