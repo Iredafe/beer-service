abcb92bbb3d43f46bf90251b0501bd8996e4bc8c
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-12 21:59:38 -0700

    binary search -added comments

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BinarySearch.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BinarySearch.java
@@ -13,8 +13,12 @@ public class BinarySearch {
             int searchResult = array[mid];
             if(target==searchResult){
                 return mid;
+                //if the target number is greater than the search result, cut away the left of the array
+                // because we now know the target should be on the right
             }else if(target>searchResult){
                 left= mid+1;
+            //if the target number is less than the search result, cut away the right of the array
+                //because we now know that the target should be on the left of the array
             }else {
                 right=mid-1;
             }
