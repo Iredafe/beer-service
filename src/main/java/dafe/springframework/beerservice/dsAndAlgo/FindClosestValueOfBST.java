1eab5e1378abc89241c1c87a2fb6f2b8c50c972c
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-13 22:51:04 -0700

    find closest BST WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FindClosestValueOfBST.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FindClosestValueOfBST.java
@@ -1,7 +1,7 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class FindClosestValueOfBST {
-
+    static BST tree;
     static class BST{
         public int value;
         BST left, right;
@@ -10,10 +10,21 @@ public class FindClosestValueOfBST {
             this.value = value;
         }
     }
-    public static int findClosestValueInBst(BST tree, int target){
+    public static int findClosestValueInBstRecursive(BST tree, int target){
 
-        return 0;
+        return findClosestValueInBstRecursiveHelper(tree, target, tree.value);
+         }
+
+    public static int findClosestValueInBstRecursiveHelper(BST tree, int target, int closest){
+        if(tree == null) return closest;
+        else if(Math.abs(target-closest) > Math.abs(target-tree.value)){
+            closest=tree.value;
+        }else if(target< tree.value && tree.left!=null){
+            return findClosestValueInBstRecursiveHelper(tree.left, target, closest);
+        }else if(target>tree.value && tree.right!=null){
+            return findClosestValueInBstRecursiveHelper(tree.right, target, closest);
+        }
+    return closest;
     }
-
 
 }
