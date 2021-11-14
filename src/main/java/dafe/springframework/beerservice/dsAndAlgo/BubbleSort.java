9e7463bb389024dbac17e6c3c309fadee23813d4
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-14 16:31:13 -0700

    bubble sort done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BubbleSort.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BubbleSort.java
@@ -1,4 +1,35 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.Arrays;
+
 public class BubbleSort {
+
+    public static int[] bubbleSort(int [] array){
+        if(array.length==0){
+            return new int[]{};
+        }
+        boolean isSorted = false;
+        while (!isSorted){
+            isSorted=true;
+            for(int firstIndex=0; firstIndex<array.length-1; firstIndex++){
+                if(array[firstIndex] > array[firstIndex+1]){
+                    swap(array, firstIndex, firstIndex+1);
+                isSorted=false;
+                }
+            }
+        }
+        return array;
+    }
+    private static void swap(int[] array, int firstIndex, int secondIndex) {
+
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex]=temp;
+    }
+
+    public static void main(String[] args) {
+        int [] array = new int[]{8, 5, 2, 9, 5, 6, 3};
+        int [] result = bubbleSort(array);
+        System.out.println("This is the sorted array : " + Arrays.toString(result));
+    }
 }
