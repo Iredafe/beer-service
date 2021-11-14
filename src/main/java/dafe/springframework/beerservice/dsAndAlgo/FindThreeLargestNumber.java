aa3234be9234500b99ca989195f1af746b5df238
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-13 18:08:11 -0700

    find three largest number soln

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FindThreeLargestNumber.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FindThreeLargestNumber.java
@@ -1,16 +1,41 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.Arrays;
+
 public class FindThreeLargestNumber {
 
     public static int[] findThreeSum(int [] array){
 
+        int [] result = {Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE};
+        for(int index=0; index<array.length; index++){
+            updateLargestNumber(result, array[index]);
+        }
+        return result;
+    }
 
-        return null;
+    private static void updateLargestNumber(int[] result, int number) {
+        if(number > result[2]){
+            shiftAndUpdate(result, number, 2);
+        }else if(number > 1){
+            shiftAndUpdate(result, number, 1);
+        }else if(number > 0){
+            shiftAndUpdate(result, number, 0);
+        }
+    }
+
+    private static void shiftAndUpdate(int [] array, int number, int index){
+        for(int idx=0; idx<= index; idx++){
+         if(idx == index){
+             array[idx] = number;
+         }else {
+             array[idx] = array[idx+1];
+         }
+        }
     }
 
     public static void main(String[] args) {
-        int [] input ={141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7};
-
-        System.out.println("These are the three largest numbers in the array : " + findThreeSum(input));
+        int [] input =new int [] {141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7};
+    int [] result = findThreeSum(input);
+        System.out.println("These are the three largest numbers in the array : " + Arrays.toString(result));
     }
 }
