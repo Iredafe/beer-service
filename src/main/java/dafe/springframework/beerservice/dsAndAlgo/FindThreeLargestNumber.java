135433c422686431b860ce4fbef53617c3549a2b
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-13 17:06:39 -0700

    find three largest number WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FindThreeLargestNumber.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FindThreeLargestNumber.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class FindThreeLargestNumber {
+
+    public static int[] findThreeSum(int [] array){
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int [] input ={141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7};
+
+        System.out.println("These are the three largest numbers in the array : " + findThreeSum(input));
+    }
 }
