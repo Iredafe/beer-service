b09f3a5863a0e886ca0345c302e95284fdb71b5d
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-13 22:41:43 -0700

    build BST

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FindClosestValueOfBST.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FindClosestValueOfBST.java
@@ -1,4 +1,19 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class FindClosestValueOfBST {
+
+    static class BST{
+        public int value;
+        BST left, right;
+
+        public BST(int value){
+            this.value = value;
+        }
+    }
+    public static int findClosestValueInBst(BST tree, int target){
+
+        return 0;
+    }
+
+
 }
