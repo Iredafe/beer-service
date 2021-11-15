a3d670c513d06cfb1f18d509243219b125d22ad4
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-14 20:27:31 -0700

    reverse words in string solution not as desired

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ReverseWordsInString.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ReverseWordsInString.java
@@ -1,4 +1,22 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class ReverseWordsInString {
+
+    public static String reverseWordsInString(String string){
+
+        StringBuilder result = new StringBuilder();
+        for(int index = string.length()-1; index>=0; index--){
+            char character = string.charAt(index);
+            result.append(character);
+        }
+
+        return result.toString();
+    }
+
+
+    public static void main(String[] args) {
+        String input =  "AlgoExpert is the best!";
+
+        System.out.println("This is the reversed string : " + reverseWordsInString(input));
+    }
 }
