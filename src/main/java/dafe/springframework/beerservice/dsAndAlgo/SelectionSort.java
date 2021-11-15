ce33707e21399e2f633679ac0087087f91e13bbb
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-14 17:19:32 -0700

    selection sort WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SelectionSort.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SelectionSort.java
@@ -1,4 +1,21 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.Arrays;
+
 public class SelectionSort {
+
+    public static int[] selectionSort(int[] array){
+
+
+
+        return array;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] input = {8, 5, 2, 9, 5, 6, 3};
+        int[] result = selectionSort(input);
+        System.out.println("This is the new array of selection sorting : " + Arrays.toString(result));
+    }
 }
