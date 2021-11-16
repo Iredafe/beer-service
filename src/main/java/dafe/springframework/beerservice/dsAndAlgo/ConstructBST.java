564864e28800b684e4bd0ef33df528aa1320f71d
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-15 19:41:57 -0700

    BST construction started

---
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ConstructBST.java
@@ -0,0 +1,29 @@
+package dafe.springframework.beerservice.dsAndAlgo;
+
+public class ConstructBST {
+
+    static class BST{
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value){
+            this.value = value;
+        }
+
+        public BST insert(int value){
+
+            return this;
+        }
+
+        public boolean contains(int value){
+
+            return false;
+        }
+
+        public BST remove(int value){
+
+            return this;
+        }
+    }
+}
--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ReverseWordsInString.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ReverseWordsInString.java
@@ -1,9 +1,6 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class ReverseWordsInString {
     public static String reverseWordsInString(String string) {
@@ -21,13 +18,16 @@ public class ReverseWordsInString {
          }
      }
      //add the last word to the list because if there is no space after the last word, it would not be added to the string
-     results.add(string.substring(startOfWord));
+    // results.add(string.substring(startOfWord));
      Collections.reverse(results);
      return String.join("", results);
     }
 
+
     public static void main(String[] args) {
-        String input =  "4 wh";
+        String input =  "4 whaat '.";
+        String input1 =  "4 whaat '. ";
         System.out.println("This is the reversed string : " + reverseWordsInString(input));
+        System.out.println("This is the reversed string : " + reverseWordsInString(input1));
     }
 }
