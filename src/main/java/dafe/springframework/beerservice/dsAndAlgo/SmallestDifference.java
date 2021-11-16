f3ea8e6209bf2953824cd808ae80ab82630c5874
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-15 20:12:20 -0700

    smallest difference WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SmallestDifference.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SmallestDifference.java
@@ -1,4 +1,19 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class SmallestDifference {
+
+    public static int[] smallestDifference(int [] firstArray, int [] secondArray){
+        //create 2 hashmap stores for both arrays
+        //loop through the first hashmap and store each value in the firs array
+        //loop through the second hashmap and subtract
+
+        return new int[]{};
+    }
+
+
+    public static void main(String[] args) {
+        int [] input1 = {-1, 5, 10, 20, 28, 3};
+        int [] input2 ={26, 134, 135, 15, 17 };
+        System.out.println("These are the numbers with the smallest difference : " + smallestDifference(input1, input2));
+    }
 }
