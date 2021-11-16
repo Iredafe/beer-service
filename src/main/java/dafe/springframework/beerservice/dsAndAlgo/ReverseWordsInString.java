6f7828ec5f4ac42de5c3b27e3957c1f28e203bda
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-15 18:15:18 -0700

    reverse words in string solution done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ReverseWordsInString.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ReverseWordsInString.java
@@ -1,22 +1,33 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class ReverseWordsInString {
-
-    public static String reverseWordsInString(String string){
-
-        StringBuilder result = new StringBuilder();
-        for(int index = string.length()-1; index>=0; index--){
-            char character = string.charAt(index);
-            result.append(character);
-        }
-
-        return result.toString();
+    public static String reverseWordsInString(String string) {
+     List<String> results = new ArrayList<>();
+     int startOfWord = 0;
+     for(int index = 0; index<string.length(); index++){
+        char character = string.charAt(index);
+         if(character == ' '){
+             String charactersToAdd = string.substring(startOfWord, index);
+             results.add(charactersToAdd);
+             startOfWord=index;
+         }else if(string.charAt(startOfWord) == ' '){
+             results.add(" ");
+             startOfWord=index;
+         }
+     }
+     //add the last word to the list because if there is no space after the last word, it would not be added to the string
+     results.add(string.substring(startOfWord));
+     Collections.reverse(results);
+     return String.join("", results);
     }
 
-
     public static void main(String[] args) {
-        String input =  "AlgoExpert is the best!";
-
+        String input =  "4 wh";
         System.out.println("This is the reversed string : " + reverseWordsInString(input));
     }
 }
