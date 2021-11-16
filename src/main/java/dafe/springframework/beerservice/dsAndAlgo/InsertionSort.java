35bba4e3b925fb27f2b3994b28e7c02fe685141c
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-15 23:16:09 -0700

    insertion sort algorithm WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/InsertionSort.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/InsertionSort.java
@@ -1,4 +1,20 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.Arrays;
+
 public class InsertionSort {
+
+    public static int[] insertionSort(int[] array){
+
+        return new int[] {};
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int [] input = {8, 5, 2, 9, 5, 6, 3};
+        int [] result = insertionSort(input);
+        System.out.println("This is the sorted array : " + Arrays.toString(result));
+    }
 }
