1cfe029762ca35c6c974a89ac0508e9f16bdaaf4
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-16 00:15:45 -0700

    insertion sort algorithm done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/InsertionSort.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/InsertionSort.java
@@ -6,9 +6,21 @@ public class InsertionSort {
 
     public static int[] insertionSort(int[] array){
 
-        return new int[] {};
+        for(int i = 1; i< array.length; i++){
+            int j = i;
+            while(j >0 && array[j] < array[j-1]){
+                swap(array, j, j-1);
+                j--;
+            }
+        }
+        return array;
     }
 
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 
     public static void main(String[] args) {
 
