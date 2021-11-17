7f6e068c9b214fd0410f46457f559f13829e3e95
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-16 23:15:40 -0700

    maximum subset sum no adjacent with iterative solution

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MaxSubsetSumNoAdjacent.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MaxSubsetSumNoAdjacent.java
@@ -14,10 +14,27 @@ public class MaxSubsetSumNoAdjacent {
         return maxSums[array.length-1];
     }
 
+    //I still do not fully grasp this approach -- revisit
+    public static int maxSubsetSumNoAdjacentIterative(int [] array){
+
+        if(array.length ==0) return 0;
+        else if(array.length==1) return array[0];
+
+        int first = array[1];
+        int second = array[0];
+
+        for(int index=2; index<array.length; index++){
+            int current = Math.max(first, second+array[index]);
+            second = first;
+            first = current;
+        }
+     return first;
+    }
     public static void main(String[] args) {
 
         int [] input = {75, 105, 120, 75, 90, 135,900};
 
         System.out.println("This is the maximum : " + maxSubsetSumNoAdjacentRecursive(input));
+        System.out.println("This is the maximum : " + maxSubsetSumNoAdjacentIterative(input));
     }
 }
