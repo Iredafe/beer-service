c3aabc640c40f5abc0594be359f6c2dc4786e582
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-17 22:10:02 -0700

    no of ways to make change - DP done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NumberOfWaysToMakeChange.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NumberOfWaysToMakeChange.java
@@ -1,4 +1,31 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class NumberOfWaysToMakeChange {
+
+
+    public static int numberOfWaysToMakeChange(int [] denominations, int targetAmount){
+
+        int [] ways = new int[targetAmount+1];
+        ways[0] = 1;
+        for(int denomination : denominations){
+            for(int amount=1; amount < targetAmount+1; amount++){
+                if(denomination<=amount){
+                    ways[amount] += ways[amount-denomination];
+                }
+            }
+        }
+        return ways[targetAmount];
+    }
+
+    public static void main(String[] args) {
+        int denoms [] = {2, 3, 4, 7};
+        int denoms1[] = {1,5};
+        int target = 0;
+        int target1= 6;
+
+        int result = numberOfWaysToMakeChange(denoms, target);
+        int result1 = numberOfWaysToMakeChange(denoms1,target1);
+        System.out.println("The number of ways is : " + result);
+        System.out.println("The number of ways is : " + result1);
+    }
 }
