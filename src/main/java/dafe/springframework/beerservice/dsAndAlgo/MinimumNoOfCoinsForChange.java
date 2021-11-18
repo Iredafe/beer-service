b617954f9bbe40004079fb6b19e9fed30bf4e6bc
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-17 22:34:36 -0700

    minimum no of coins for change - DP WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinimumNoOfCoinsForChange.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinimumNoOfCoinsForChange.java
@@ -1,4 +1,26 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class MinimumNoOfCoinsForChange {
+
+    public static int minimumNoOfCoinsForChange(int target, int [] denominations){
+
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int target = 7;
+        int [] denom = {1,5,10};
+
+        int target1 = 7;
+        int [] denom1 = {10, 1, 5};
+
+        int result = minimumNoOfCoinsForChange(target, denom);
+        int result1 = minimumNoOfCoinsForChange(target1, denom1);
+        System.out.println("This is the minimum no of coins for change " + result);
+        System.out.println("This is the minimum no of coins for change " + result1);
+    }
+
+
 }
