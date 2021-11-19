6b607f41d28ef98548cc2f0914ee59eb20744b6b
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-18 18:13:34 -0700

    minimum waiting time - done with greedy algorithm

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinimumWaitingTime.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinimumWaitingTime.java
@@ -1,10 +1,26 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.Arrays;
+
 public class MinimumWaitingTime {
 
+    //GREEDY ALGORITHM
     public static  int minimumWaitingTime(int [] queries){
+        //sort the array
+        //loop through the array
+        //at each index, get the value in the previous index and multiply it by the number of indexes left
+        //update the total no of queries with your answer
+        //after looping the array, return the total no of minimum waiting time
+        int minimumWaitTime =0;
+        Arrays.sort(queries);
+        if(queries.length==0) return minimumWaitTime;
+        for(int index = 0; index<queries.length; index++){
+            int queryDuration = queries[index];
+            int numberOfWaitingQueries = queries.length-(index+1);
+            minimumWaitTime += queryDuration * numberOfWaitingQueries;
+        }
 
-        return 1;
+        return minimumWaitTime;
     }
 
 
