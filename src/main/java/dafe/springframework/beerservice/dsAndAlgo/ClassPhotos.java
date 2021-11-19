e2e2c1b559cb629ced15bbcc6d9d34e4d12906c4
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-18 19:16:13 -0700

    class photos WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ClassPhotos.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ClassPhotos.java
@@ -1,4 +1,21 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class ClassPhotos {
+
+    public static boolean isClassPhotosPossible(int[] redShirts, int [] blueShirts){
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int [] redShirtHeights = {6, 9, 2, 4, 5};
+        int [] blueShirtHeights ={5, 8, 1, 3, 4};
+        boolean answer = isClassPhotosPossible(redShirtHeights,blueShirtHeights);
+
+        System.out.println("Is the class photo possible to do successfully ? " + answer);
+
+    }
 }
