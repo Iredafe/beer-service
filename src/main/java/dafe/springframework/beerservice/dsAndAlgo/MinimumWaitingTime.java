b6a050abbaab2d988f843f23a4d81a18871c0d78
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-18 17:45:59 -0700

    minimum waiting time - WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinimumWaitingTime.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinimumWaitingTime.java
@@ -1,4 +1,17 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class MinimumWaitingTime {
+
+    public static  int minimumWaitingTime(int [] queries){
+
+        return 1;
+    }
+
+
+    public static void main(String[] args) {
+
+        int [] arrayOfQueries = {3, 2, 1, 2, 6};
+
+        System.out.println("This is the minimum waiting time for the query to complete : " + minimumWaitingTime(arrayOfQueries));
+    }
 }
