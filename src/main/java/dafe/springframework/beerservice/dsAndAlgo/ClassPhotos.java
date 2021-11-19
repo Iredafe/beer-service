fd3111327e3e3a93362ef8eca9017cabd034e552
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-18 19:56:05 -0700

    class photos done with arrays

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ClassPhotos.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ClassPhotos.java
@@ -1,9 +1,23 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.Arrays;
+
 public class ClassPhotos {
 
     public static boolean isClassPhotosPossible(int[] redShirts, int [] blueShirts){
+        Arrays.sort(blueShirts);
+        Arrays.sort(redShirts);
+       String shirtForFirstRow = redShirts[0] < blueShirts[0] ? "RED" : "BLUE";
 
+       for (int index = 0; index< redShirts.length; index++){
+          if(shirtForFirstRow == "RED"){
+              if(redShirts[index] >= blueShirts[index]){
+                  return false;
+              }
+          }else if(blueShirts[index] >= redShirts[index]){
+              return false;
+          }
+       }
         return true;
     }
 
