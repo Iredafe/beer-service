b7174ff1fafb9aa2de0e6042d51d9f61b55a4ecd
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-20 00:29:27 -0700

    min max stack constructions done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinMaxStackImplementation.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinMaxStackImplementation.java
@@ -1,26 +1,44 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MinMaxStackImplementation {
 
+    static List<Integer> stack = new ArrayList<>();
+    static List<Map<String, Integer>> minMaxStack = new ArrayList<>();
     static  class MinMaxStack {
          public int peek(){
-             return -1;
+             return stack.get(stack.size()-1);
          }
 
          public int pop(){
-             return -1;
+             minMaxStack.remove(minMaxStack.size()-1);
+             return stack.remove(stack.size()-1);
          }
 
          public void push(Integer number){
+            Map<String, Integer> newMinMax = new HashMap<>();
+            newMinMax.put("min", number);
+            newMinMax.put("max", number);
 
+            if(minMaxStack.size() > 0 ){
+                Map<String, Integer> lastMinMax = new HashMap<>(minMaxStack.get(minMaxStack.size()-1));
+                newMinMax.replace("min",Math.min(lastMinMax.get("min"), number));
+                newMinMax.replace("max", Math.max(lastMinMax.get("max"), number));
+            }
+             minMaxStack.add(newMinMax);
+             stack.add(number);
          }
 
          public int getMin(){
-             return -1;
+             return minMaxStack.get(minMaxStack.size()-1).get("min");
          }
 
          public int getMax(){
-             return -1;
+             return minMaxStack.get(minMaxStack.size()-1).get("max");
          }
 
     }
@@ -28,7 +46,7 @@ public class MinMaxStackImplementation {
 
     public static void main(String[] args) {
         MinMaxStack stack = new MinMaxStack();
-        stack.pop();
+
         stack.push(3);
         stack.push(2);
         System.out.println(" Stack 1 : " + stack);
