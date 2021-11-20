a17eca26208c63efd8ca483c8c5f5c9190df51b5
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-20 10:53:23 -0700

    validate BST WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidateBST.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidateBST.java
@@ -1,4 +1,35 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class ValidateBST {
+    static BST root;
+    static class BST{
+        int value; BST left; BST right;
+        public BST(int value){
+            this.value = value;
+        }
+    }
+
+
+    public static boolean validateBST(BST tree){
+
+        return true;
+    }
+
+
+    public static void main(String[] args){
+
+        ValidateBST tree = new ValidateBST();
+
+        tree.root = new BST(10);
+        tree.root.left = new BST(5);
+        tree.root.right = new BST(15);
+        tree.root.left.left = new BST(2);
+        tree.root.left.right = new BST(5);
+        tree.root.left.left.left = new BST(1);
+        tree.root.right.left = new BST(13);
+        tree.root.right.right = new BST(22);
+        tree.root.right.left.right = new BST(14);
+
+        System.out.println(" This BST is valid : " + tree.validateBST(root));
+    }
 }
