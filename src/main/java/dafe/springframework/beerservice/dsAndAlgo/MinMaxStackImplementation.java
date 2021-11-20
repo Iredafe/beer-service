a071be664ad18570c67b33e6e5f20dd6ba032edc
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-19 23:27:07 -0700

    min max stack constructions WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinMaxStackImplementation.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinMaxStackImplementation.java
@@ -2,5 +2,13 @@ package dafe.springframework.beerservice.dsAndAlgo;
 
 public class MinMaxStackImplementation {
 
+    static  class MinMaxStack {
+         public int peek(){
+             return -1;
+         }
 
+         public int pop(){
+             return -1;
+         }
+    }
 }
