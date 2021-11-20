cb02d307be42924d834698cba2180fbf8061b7b3
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-19 21:43:26 -0700

    single cycle check done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SingleCycleCheck.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SingleCycleCheck.java
@@ -1,4 +1,28 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class SingleCycleCheck {
+
+    public static boolean singleCycleCheck(int [] array){
+
+        int numberOfElementsVisited = 0;
+        int currentIndex = 0;
+        while (numberOfElementsVisited< array.length){
+            if(numberOfElementsVisited > 0 && currentIndex== 0) return false;
+            numberOfElementsVisited++;
+            currentIndex = getCurrentIndex(currentIndex, array);
+        }
+        return true;
+    }
+
+    public static int getCurrentIndex(int currentIndex, int [] array){
+        int jump = array[currentIndex];
+        int nextIndex = (jump + currentIndex) % array.length;
+        return nextIndex >= 0 ? nextIndex : nextIndex + array.length;
+    }
+
+    public static void main(String[] args) {
+
+        int[] input ={2, 3, 1, -4, -4, 2};
+        System.out.println("Is singly cycle check? : " + singleCycleCheck(input));
+    }
 }
