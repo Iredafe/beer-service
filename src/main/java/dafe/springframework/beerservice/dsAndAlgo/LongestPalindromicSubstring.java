d8ff389cc5b0aff215b026e0a2d1095fddaa8daa
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-20 22:55:54 -0700

    longest palindromic substring done sub-optimally

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LongestPalindromicSubstring.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LongestPalindromicSubstring.java
@@ -3,9 +3,26 @@ package dafe.springframework.beerservice.dsAndAlgo;
 public class LongestPalindromicSubstring {
 
     public static String longestPalindromicSubstring(String input ){
+    String longest = "";
+    for(int i=0; i < input.length(); i++){
+        for(int j=i; j<input.length(); j++){
+            String substring = input.substring(i, j+1);
+            if(substring.length() > longest.length() && isPalindrome(substring)){
+                longest = substring;
+            }
+        }
+    }
+        return longest;
+    }
 
-
-        return "";
+    public static boolean isPalindrome(String input){
+        int left = 0; int right = input.length()-1;
+        while(left < right){
+            if(input.charAt(left) != input.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
