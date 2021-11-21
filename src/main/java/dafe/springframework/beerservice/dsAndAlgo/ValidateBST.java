ea9ff3c50ccb37aebfccd3d8fd91b35c44ff9777
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-20 20:02:55 -0700

    validate BST done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidateBST.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidateBST.java
@@ -10,12 +10,20 @@ public class ValidateBST {
     }
 
 
+
     public static boolean validateBST(BST tree){
+
+        return validateBSTHelper(tree, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private static boolean validateBSTHelper(BST tree, int minValue, int maxValue){
+
+        if(tree.value < minValue || tree.value >=maxValue ) return false;
+        if(tree.left != null && !validateBSTHelper(tree.left, minValue, tree.value)) return false;
+        if(tree.right !=null && !validateBSTHelper(tree.right, tree.value, maxValue)) return false;
 
         return true;
     }
-
-
     public static void main(String[] args){
 
         ValidateBST tree = new ValidateBST();
@@ -25,9 +33,9 @@ public class ValidateBST {
         tree.root.right = new BST(15);
         tree.root.left.left = new BST(2);
         tree.root.left.right = new BST(5);
-        tree.root.left.left.left = new BST(1);
         tree.root.right.left = new BST(13);
         tree.root.right.right = new BST(22);
+        tree.root.left.left.left = new BST(1);
         tree.root.right.left.right = new BST(14);
 
         System.out.println(" This BST is valid : " + tree.validateBST(root));
