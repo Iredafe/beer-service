1484bd960ea98e6adad85fbfbff491f905eab93a
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-20 22:07:04 -0700

    longest palindromic substring WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LongestPalindromicSubstring.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LongestPalindromicSubstring.java
@@ -1,4 +1,17 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class LongestPalindromicSubstring {
+
+    public static String longestPalindromicSubstring(String input ){
+
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+        String input = "abaxyzzyxf";
+
+        System.out.println("This is the longest palindromic substring : " + longestPalindromicSubstring(input));
+    }
 }
