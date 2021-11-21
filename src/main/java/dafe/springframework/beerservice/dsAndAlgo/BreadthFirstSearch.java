3a4d1af854f31f7e8154537dfcd5f1f05b7c1baa
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-20 22:04:08 -0700

    BFS WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BreadthFirstSearch.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BreadthFirstSearch.java
@@ -1,4 +1,25 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BreadthFirstSearch {
+
+    static class Node{
+
+        static String name;
+        static List<Node> children = new ArrayList<>();
+
+        public Node(String name){
+            this.name = name;
+        }
+        public Node addChild(String name){
+            Node child = new Node(name);
+            children.add(child);
+
+            return this;
+        }
+    }
+
+
 }
