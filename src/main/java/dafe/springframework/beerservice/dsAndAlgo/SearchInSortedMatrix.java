a8fc477b54245feb2fe34ab353925ba16ff6cdaf
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-22 22:40:47 -0700

    implement search in sorted matrix WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SearchInSortedMatrix.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SearchInSortedMatrix.java
@@ -1,4 +1,10 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class SearchInSortedMatrix {
+
+    public static int[] searchSortedMatrix(int[][] array, int target){
+
+
+        return new int[]{-1,-1};
+    }
 }
