6e75208c72d662443b3e50da2f3c593497051d80
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-22 23:36:02 -0700

    implement search in sorted matrix done, closes #37

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SearchInSortedMatrix.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SearchInSortedMatrix.java
@@ -1,27 +1,34 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.Arrays;
+
 public class SearchInSortedMatrix {
 
-    public static int[] searchSortedMatrix(int[][] array, int target){
+    public static int[] searchSortedMatrix(int[][] matrix, int target){
+    int row =0, column = matrix[0].length-1;
+        while(row<matrix[0].length && column>= 0){
 
-
+            if(matrix[row][column] > target) column--;
+            else if(matrix[row][column] < target) row++;
+            else return new int[]{row, column};
+        }
         return new int[]{-1,-1};
     }
 
 
 
     public static void main(String[] args) {
-        int target=44;
+        int target=1;
         int[][] matrix = {
                 {1, 4, 7, 12, 15, 1000},
-                {2, 5, 19, 31, 32, 1001},
-                {3, 8, 24, 33, 35, 1002},
-                {40, 41, 42, 44, 45, 1003},
-                {99, 100, 103, 106, 128, 1004}
+        {2, 5, 19, 31, 32, 1001},
+        {3, 8, 24, 33, 35, 1002},
+        {40, 41, 42, 44, 45, 1003},
+        {99, 100, 103, 106, 128, 1004}
         };
 
         int [] result = searchSortedMatrix(matrix,target);
-        System.out.println("This  is the position of the target : " + result);
+        System.out.println("This  is the position of the target : " + Arrays.toString(result));
 
     }
 }
