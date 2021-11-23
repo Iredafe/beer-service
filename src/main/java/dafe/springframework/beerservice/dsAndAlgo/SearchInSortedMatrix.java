8a9a0f0ac2217f6ecf8e32ac9a24282b13aff7cd
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-22 22:43:37 -0700

    implement search in sorted matrix WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SearchInSortedMatrix.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SearchInSortedMatrix.java
@@ -7,4 +7,21 @@ public class SearchInSortedMatrix {
 
         return new int[]{-1,-1};
     }
+
+
+
+    public static void main(String[] args) {
+        int target=44;
+        int[][] matrix = {
+                {1, 4, 7, 12, 15, 1000},
+                {2, 5, 19, 31, 32, 1001},
+                {3, 8, 24, 33, 35, 1002},
+                {40, 41, 42, 44, 45, 1003},
+                {99, 100, 103, 106, 128, 1004}
+        };
+
+        int [] result = searchSortedMatrix(matrix,target);
+        System.out.println("This  is the position of the target : " + result);
+
+    }
 }
