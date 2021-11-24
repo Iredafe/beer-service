bd45fa449278003a40fd407855aaeb3997ac1128
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-23 23:36:17 -0700

    implementing reverse linkedlist algo WIP II- test

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ReverseLinkedListAlgo.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ReverseLinkedListAlgo.java
@@ -1,7 +1,7 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class ReverseLinkedListAlgo {
-
+    static Linkedlist head;
     static class Linkedlist{
         Linkedlist next = null;
         int value;
@@ -22,5 +22,15 @@ public class ReverseLinkedListAlgo {
             currentNode = nextNode;
         }
         return previousNode;
+    }
+
+    public static void main(String[] args) {
+
+        ReverseLinkedListAlgo list = new ReverseLinkedListAlgo();
+        list.head = new ReverseLinkedListAlgo.Linkedlist(85);
+        list.head.next = new ReverseLinkedListAlgo.Linkedlist(15);
+        list.head.next.next = new ReverseLinkedListAlgo.Linkedlist(4);
+        list.head.next.next.next = new ReverseLinkedListAlgo.Linkedlist(20);
+        list.head.next.next.next.next = new ReverseLinkedListAlgo.Linkedlist(5);
     }
 }
