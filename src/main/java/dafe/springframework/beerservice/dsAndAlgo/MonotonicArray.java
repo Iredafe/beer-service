5a293e13eda47be156ab4aa0e1ef83e84e5881bd
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-24 16:47:06 -0700

    monotonic array second done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MonotonicArray.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MonotonicArray.java
@@ -20,12 +20,34 @@ public class MonotonicArray {
         return true;
     }
 
+
+    public static boolean isMonotonicArray2(int [] array){
+        boolean isIncreasing = true;
+        boolean isDecreasing = true;
+
+        for (int index = 0; index< array.length-1; index++){
+            if(array[index] > array[index + 1])
+                isIncreasing = false;
+            if(array[index] < array[index+1])
+                isDecreasing = false;
+        }
+        return isDecreasing||isIncreasing;
+    }
+
     public static void main(String[] args) {
 
         int[] input = {-1, -5, -10, -1100, -1100, -1101, -1102, -9001};
         int[] input1 ={-1, -5, -10, -1100, -900, -1101, -1102, -9001};
+        int [] input2 = {};
 
         System.out.println("This array is monotonic : " + isMonotonicArray(input));
         System.out.println("This array is monotonic 1 : " + isMonotonicArray(input1));
+        System.out.println("This array is monotonic 2 : " + isMonotonicArray(input2));
+
+
+        System.out.println("This array is 2monotonic : " + isMonotonicArray2(input));
+        System.out.println("This array is 2monotonic 1 : " + isMonotonicArray2(input1));
+        System.out.println("This array is 2monotonic 2 : " + isMonotonicArray2(input2));
+
     }
 }
