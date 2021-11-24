f38b1fb94ee622c87f0d9b570a009576ee2e6404
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-24 15:23:44 -0700

    monotonic array WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MonotonicArray.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MonotonicArray.java
@@ -1,4 +1,18 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class MonotonicArray {
+
+    public static boolean isMonotonicArray(int [] array){
+
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] input = {-1, -5, -10, -1100, -1100, -1101, -1102, -9001};
+
+        System.out.println("This array is monotonic : " + isMonotonicArray(input));
+    }
 }
