3137c6d289f986f80d7c3b78aa9d17c5c89111ff
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-23 20:47:23 -0700

    implement doubly linkedlist WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/DoublyLinkedListConstruction.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/DoublyLinkedListConstruction.java
@@ -1,4 +1,14 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class DoublyLinkedListConstruction {
+
+    static class Node{
+        int value;
+        Node prev;
+        Node next;
+
+        public Node(int value){
+            this.value=value;
+        }
+    }
 }
