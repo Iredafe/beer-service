d4c1c6a595efa9a447109bba6f8212b27bc1e90c
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-24 21:52:30 -0700

    phone number mnemonics done close #41

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/PhoneNumberMnemonics.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/PhoneNumberMnemonics.java
@@ -1,4 +1,53 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneNumberMnemonics {
+    static Map<Character, String[]> DIGIT_LETTERS = new HashMap<>();
+    static {
+        DIGIT_LETTERS.put('0', new String[]{"0"});
+        DIGIT_LETTERS.put('1', new String[]{"1"});
+        DIGIT_LETTERS.put('2', new String[]{"a", "b", "c"});
+        DIGIT_LETTERS.put('3', new String[]{"d", "e", "f"});
+        DIGIT_LETTERS.put('4', new String[]{"g", "h", "i"});
+        DIGIT_LETTERS.put('5', new String[]{"j", "k", "l"});
+        DIGIT_LETTERS.put('6', new String[]{"m", "n", "o"});
+        DIGIT_LETTERS.put('7', new String[]{"p", "q", "r", "s"});
+        DIGIT_LETTERS.put('8', new String[]{"t", "u", "v"});
+        DIGIT_LETTERS.put('9', new String[]{"w", "x", "y", "z"});
+    }
+
+    public static ArrayList<String> phoneNumberMnemonics (String phoneNumber){
+
+        String [] currentLetters = new String[phoneNumber.length()];
+        Arrays.fill(currentLetters, "0");
+        ArrayList<String> result = new ArrayList<>();
+        phoneNumberMnemonicsHelper(0, currentLetters, result, phoneNumber);
+        return result;
+    }
+
+    public static void phoneNumberMnemonicsHelper
+            (int index, String[] currentLetters, ArrayList<String> result, String phoneNumber){
+        if(index == phoneNumber.length()){
+            String mnemonic = String.join("", currentLetters);
+            result.add(mnemonic);
+        }else{
+            char digit = phoneNumber.charAt(index);
+            String [] letters = DIGIT_LETTERS.get(digit);
+            for(String letter : letters){
+                currentLetters[index] = letter;
+                phoneNumberMnemonicsHelper(index+1, currentLetters, result, phoneNumber);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String phoneNumber = "1905";
+
+        System.out.println("These are the mnemonics : " + phoneNumberMnemonics(phoneNumber));
+    }
 }
