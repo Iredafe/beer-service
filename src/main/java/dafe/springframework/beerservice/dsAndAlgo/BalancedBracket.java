453cd0e11339e03a8a4318a5cdbbbdb5145b8eb0
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-24 18:02:55 -0700

    balanced brackets WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BalancedBracket.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BalancedBracket.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class BalancedBracket {
+
+    public static boolean balancedBracket(String input) {
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+
+        String input = "([])(){}(())()()";
+        System.out.println("This input is a balanced bracket : " + balancedBracket(input));
+    }
 }
