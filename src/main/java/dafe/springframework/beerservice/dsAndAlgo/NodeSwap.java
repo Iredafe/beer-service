927d7dcabfa92128a3e923776178fbed1a74d623
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-25 22:10:31 -0700

    node swap WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NodeSwap.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NodeSwap.java
@@ -1,4 +1,27 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class NodeSwap {
+
+    static class Node{
+        int value;
+        Node next = null;
+
+    public Node(int value){
+        this.value = value;
+    }
+    }
+
+    public static Node swapNode(Node head){
+
+        Node currentNode = head;
+        if(head !=null || head.next != null){
+            currentNode = head.next;
+            head.next = swapNode(head.next.next);
+            head.next.next = currentNode;
+        }
+        return head;
+    }
+
+
+
 }
