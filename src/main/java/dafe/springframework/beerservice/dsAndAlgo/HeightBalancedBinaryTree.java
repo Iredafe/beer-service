544f3dac5be6d5ae4514e0c970b0f59bf8daf18a
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-25 23:31:27 -0700

    height balaanced binary tree WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/HeightBalancedBinaryTree.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/HeightBalancedBinaryTree.java
@@ -1,4 +1,18 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class HeightBalancedBinaryTree {
+
+    static class BinaryTree{
+        int value; BinaryTree left, right;
+
+        public BinaryTree(int value){
+            this.value = value;
+        }
+    }
+
+
+    public boolean heightBalancedTree(BinaryTree tree){
+
+        return false;
+    }
 }
