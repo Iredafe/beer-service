30b845926eb0fc0ec7e9998a9b9928fe3cc08d1c
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-25 23:43:28 -0700

    height balaanced binary tree done recursively with tests

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/HeightBalancedBinaryTree.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/HeightBalancedBinaryTree.java
@@ -1,17 +1,15 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class HeightBalancedBinaryTree {
-
+    private static BinaryTree root;
     static class BinaryTree{
         int value; BinaryTree left, right;
-
         public BinaryTree(int value){
             this.value = value;
         }
     }
 
     public boolean heightBalancedTree(BinaryTree tree){
-
         return customHeight(tree) != -1;
     }
 
@@ -22,9 +20,24 @@ public class HeightBalancedBinaryTree {
         if(leftHeight == -1) return -1;
         int rightHeight = customHeight(tree.right);
         if(rightHeight == -1) return -1;
-
         if(Math.abs(leftHeight - rightHeight) > 1) return -1;
-
         return Math.max(leftHeight, rightHeight) +1;
+    }
+
+
+    public static void main(String[] args) {
+
+        HeightBalancedBinaryTree tree = new HeightBalancedBinaryTree();
+        tree.root = new BinaryTree(1);
+        tree.root.left = new BinaryTree(2);
+        tree.root.right = new BinaryTree(3);
+        tree.root.left.left = new BinaryTree(4);
+        tree.root.left.right = new BinaryTree(5);
+        tree.root.right.right=new BinaryTree(6);
+        tree.root.left.right.left = new BinaryTree(7);
+        tree.root.left.right.right = new BinaryTree(8);
+
+        System.out.println("Is the tree height balanced?  : " + tree.heightBalancedTree(root));
+
     }
 }
