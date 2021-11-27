b83bd7a790b35c24cfa087c789edf65f84ef4a00
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-27 16:52:48 -0700

    longest peak pseudocode

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LongestPeak.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LongestPeak.java
@@ -1,4 +1,18 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class LongestPeak {
+
+    public int longestPeak(int[]array){
+        //while index is less than array length
+        //check for a peak by comparing 3 consecutive integers with each other
+        //once a peak is found, expand the left and right index to check the length of that peak (current peak)
+        //once you hit a point that is not part of the peak, assign the length to the longest peak variable if it is longer than the current peak
+        //loop again until while condition is fully met
+        // return the longest peak
+
+
+
+
+        return -1;
+    }
 }
