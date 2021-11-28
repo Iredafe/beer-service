8874bdce77a888f0d12e595ea4c339d2d0c598dd
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-27 17:10:42 -0700

    longest peak done and tested

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LongestPeak.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LongestPeak.java
@@ -2,7 +2,7 @@ package dafe.springframework.beerservice.dsAndAlgo;
 
 public class LongestPeak {
 
-    public int longestPeak(int[]array){
+    public static int longestPeak(int[]array){
         //while index is less than array length
         //check for a peak by comparing 3 consecutive integers with each other
         //once a peak is found, expand the left and right index to check the length of that peak (current peak)
@@ -11,8 +11,34 @@ public class LongestPeak {
         // return the longest peak
 
 
+        int longestPeak= 0;
+        int index = 1;
+        while( index < array.length-1){
+            boolean isPeak = array[index-1] < array[index] && array[index] > array[index+1];
+            if(!isPeak){
+                index++;
+                continue;
+            }
+            int leftIndex = index-2;
+            while (leftIndex >= 0 && array[leftIndex] < array[leftIndex+1]){
+                leftIndex--;
+            }
+            int rightIndex = index+2;
+            while(rightIndex < array.length && array[rightIndex] < array[rightIndex-1]){
+                rightIndex++;
+            }
+
+            int currentPeakLength = rightIndex - leftIndex -1 ;
+            if(currentPeakLength > longestPeak) longestPeak = currentPeakLength;
+            index = rightIndex;
+        }
+        return longestPeak;
+    }
 
 
-        return -1;
+    public static void main(String[] args) {
+        int [] input ={1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3};
+
+        System.out.println("The longest peak is : " + longestPeak(input));
     }
 }
