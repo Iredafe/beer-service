7f7148102a91b68401fe0840b6da1047dac61a98
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-27 20:46:02 -0700

    shifted binary search WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ShiftedBinarySearch.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ShiftedBinarySearch.java
@@ -1,8 +1,16 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class ShiftedBinarySearch {
-    public static int shiftedBinarySearch(int [] array){
+    public static int shiftedBinarySearch(int [] array, int target){
 
         return 0;
+    }
+
+
+    public static void main(String[] args) {
+        int[] input ={45, 61, 71, 72, 73, 0, 1, 21, 33, 37};
+        int searchTerm = 33;
+
+        System.out.println("This is the index of the target integer : " + shiftedBinarySearch(input,searchTerm));
     }
 }
