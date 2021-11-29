e907791137bf2b59142949f5c83fffbcb06959a8
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-28 21:21:53 -0700

    longest substring without duplicate WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LongestSubstringWithoutDuplicates.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LongestSubstringWithoutDuplicates.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class LongestSubstringWithoutDuplicates {
+
+    public  static String longestSubstring(String input){
+
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+        String string =  "clementisacap";
+
+        System.out.println("The longest substring without duplicate is : " + longestSubstring(string));
+    }
 }
