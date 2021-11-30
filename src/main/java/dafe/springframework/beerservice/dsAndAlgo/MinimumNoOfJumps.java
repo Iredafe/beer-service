f2233240276efb215c30b19322b63461ef565aed
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-29 20:15:13 -0700

    minimum no of jumps WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinimumNoOfJumps.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinimumNoOfJumps.java
@@ -1,4 +1,15 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class MinimumNoOfJumps {
+
+    public static int minimumNoOfJumps(int [] array){
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int [] input ={3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3};
+
+        System.out.println("This is the minimum number of jumps : " + minimumNoOfJumps(input));
+    }
 }
