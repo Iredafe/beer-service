68e48ed931ba3d6d1aaf2f4e4488c2cbcd1f95c0
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-30 17:27:08 -0700

    spiral traverse WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SpiralTraverse.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SpiralTraverse.java
@@ -1,4 +1,19 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class SpiralTraverse {
+
+    public static int[] spiralTraverse(int[][] array){
+
+        return new int[]{};
+    }
+
+
+    public static void main(String[] args) {
+        int [][] input = {{1, 2, 3, 4},
+        {12, 13, 14, 5},
+        {11, 16, 15, 6},
+        {10, 9, 8, 7}};
+
+        System.out.println("This is the result of spiral traverse : " + spiralTraverse(input));
+    }
 }
