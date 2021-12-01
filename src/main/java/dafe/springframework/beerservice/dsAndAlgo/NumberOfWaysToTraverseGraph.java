5516f5e7c630bdaefee18d24613725bb7806135c
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-30 23:49:24 -0700

    number of ways to traverse a graph done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NumberOfWaysToTraverseGraph.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NumberOfWaysToTraverseGraph.java
@@ -1,7 +1,28 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class NumberOfWaysToTraverseGraph {
-    public static int uniquePaths(int height, int width){
-        return -1;
+    public static int uniquePaths(int width,int height){
+
+        int [][] numberOfWays = new int[height+1][width+1];
+        for(int widthIndex =1; widthIndex < width+1; widthIndex++){
+            for(int heightIndex = 1; heightIndex<height+1; heightIndex++){
+                if(widthIndex ==1 || heightIndex == 1){
+                    numberOfWays[heightIndex][widthIndex] = 1;
+                }else{
+                    int waysLeft = numberOfWays[heightIndex][widthIndex-1];
+                    int waysUp = numberOfWays[heightIndex-1][widthIndex];
+                    numberOfWays[heightIndex][widthIndex] = waysLeft+waysUp;
+                }
+            }
+        }
+        return numberOfWays[height][width];
+    }
+
+
+    public static void main(String[] args) {
+        int width = 4;
+        int height= 3;
+
+        System.out.println("These are the unique paths : " + uniquePaths(height, width) );
     }
 }
