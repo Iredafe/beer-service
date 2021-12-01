0aaa4317dbd7d9cfabc4d8d24a895cb89eb8fdbc
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-11-30 19:20:00 -0700

    spiral traverse done iteratively

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SpiralTraverse.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SpiralTraverse.java
@@ -1,10 +1,38 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpiralTraverse {
 
-    public static int[] spiralTraverse(int[][] array){
+    public static List<Integer> spiralTraverse(int[][] array){
+        if(array.length==0) return new ArrayList<Integer>();
 
-        return new int[]{};
+        var result = new ArrayList<Integer>();
+        var startRow = 0; var startColumn=0; var endRow= array.length-1; var endColumn = array[0].length-1;
+
+        while (startRow <= endRow  && startColumn <= endColumn){
+            for (int column=startColumn; column<=endColumn; column++){
+                result.add(array[startColumn][column]);
+            }
+            for(int row=startRow+1; row<=endRow; row++){
+                result.add(array[row][endRow]);
+            }
+            for(int column=endColumn-1; column>=startColumn; column--){
+                if(startRow == endRow) break;
+                result.add(array[endRow][column]);
+            }
+            for (int row=endRow-1; row>startRow; row--){
+                if(startColumn==endColumn) break;;
+                result.add(array[row][startColumn]);
+            }
+
+            startRow++;
+            startColumn++;
+            endRow--;
+            endColumn--;
+        }
+        return result;
     }
 
 
