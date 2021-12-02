f4a2ff687f373b97cb713e700445912d450555d9
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-01 18:42:37 -0700

    tandem bicycle WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/TandemBicycle.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/TandemBicycle.java
@@ -1,4 +1,30 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class TandemBicycle {
+
+    public static int tandemBicycle(int [] redShirtTeam, int [] blueShirtTeam, boolean fastest){
+
+        if(!fastest){
+            reverseArray(redShirtTeam);
+        }
+        return 1;
+    }
+
+    public static void reverseArray(int[] array){
+        int left = 0; int right = array.length-1;
+
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+        right--;
+        left++;
+    }
+
+
+    public static void main(String[] args) {
+
+        int redShirtTeam [] = {5, 5, 3, 9, 2};
+        int blueShirtTeam [] ={3, 6, 7, 2, 1};
+        boolean fastest = true;
+    }
 }
