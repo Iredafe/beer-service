bc6ffe7305c4525e9b3510dc31fa51c7c47500c9
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-01 20:27:43 -0700

    array of products first solution done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ArrayOfProducts.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ArrayOfProducts.java
@@ -1,16 +1,30 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.Arrays;
+
 public class ArrayOfProducts {
 
     public static int[] arrayOfProducts(int [] array){
 
-        return new int[]{};
+        int product [] = new int[array.length];
+
+        for(int firstPointer = 0; firstPointer< array.length; firstPointer++){
+            int runningProduct = 1;
+            for(int secondPointer = 0; secondPointer<array.length; secondPointer++){
+                if(firstPointer!=secondPointer){
+                    runningProduct *= array[secondPointer];
+                }
+                product[firstPointer] = runningProduct;
+            }
+        }
+        return product;
     }
 
     public static void main(String[] args) {
 
         int [] input = {5, 1, 4, 2}; //1,2,4,5
                                     //10,20,40,8
-        System.out.println("This is the result of array of products : " + arrayOfProducts(input));
+        int [] result =arrayOfProducts(input);
+        System.out.println("This is the result of array of products : " + Arrays.toString(result));
     }
 }
