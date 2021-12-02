162dee16c1ad87be27391132bed227c5ebc66cb2
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-01 19:56:50 -0700

    array of products WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ArrayOfProducts.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ArrayOfProducts.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class ArrayOfProducts {
+
+    public static int[] arrayOfProducts(int [] array){
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+
+        int [] input = {5, 1, 4, 2}; //1,2,4,5
+                                    //10,20,40,8
+        System.out.println("This is the result of array of products : " + arrayOfProducts(input));
+    }
 }
