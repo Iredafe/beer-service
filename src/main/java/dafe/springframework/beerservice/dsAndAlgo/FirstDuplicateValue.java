9234d13f44e791e99a021567cab5cacb3f43fba0
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-02 23:15:16 -0700

    first duplicate value WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FirstDuplicateValue.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FirstDuplicateValue.java
@@ -8,7 +8,7 @@ public class FirstDuplicateValue {
         for(int i=0; i<array.length; i++){
             for(int j=1; j<array.length; j++){
                 if(array[i]==array[j]){
-                    result = j;
+                    result = array[j];
                     return result;
                 }
             }
@@ -17,9 +17,24 @@ public class FirstDuplicateValue {
         return result;
     }
 
-    public static void main(String[] args) {
-        int [] input ={2, 1, 5, 2, 3, 3, 4};
+    public static  int firstDuplicateBrute(int [] array){
+        int minIndex = array.length;
+        for(int i =0; i<array.length; i++){
+            int value = array[i];
+            for(int j=i+1; j<array.length; j++){
+                int valueToCompare = array[j];
+                if(value==valueToCompare){
+                    minIndex = Math.min(j, minIndex);
+                }
+            }
+        }
+        if(minIndex==array.length) return -1;
+        return array[minIndex];
+    }
 
-        System.out.println("This is the index of duplicate : " + firstDuplicateValue(input));
+    public static void main(String[] args) {
+        int [] input ={2, 1, 5, 3, 3, 2, 4};
+
+        System.out.println("This is the index of duplicate : " + firstDuplicateBrute(input));
     }
 }
