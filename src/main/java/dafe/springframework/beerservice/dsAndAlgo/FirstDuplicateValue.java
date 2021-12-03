9e195f7bd58a9f6b83a8c9b77e7e7cb48e6ef0bf
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-02 23:07:25 -0700

    first duplicate value WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FirstDuplicateValue.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FirstDuplicateValue.java
@@ -1,4 +1,25 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class FirstDuplicateValue {
+
+    public static int firstDuplicateValue(int[] array) {
+        // Write your code here.
+        int result = 0;
+        for(int i=0; i<array.length; i++){
+            for(int j=1; j<array.length; j++){
+                if(array[i]==array[j]){
+                    result = j;
+                    return result;
+                }
+            }
+            result=-1;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int [] input ={2, 1, 5, 2, 3, 3, 4};
+
+        System.out.println("This is the index of duplicate : " + firstDuplicateValue(input));
+    }
 }
