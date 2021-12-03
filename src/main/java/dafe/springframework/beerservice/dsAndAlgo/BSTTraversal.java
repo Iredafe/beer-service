44fd8de24f62e6a32a045f5dc99d35ecaf7459ed
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-02 22:02:25 -0700

    BST traversal WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BSTTraversal.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BSTTraversal.java
@@ -1,4 +1,21 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BSTTraversal {
+
+    static class BST{
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value){
+            this.value = value;
+        }
+    }
+    public static List<Integer> inOrderTraverse(BST tree, List<Integer> array){
+
+        return new ArrayList<Integer>();
+    }
 }
