97f4958ac6adc778e11ac0c95a0452ca408e973a
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-04 13:18:55 -0700

    find successor WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FindSuccessor.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FindSuccessor.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class FindSuccessor {
+
+    static class BinaryTree{
+        int value;
+        BinaryTree parent=null;
+        BinaryTree left=null;
+        BinaryTree right=null;
+        public BinaryTree(int value){
+            this.value = value;
+        }
+    }
+
+
 }
