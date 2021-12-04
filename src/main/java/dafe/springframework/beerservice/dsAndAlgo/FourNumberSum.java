d5f5777f0cfd364571774bd36cc65e2081dfd00d
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-03 22:43:11 -0700

    four number sum WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FourNumberSum.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FourNumberSum.java
@@ -1,4 +1,21 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FourNumberSum {
+
+    public static List<Integer>  fourNumberSum(int [] array, int target){
+
+
+        return new ArrayList<>();
+    }
+
+
+    public static void main(String[] args) {
+
+        int [] array = {7, 6, 4, -1, 1, 2};
+        int target = 16;
+        System.out.println("These are the four number sums : " + fourNumberSum(array, target));
+    }
 }
