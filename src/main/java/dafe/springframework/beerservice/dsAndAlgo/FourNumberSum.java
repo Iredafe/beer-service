df81869ceb59fc698a813751b07b6793670c0edb
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-03 22:54:06 -0700

    four number sum WIP II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FourNumberSum.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FourNumberSum.java
@@ -6,11 +6,19 @@ import java.util.List;
 import java.util.Map;
 
 public class FourNumberSum {
-
-    public static List<Integer[]>  fourNumberSum(int [] array, int target){
+        //create a variable to store the quadruplets and another one to keep track of the pairs
+        // loop through the array, and create a current sum starting from the second index
+        // find the difference between the current sum and target sum
+        //check the hashmap to see it the difference exists
+        //if the difference exists, add the pair to the quadruplet
+        //if it does not exist, do nothing
+        //do another for loop, create another current sum variable, check if the sum is in the hashmap
+        //if it exists in the hashmap, append/add to the hashmap, if it doesn't, set it into the hashmap
+    public static List<Integer[]>  fourNumberSum(int [] array, int targetSum){
         Map<Integer, List<Integer[]>> allPairsSum = new HashMap<>();
         List<Integer[]> quadruplets = new ArrayList<>();
 
+        //
         return quadruplets;
     }
 
