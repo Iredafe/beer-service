45cd682a0a5d3ed498a65570f82821b8ddc9494b
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-04 19:01:40 -0700

    kadane's algorithm WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/KadaneAlgorithm.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/KadaneAlgorithm.java
@@ -1,4 +1,18 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class KadaneAlgorithm {
+
+    public static  int kadane(int[] array){
+
+
+        return 1;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] input = {3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4};
+
+        System.out.println("This is the maximum number : " + kadane(input));
+    }
 }
