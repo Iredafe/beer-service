febecd16b28f4a2c10d3fc66d608b5683e8d6a89
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-05 16:00:05 -0700

    permutation WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/Permutation.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/Permutation.java
@@ -1,4 +1,21 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Permutation {
+
+    public static List<List<Integer>> permutation(List<Integer> array){
+
+        return new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> array = new ArrayList<Integer>();
+        array.add(1);
+        array.add(2);
+        array.add(3);
+
+        System.out.println("These are the permutations : " + permutation(array));
+    }
 }
