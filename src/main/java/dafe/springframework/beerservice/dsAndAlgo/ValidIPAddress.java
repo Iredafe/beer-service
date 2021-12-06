5533273adcf54761b09023f77690ee2b653e5f3c
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-05 20:34:31 -0700

    valid IP address WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidIPAddress.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidIPAddress.java
@@ -1,6 +1,29 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+
+import java.util.ArrayList;
+
 public class ValidIPAddress {
 
+public static ArrayList<String> validIPAddress(String input){
 
+
+    return new ArrayList<>();
+}
+
+
+
+public static boolean isValidPart(String inputString){
+    int stringAsInt = Integer.parseInt(inputString);
+    if(stringAsInt > 255){
+        return false;
+    }
+    return true;
+}
+
+
+public String join(String [] inputArrayStrings){
+
+    return "";
+}
 }
