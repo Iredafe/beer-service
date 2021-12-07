37f62057fea5a285f5191b42c435b8781fdf6e68
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-07 00:07:14 -0700

    find kth largest value in bst WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/KthLargestValueInBST.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/KthLargestValueInBST.java
@@ -1,4 +1,29 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+
 public class KthLargestValueInBST {
+
+    static class BST{
+        int value;
+        BST left;
+        BST right;
+
+        public BST(int value){
+            this.value = value;
+        }
+    }
+
+    public static int findKthLargestValueInBst(BST tree, int k){
+        ArrayList<Integer> sortedArray = new ArrayList<>();
+        inOrderTraversal(tree, sortedArray);
+        return sortedArray.get(sortedArray.size()-k);
+    }
+
+    public static void inOrderTraversal(BST tree, ArrayList<Integer> array){
+        if(tree == null) return;
+        inOrderTraversal(tree.left, array);
+        array.add(tree.value);
+        inOrderTraversal(tree.right, array);
+    }
 }
