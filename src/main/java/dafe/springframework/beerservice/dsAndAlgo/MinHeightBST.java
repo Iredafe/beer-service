4864f300732e137c52182b431d0c39304dd8ab91
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-06 21:53:46 -0700

    min height bst WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinHeightBST.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinHeightBST.java
@@ -1,4 +1,27 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.List;
+
 public class MinHeightBST {
+
+    public static BST minHeightBST(List<Integer> array){
+
+        return minHeightBSTHelper(array, null, 0, array.size()-1);
+    }
+
+    public static BST minHeightBSTHelper(List<Integer> array, BST bst, int startIdx, int endIdx){
+
+
+        return bst;
+    }
+
+    static class BST{
+        int value;
+        BST left;
+        BST right;
+
+        public BST(int value){
+            this.value=value;
+        }
+    }
 }
