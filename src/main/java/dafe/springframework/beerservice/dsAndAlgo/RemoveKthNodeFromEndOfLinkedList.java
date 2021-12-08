13b51963957045e0f6ef6c3b80e6c73724dda1c7
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-07 21:46:52 -0700

    remove kth node WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/RemoveKthNodeFromEndOfLinkedList.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/RemoveKthNodeFromEndOfLinkedList.java
@@ -1,4 +1,15 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class RemoveKthNodeFromEndOfLinkedList {
+
+    static class Node{
+        int value;
+        Node next =null;
+        public Node(int value){
+            this.value=value;
+        }
+    }
+    public static void removeKthNode(Node node, int k){
+
+    }
 }
