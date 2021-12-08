b14b273e665ca9a4f16b11f9dc3e15e89d184a8f
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-07 20:05:31 -0700

    powerset WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/Powerset.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/Powerset.java
@@ -1,4 +1,27 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Powerset {
+    public static List<List<Integer>> powerset(List<Integer> array){
+        return helper(array, array.size()-1);
+    }
+
+    public static List<List<Integer>> helper(List<Integer> array, int index){
+        if(index<0){
+            List<List<Integer>> emptySet = new ArrayList<>();
+            emptySet.add(new ArrayList<>());
+            return emptySet;
+        }
+        int element = array.get(index);
+        List<List<Integer>> subsets = helper(array, index-1);
+        int length = subsets.size();
+        for (int i = 0; i<length; i++){
+            List<Integer> currentSubset = new ArrayList<>(subsets.get(i));
+            currentSubset.add(element);
+            subsets.add(currentSubset);
+        }
+        return subsets;
+    }
 }
