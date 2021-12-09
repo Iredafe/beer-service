f70da6a1606876383fa64733917fe89638bf1afa
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-08 20:59:14 -0700

    sum linkedlist WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SumLinkedList.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SumLinkedList.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class SumLinkedList {
+
+    static class Node {
+        int value;
+        Node next;
+        public Node(int value){
+            this.value = value;
+            this.next = null;
+        }
+    }
 }
