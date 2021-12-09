5ed89129108c11fe648ccf65c9dd5fd43acda7a5
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-08 21:09:22 -0700

    sum linkedlist WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SumLinkedList.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SumLinkedList.java
@@ -10,4 +10,30 @@ public class SumLinkedList {
             this.next = null;
         }
     }
+
+    public static Node sumLinkedList(Node firstNode, Node secondNode){
+
+        Node dummy = new Node(0);
+        Node currentNode = dummy;
+        int carry = 0;
+
+        Node nodeOne = firstNode;
+        Node nodeTwo = secondNode;
+
+        while (nodeOne!=null || nodeTwo!=null || carry!=0){
+            int valueOne = (nodeOne!=null) ? nodeOne.value:0;
+            int valueTwo = (nodeTwo!=null) ? nodeTwo.value:0;
+            int sumOfValues = valueOne + valueTwo;
+
+            int newValue = sumOfValues%10;
+            Node newNode = new Node(newValue);
+            currentNode = newNode;
+
+            carry= sumOfValues/10;
+            nodeOne = (nodeOne!=null) ? nodeOne.next:null;
+            nodeTwo = (nodeTwo!=null) ? nodeTwo.next:null;
+        }
+        return dummy.next;
+
+    }
 }
