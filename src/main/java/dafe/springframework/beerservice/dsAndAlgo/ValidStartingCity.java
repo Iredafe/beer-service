8a3490e3fbbca57994dfd226f293dafbbc0fad43
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-08 21:56:50 -0700

    valid starting city WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidStartingCity.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidStartingCity.java
@@ -1,4 +1,9 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class ValidStartingCity {
+
+    public static int validStartingCity(int [] distance, int [] duel, int [] mpg){
+
+        return 1;
+    }
 }
