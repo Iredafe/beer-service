2d20ba623d35229a15a1eae77de3e77bbdf62b39
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-09 22:19:24 -0700

    sub array sort done - bugfixed

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SubArraySort.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SubArraySort.java
@@ -32,7 +32,11 @@ public class SubArraySort {
      }
 
     public static void main(String[] args) {
-        int[] input = {1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19};
+        int[] input = {1};
+        int[] input2 = {1, 2,3, 4};
+
+
         System.out.println("This is the sub array : " + Arrays.toString(subArraySort(input)));
+        System.out.println("This is the sub array : " + Arrays.toString(subArraySort(input2)));
     }
 }
