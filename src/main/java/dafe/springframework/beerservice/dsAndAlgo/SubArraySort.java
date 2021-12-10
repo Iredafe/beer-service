f880038ff38ec08b76331536b64cd2bea96af04c
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-09 19:28:05 -0700

    sub array sort WIP

---
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SubArraySort.java
@@ -0,0 +1,16 @@
+package dafe.springframework.beerservice.dsAndAlgo;
+
+public class SubArraySort {
+
+     public static int[] subArraySort(int [] array){
+
+         return new int[]{};
+     }
+
+
+    public static void main(String[] args) {
+        int[] input = {1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19};
+
+        System.out.println("This is the sub array : " + subArraySort(input));
+    }
+}
