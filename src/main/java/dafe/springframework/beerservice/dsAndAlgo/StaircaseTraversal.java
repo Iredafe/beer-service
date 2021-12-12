ea1a08322b7852e334d7ddc3e1d7502cc2175e88
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-11 21:56:00 -0700

    staircase traversal first solution given

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/StaircaseTraversal.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/StaircaseTraversal.java
@@ -2,16 +2,22 @@ package dafe.springframework.beerservice.dsAndAlgo;
 
 public class StaircaseTraversal {
 
-    public static void main(String[] args) {
-
-        int height = 4;
-        int maxSteps = 2;
-
-        System.out.println("This is the maximum number of steps : " + staircaseTraversal(height, maxSteps));
+    private static int staircaseTraversal(int height, int maxSteps) {
+        return staircaseTraversalHelper(height, maxSteps);
     }
 
-    private static int staircaseTraversal(int height, int maxSteps) {
+    private static  int staircaseTraversalHelper(int height, int maxSteps){
+        int numberOfWays = 0;
+        if(height <= 1) return 1;
 
-        return 1;
+        for(int step=1; step < Math.min(height, maxSteps)+1; step++){
+            numberOfWays += staircaseTraversalHelper(height-step, maxSteps);
+        }
+        return numberOfWays;
+    }
+    public static void main(String[] args) {
+        int height = 4;
+        int maxSteps = 2;
+        System.out.println("This is the maximum number of steps : " + staircaseTraversal(height, maxSteps));
     }
 }
