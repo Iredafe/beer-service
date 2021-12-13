250c92b5a2fbfd2710096801781eb6b50ce5bd65
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-12 23:29:14 -0700

    quick sort WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/QuickSort.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/QuickSort.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class QuickSort {
+
+    public static int[] quickSort(int [] array) {
+
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int [] input ={8, 5, 2, 9, 5, 6, 3};
+
+        System.out.println("This is the quick sort result : " + quickSort(input));
+    }
 }
