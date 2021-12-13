a005fc7919295491972390c8927acf9c93cec4de
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-12 23:49:14 -0700

    quick sort WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/QuickSort.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/QuickSort.java
@@ -3,9 +3,33 @@ package dafe.springframework.beerservice.dsAndAlgo;
 public class QuickSort {
 
     public static int[] quickSort(int [] array) {
+        quickSortHelper(array, 0, array.length-1);
+        return array;
+    }
 
-
+    public static void quickSortHelper(int [] array, int startIdx, int endIdx){
+        int pivotIdx = startIdx;
+        int leftIdx = startIdx+1;
+        int rightIdx = endIdx;
+        if(startIdx >= endIdx) return ;
+     while (leftIdx < rightIdx){
+       if(array[leftIdx] > array[pivotIdx] && array[rightIdx] < array[pivotIdx]){
+             swap(array, leftIdx, rightIdx);
+         }
+         if(array[leftIdx] <= array[pivotIdx]){
+             leftIdx++;
+         }
+         if(array[rightIdx] >= array[pivotIdx]){
+             rightIdx--;
+         }
+     }
         return new int[]{};
+    }
+
+    public static void swap(int [] array, int i, int j){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 
     public static void main(String[] args) {
