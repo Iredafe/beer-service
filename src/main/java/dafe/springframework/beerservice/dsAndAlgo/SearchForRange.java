840393c3a02ba8ed58f53cee168b40cfdf721525
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-14 14:00:43 -0700

    search for range done and tested, closes #70

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SearchForRange.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SearchForRange.java
@@ -1,5 +1,7 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.Arrays;
+
 public class SearchForRange {
     public static int[] searchRange(int[] array, int target){
 
@@ -11,13 +13,11 @@ public class SearchForRange {
 
     public static void searchRangeHelper(int[] array, int target, int left, int right, int [] result, boolean goLeft){
         if(left > right) return;
-
         int mid = (left+right)/2;
-
         if(array[mid] < target){
-            searchRangeHelper(array, target, left, mid-1, result, goLeft);
+            searchRangeHelper(array, target, mid+1, right , result, goLeft);
         }else if (array[mid] > target){
-            searchRangeHelper(array,target,mid+1, right, result, goLeft);
+            searchRangeHelper(array,target,left, mid-1, result, goLeft);
         }else{
             if(goLeft){
                 if (mid==0 || array[mid-1] != target){
@@ -28,9 +28,18 @@ public class SearchForRange {
             }else{
                 if(mid== array.length-1 || array[mid+1] != target){
                     result[1] = mid;
+                }else{
+                    searchRangeHelper(array,target, mid+1, right, result,goLeft);
                 }
             }
         }
 
+    }
+
+    public static void main(String[] args) {
+        int [] array = {0, 1, 21, 33, 45, 45, 45, 45, 45, 45, 61, 71, 73};
+        int target = 45;
+
+        System.out.println("This is  the search range : " + Arrays.toString(searchRange(array,target)));
     }
 }
