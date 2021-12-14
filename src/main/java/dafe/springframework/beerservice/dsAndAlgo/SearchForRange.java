9fec1e061b74c13bfb798edb5da35534c8eea8c2
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-14 12:53:52 -0700

    search for range WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SearchForRange.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SearchForRange.java
@@ -1,4 +1,15 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class SearchForRange {
+    public static int[] searchRange(int[] array, int target){
+
+        int result[] = {-1,-1};
+        searchRangeHelper(array, target, 0, array.length-1, result);
+        return result;
+    }
+
+    public static void searchRangeHelper(int[] array, int target, int left, int right, int [] result){
+
+
+    }
 }
