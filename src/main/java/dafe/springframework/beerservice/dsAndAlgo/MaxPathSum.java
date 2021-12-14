18a71f87dc899ac2a2d5990f8816af832f17840f
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-13 22:27:51 -0700

    max path sum WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MaxPathSum.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MaxPathSum.java
@@ -16,5 +16,13 @@ public class MaxPathSum {
     }
     public static void main(String[] args) {
 
+        MaxPathSum tree = new MaxPathSum();
+        tree.root = new BinaryTree(1);
+        tree.root.left = new BinaryTree(2);
+        tree.root.right = new BinaryTree(3);
+        tree.root.left.left = new BinaryTree(4);
+        tree.root.left.right = new BinaryTree(5);
+        tree.root.right.left = new BinaryTree(6);
+        tree.root.right.right=new BinaryTree(7);
     }
 }
