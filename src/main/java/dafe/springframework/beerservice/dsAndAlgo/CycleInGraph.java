843604dc825035ceedb02e5c714ab205bd43ed50
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-14 19:41:20 -0700

    cycle in graph done I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/CycleInGraph.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/CycleInGraph.java
@@ -15,7 +15,6 @@ public class CycleInGraph {
             if(visited[node]){
                 continue;
             }
-
             boolean containsCycle = isNodeInCycle(node, edges, visited, currentlyInStack);
             if(containsCycle) return true;
         }
@@ -25,7 +24,6 @@ public class CycleInGraph {
     public static boolean isNodeInCycle(int node, int [][]edges, boolean visited[], boolean []currentlyInStack){
         visited[node]=true;
         currentlyInStack[node] = true;
-
         boolean containsCycle = false;
         int [] neighbors = edges[node];
         for(int neighbor : neighbors){
@@ -40,10 +38,40 @@ public class CycleInGraph {
         currentlyInStack[node] = false;
       return false;
     }
+    public static int WHITE =0;
+    public static int GREY =1;
+    public static int BLACK =3;
+
+    public static boolean isGraphCycle(int [][]edges){
+        int numberOfNodes = edges.length;
+        int colors[] = new int[numberOfNodes];
+        Arrays.fill(colors, WHITE);
+        for(int node = 0; node < numberOfNodes; node++){
+            if(colors[node] != WHITE) continue;
+        boolean containsCycle = traverseAndColorNodes(node, edges, colors);
+        if(containsCycle) return true;
+        }
+        return false;
+    }
+
+    public static boolean traverseAndColorNodes(int node, int[][] edges, int colors[]){
+        colors[node] = GREY;
+        int [] neighbors = edges[node];
+        for(int neighbor : neighbors){
+            int neighborColor = colors[neighbor];
+            if(neighborColor == GREY) return true;
+            if(neighborColor == BLACK) continue;
+            boolean containsCycle = traverseAndColorNodes(neighbor, edges, colors);
+            if(containsCycle) return true;
+        }
+        colors[node] = BLACK;
+        return false;
+    }
 
     public static void main(String[] args) {
 
         int [][] edges = {{1,3}, {2,3,4}, {0}, {}, {2,5}, {}};
         System.out.println("Is there a cycle in the graph? : " + cycleInGraph(edges));
+        System.out.println("Is there a cycle in the graph? : " + isGraphCycle(edges));
     }
 }
