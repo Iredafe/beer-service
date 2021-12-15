006db0858e2b1940bf9658be431235003adb544e
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-15 15:13:25 -0700

    sort stack WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SortStack.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SortStack.java
@@ -1,4 +1,20 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SortStack {
+
+    public static List<Integer> sortStack(List<Integer> stack){
+
+        return new ArrayList<>();
+    }
+
+
+    public static void main(String[] args) {
+         List<Integer> stack = new ArrayList<>(Arrays.asList(-5, 2, -2, 4, 3, 1));
+
+        System.out.println("This is the sorted stack : " + sortStack(stack));
+    }
 }
