81bb248ca663b7a02591883eaefa6c5fd11c1e5d
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-14 22:21:46 -0700

    max path sum practice II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MaxPathSumPractice.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MaxPathSumPractice.java
@@ -4,17 +4,34 @@ import java.util.List;
 
 public class MaxPathSumPractice {
 
+    static BinaryTree root;
     static class BinaryTree{
+        BinaryTree left;
+        BinaryTree right;
         int value;
 
         public BinaryTree(int value){
             this.value=value;
-            BinaryTree left=null;
-            BinaryTree right=null;
         }
     }
 
-    public static List<Integer> maxPathSum(BinaryTree tree){
+    public static List<Integer> maxPathSum(BinaryTree tree) {
 
     }
+
+    public static void main(String[] args) {
+
+        MaxPathSumPractice tree = new MaxPathSumPractice();
+        tree.root = new BinaryTree(1);
+        tree.root.left = new BinaryTree(2);
+        tree.root.right = new BinaryTree(3);
+        tree.root.left.left = new BinaryTree(4);
+        tree.root.left.right = new BinaryTree(5);
+        tree.root.right.left = new BinaryTree(6);
+        tree.root.right.right=new BinaryTree(7);
+
+        System.out.println("This is the max path sum : " + tree.maxPathSum(root));
+    }
+
+
 }
