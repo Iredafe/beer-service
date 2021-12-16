8934cf1945b2eeaffc9e8edede1ac3472dddcf68
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-16 11:08:50 -0700

    next greater element WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NextGreaterElement.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NextGreaterElement.java
@@ -1,4 +1,23 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.Arrays;
+
 public class NextGreaterElement {
+
+    public static int[] nextGreaterElement(int [] array){
+
+        int [] result = new int[array.length];
+        Arrays.fill(result, -1);
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        int input [] = {2, 5, -3, -4, 6, 7, 2};
+
+        System.out.println
+                ("This is the result of nex greater element function : "
+                        + nextGreaterElement(input));
+    }
 }
