233643a214d6e56e97c378113e9e85cb1f5ede71
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-15 17:17:54 -0700

    levenshtein distance WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LevenshteinDistance.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LevenshteinDistance.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class LevenshteinDistance {
+
+    public static int levDistance(String str1, String str2){
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        String str1= "abc";
+        String str2 = "yabd";
+
+        System.out.println("This is the number of edits : " + levDistance(str1, str2));
+    }
 }
