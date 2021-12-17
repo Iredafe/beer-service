0310ace3b012c74bc58ff661de317da6f3fc9800
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-16 22:12:40 -0700

    sunset view WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SunsetViews.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SunsetViews.java
@@ -1,4 +1,23 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SunsetViews {
+
+    public static ArrayList<Integer> sunsetView(int[] buildings, String direction){
+
+        ArrayList<Integer> stack = new ArrayList<>();
+
+
+        return stack;
+    }
+
+    public static void main(String[] args) {
+        int [] buildings = {3, 5, 4, 4, 3, 1, 3, 2};
+        String direction1 ="EAST";
+        String direction2 = "WEST";
+
+        System.out.println("These are the buildings : " + sunsetView(buildings, direction1));
+    }
 }
