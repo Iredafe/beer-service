83e17715d0a65bc0ce6d86e0f2c82f61b734f004
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-18 00:36:43 -0700

    largest rectangle under skyline WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LargestRectangleUnderSkyLine.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LargestRectangleUnderSkyLine.java
@@ -1,4 +1,20 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class LargestRectangleUnderSkyLine {
+
+
+    public static int largestRectangle(ArrayList<Integer> buildings){
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> buildings = new ArrayList<>(Arrays.asList(1, 3, 3, 2, 4, 1, 5, 3, 2));
+
+        System.out.println("This is the largest rectangle : " + largestRectangle(buildings));
+    }
 }
