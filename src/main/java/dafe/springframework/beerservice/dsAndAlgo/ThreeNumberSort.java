79e59c1e0781c3be21d7a846704cb56cb6e3cf94
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-17 19:10:11 -0700

    three number sort done, closes #76

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ThreeNumberSort.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ThreeNumberSort.java
@@ -1,10 +1,38 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.Arrays;
+
 public class ThreeNumberSort {
 
     public static int[] threeNumberSort(int [] array, int [] order){
+        int firstOrderValue = order[0];
+        int thirdOrderValue = order[2];
 
-        return new int[]{};
+        int firstIdx = 0;
+        int thirdIdx = array.length-1;
+
+        for(int index = 0; index < array.length; index++){
+            int value = array[index];
+            if(value==firstOrderValue){
+                swap(firstIdx, index, array);
+                firstIdx++;
+            }
+        }
+
+        for(int index = array.length-1; index >=0; index--){
+            int value = array[index];
+            if(value == thirdOrderValue){
+                swap(thirdIdx, index, array);
+                thirdIdx--;
+            }
+        }
+        return array;
+    }
+
+    public static void swap(int i, int j, int [] array){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 
     public static void main(String[] args) {
@@ -12,6 +40,6 @@ public class ThreeNumberSort {
         int input[] = {1, 0, 0, -1, -1, 0, 1, 1};
         int [] order ={0, 1, -1};
 
-        System.out.println("This is the sorted array : " + threeNumberSort(input, order));
+        System.out.println("This is the sorted array : " + Arrays.toString(threeNumberSort(input, order)));
     }
 }
