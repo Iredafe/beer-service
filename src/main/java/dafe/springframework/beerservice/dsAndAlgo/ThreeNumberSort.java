7bd11e69177e14be7a9d8274bbdcd061dc63154e
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-17 18:54:06 -0700

    three number sort WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ThreeNumberSort.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ThreeNumberSort.java
@@ -1,4 +1,17 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class ThreeNumberSort {
+
+    public static int[] threeNumberSort(int [] array, int [] order){
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+
+        int input[] = {1, 0, 0, -1, -1, 0, 1, 1};
+        int [] order ={0, 1, -1};
+
+        System.out.println("This is the sorted array : " + threeNumberSort(input, order));
+    }
 }
