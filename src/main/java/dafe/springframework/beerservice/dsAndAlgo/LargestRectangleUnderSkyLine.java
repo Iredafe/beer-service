b5d3f405dc559e750da9894b096c425880f62425
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-18 14:34:29 -0700

    largest rectangle under skyline done, close #77

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LargestRectangleUnderSkyLine.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LargestRectangleUnderSkyLine.java
@@ -2,6 +2,8 @@ package dafe.springframework.beerservice.dsAndAlgo;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
 
 public class LargestRectangleUnderSkyLine {
 
@@ -29,10 +31,29 @@ public class LargestRectangleUnderSkyLine {
         return maxArea;
     }
 
+    public static int largestRectangleUnderSkyline(List<Integer> buildings){
+
+        Stack<Integer> indices = new Stack<>();
+        int maxArea = 0;
+        ArrayList<Integer> extendedBuildings = new ArrayList<>(buildings);
+        extendedBuildings.add(0);
+        for(int i=0; i< extendedBuildings.size(); i++){
+            int height = extendedBuildings.get(i);
+            while(!indices.isEmpty() && extendedBuildings.get(indices.peek()) >= height){
+                int pillarHeight = extendedBuildings.get(indices.pop());
+                int width = (indices.isEmpty()) ? i : i-indices.peek() - 1;
+                maxArea = Math.max(width * pillarHeight, maxArea);
+            }
+            indices.push(i);
+        }
+        return maxArea;
+    }
+
     public static void main(String[] args) {
 
         ArrayList<Integer> buildings = new ArrayList<>(Arrays.asList(1, 3, 3, 2, 4, 1, 5, 3, 2));
 
         System.out.println("This is the largest rectangle : " + largestRectangle(buildings));
+        System.out.println("This is the largest rectangle : " + largestRectangleUnderSkyline(buildings));
     }
 }
