737e7fb2d657ae36ddf9ca837145ea2fe9efa6c4
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-17 23:10:27 -0700

    valid starting city solved with a working brute force solution

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidStartingCity.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidStartingCity.java
@@ -4,13 +4,13 @@ public class ValidStartingCity {
 
     public static int validStartingCity(int [] distance, int [] fuel, int mpg){
         int validStartingCity = 0;
-        for(int city=0; city < distance.length; city++){
-            int startingCity = distance[city];
-            for(int gas = city; gas < city + fuel.length; gas++){
-                int rotatedIndex = gas % fuel.length;
-                if(startingCity - (fuel[rotatedIndex] * mpg) < 0 ){
+        int numberOfCities = distance.length;
+        for(int city = 0; city < numberOfCities; city++){
+            for(int currentCityIdx = city; currentCityIdx < city + numberOfCities; currentCityIdx++){
+                int currentCityRotatedIndex = currentCityIdx % numberOfCities;
+                if((fuel[currentCityRotatedIndex] * mpg) - distance[currentCityRotatedIndex] < 0 ){
                     continue;
-                }else if(startingCity - (fuel[rotatedIndex] * mpg) >= 0) {
+                }else if((fuel[currentCityRotatedIndex] * mpg) - distance[currentCityRotatedIndex]  >= 0) {
                     validStartingCity= city;
                 }
             }
@@ -18,12 +18,35 @@ public class ValidStartingCity {
         return validStartingCity;
     }
 
+    public static int validStartingCity1(int [] distance, int []fuel, int mpg){
+        int numberOfCities = distance.length;
+        for(int startCityIdx = 0; startCityIdx< numberOfCities; startCityIdx++){
+            int milesRemaining = 0;
+            for(int currentCityIdx= startCityIdx; currentCityIdx < startCityIdx+numberOfCities; currentCityIdx++) {
+                if (milesRemaining < 0) continue;
+
+                int currentCityRotatedIndex = currentCityIdx % numberOfCities;
+                int distanceToNextCity = distance[currentCityRotatedIndex];
+                int fuelFromCurrentCity = fuel[currentCityRotatedIndex];
+
+                milesRemaining += (fuelFromCurrentCity * mpg) - distanceToNextCity;
+            }
+                if(milesRemaining >= 0) return startCityIdx;
+            }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int distance[] = {5, 25, 15, 10, 15};
         int fuel[]={1, 2, 1, 0, 3};
         int mpg=10;
 
-        System.out.println("This is the valid starting city : " + validStartingCity(distance, fuel, mpg));
+        int distance1[] = {10, 20, 10, 15, 5, 15, 25};
+        int fuel1[]={0, 2, 1, 0, 0, 1, 1};
+        int mpg1=20;
+
+        System.out.println("This is the valid starting city : " + validStartingCity1(distance, fuel, mpg));
+        System.out.println("This is the valid starting city : " + validStartingCity1(distance1, fuel1, mpg1));
     }
 }
 
