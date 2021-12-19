a47447439da450fa9179f920268a98a60b280e0c
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-18 22:50:34 -0700

    minimum number of words non-working solution

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinimumCharactersForWords.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinimumCharactersForWords.java
@@ -1,13 +1,15 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class MinimumCharactersForWords {
 
     public static char[] minimumCharacterForWords(String [] words){
 
-        ArrayList<Integer> output = new ArrayList<>();
+        ArrayList<Character> output = new ArrayList<>();
         HashMap<Character, Integer> bucket = new HashMap<>();
         for(String word : words){
             HashMap<Character, Integer> secondBucket = new HashMap<>();
@@ -16,20 +18,35 @@ public class MinimumCharactersForWords {
                 if(!secondBucket.isEmpty()){
                     if(secondBucket.containsKey(character) && secondBucket.get(character)!=frequency){
                         secondBucket.put(character, secondBucket.getOrDefault(character, 0) +1);
+                        frequency++;
                         bucket.put(character, secondBucket.get(character));
+                    }else{
+                        secondBucket.put(character, 1);
                     }
                 }
             }
         }
+        for(Map.Entry <Character,Integer> frequency : bucket.entrySet()){
+            char character = frequency.getKey();
+            int characterFrequency = frequency.getValue();
 
+            for(int i=0; i<characterFrequency; i++){
+                output.add(character);
+            }
+        }
 
-        return new char[]{};
+        char [] charactersArray = new char[output.size()];
+        for(int idx = 0; idx< output.size(); idx++){
+            charactersArray[idx] = output.get(idx);
+        }
+
+        return charactersArray;
     }
 
     public static void main(String [] args){
 
         String [] input = {"this", "that", "did", "deed", "them!", "a"};
-        System.out.println("This is the minimum number of characters for words : ");
+        System.out.println("This is the minimum number of characters for words : " + Arrays.toString(minimumCharacterForWords(input)));
 
     }
 }
