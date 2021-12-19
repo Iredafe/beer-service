55d296670c44dc2ce711222f616141fb4276d8df
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-18 22:38:53 -0700

    minimum number of words WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinimumCharactersForWords.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinimumCharactersForWords.java
@@ -1,4 +1,35 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MinimumCharactersForWords {
+
+    public static char[] minimumCharacterForWords(String [] words){
+
+        ArrayList<Integer> output = new ArrayList<>();
+        HashMap<Character, Integer> bucket = new HashMap<>();
+        for(String word : words){
+            HashMap<Character, Integer> secondBucket = new HashMap<>();
+            int frequency = 0;
+            for(char character : word.toCharArray()){
+                if(!secondBucket.isEmpty()){
+                    if(secondBucket.containsKey(character) && secondBucket.get(character)!=frequency){
+                        secondBucket.put(character, secondBucket.getOrDefault(character, 0) +1);
+                        bucket.put(character, secondBucket.get(character));
+                    }
+                }
+            }
+        }
+
+
+        return new char[]{};
+    }
+
+    public static void main(String [] args){
+
+        String [] input = {"this", "that", "did", "deed", "them!", "a"};
+        System.out.println("This is the minimum number of characters for words : ");
+
+    }
 }
