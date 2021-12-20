fab845aecb75424b9ead973f1c2c448c002ea708
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-19 23:38:50 -0700

    valid IP addresses WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidIPAddresses.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidIPAddresses.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+
 public class ValidIPAddresses {
+
+    public static ArrayList<String> validIPAddresses(String input){
+
+        return new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        String input = "1921680";
+        System.out.println("These are the valid IP addresses : " + validIPAddresses(input));
+    }
 }
