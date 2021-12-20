4a3e574b455d299eca0f839e762f48552d70bccb
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-20 00:26:18 -0700

    valid IP addresses done - closes #79

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidIPAddresses.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidIPAddresses.java
@@ -5,15 +5,35 @@ import java.util.ArrayList;
 public class ValidIPAddresses {
 
     public static ArrayList<String> validIPAddresses(String input){
+        ArrayList<String> ipAddressesFound = new ArrayList<>();
 
-        return new ArrayList<>();
+        for(int i = 1; i< Math.min(input.length(), 4); i++){
+            String [] currentIPAddressParts = new String[]{"","","",""};
+
+            currentIPAddressParts[0] = input.substring(0,i);
+            if(!isValidPart(currentIPAddressParts[0])) continue;
+
+            for(int j=i+1; j< i+Math.min(input.length()-i, 4); j++){
+                currentIPAddressParts[1] = input.substring(i,j);
+                if(!isValidPart(currentIPAddressParts[1])) continue;
+
+                for(int k=j+1; k< j+ Math.min(input.length()-j, 4); k++){
+                    currentIPAddressParts[2] = input.substring(j, k);
+                    currentIPAddressParts[3] = input.substring(k);
+                    if(isValidPart(currentIPAddressParts[2]) && isValidPart(currentIPAddressParts[3])){
+                        ipAddressesFound.add(join(currentIPAddressParts));
+                    }
+                }
+            }
+        }
+
+        return ipAddressesFound;
     }
 
-    public boolean isValidPart(String input){
+    public static boolean isValidPart(String input){
         int stringAsInt = Integer.parseInt(input);
-        boolean hasLeadingZero = input.length()==Integer.toString(stringAsInt).length();
-        if(stringAsInt > 255 || !hasLeadingZero) return false;
-        return true;
+        if(stringAsInt > 255) return false;
+        return input.length()==Integer.toString(stringAsInt).length();
     }
 
     public static String join(String [] strings){
