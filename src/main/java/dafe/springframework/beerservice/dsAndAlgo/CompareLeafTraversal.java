dedd9f164d6399be56f5dbe83969c9a324432f45
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-21 13:16:32 -0700

    compare leaf traversal WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/CompareLeafTraversal.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/CompareLeafTraversal.java
@@ -1,4 +1,17 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class CompareLeafTraversal {
+    static class BinaryTree{
+        int value;
+        BinaryTree left;
+        BinaryTree right;
+
+        public BinaryTree(int value){
+            this.value=value;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
