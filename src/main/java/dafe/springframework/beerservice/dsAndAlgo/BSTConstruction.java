aa73546b7844e8bc2b9b9cbb622a3c14661708b8
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-22 12:15:37 -0700

    BST construction  WIP III

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BSTConstruction.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BSTConstruction.java
@@ -2,43 +2,50 @@ package dafe.springframework.beerservice.dsAndAlgo;
 
 public class BSTConstruction {
 
-    static class BST{
-       public int value;
+    static class BST {
+        public int value;
         public BST left;
         public BST right;
 
-        public BST(int value){
+        public BST(int value) {
             this.value = value;
         }
 
-        public BST insert(int value){
+        public BST insert(int value) {
             BST currentNode = this;
-           while (true) {
-               if (value < currentNode.value) {
-                   if (currentNode.left == null) {
-                       BST newNode = new BST(value);
-                       currentNode.left = newNode;
-                   } else {
-                       currentNode = currentNode.left;
-                   }
-               } else {
-                       if (currentNode.right == null) {
-                           BST newNode = new BST(value);
-                           currentNode.right = newNode;
-                       } else {
-                           currentNode = currentNode.right;
-                       }
-                   }
-               return this;
-           }
-           }
+            while (true) {
+                if (value < currentNode.value) {
+                    if (currentNode.left == null) {
+                        BST newNode = new BST(value);
+                        currentNode.left = newNode;
+                    } else {
+                        currentNode = currentNode.left;
+                    }
+                } else {
+                    if (currentNode.right == null) {
+                        BST newNode = new BST(value);
+                        currentNode.right = newNode;
+                    } else {
+                        currentNode = currentNode.right;
+                    }
+                }
+                return this;
+            }
         }
-        public boolean contains(int value){
 
-
-            return true;
+        public boolean contains(int value) {
+            BST currentNode = this;
+            while (currentNode != null) {
+                if (value < currentNode.value) {
+                    currentNode = currentNode.left;
+                } else if (value > currentNode.value) {
+                    currentNode = currentNode.right;
+                } else {
+                    return true;
+                }
+            }
+            return false;
         }
+
     }
-
-
 }
