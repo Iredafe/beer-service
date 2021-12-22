b2c8ce52f18d67da48e8815ce5b7181029d6d2a8
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-21 22:11:34 -0700

    youngest common ancestor WIP II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/YoungestCommonAncestor.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/YoungestCommonAncestor.java
@@ -14,6 +14,40 @@ public class YoungestCommonAncestor {
 
     }
 
+    public static BinaryTree getYoungestCommonAncestor(BinaryTree topAncestor, BinaryTree descendantOne, BinaryTree descendantTwo){
+
+        int depthOne = getDescendant(descendantOne, topAncestor);
+        int depthTwo = getDescendant(descendantTwo, topAncestor);
+
+        if(depthOne > depthTwo){
+            return backtrackAncestralTree(descendantOne, descendantTwo, depthOne-depthTwo);
+        }else{
+            return backtrackAncestralTree(descendantTwo, descendantTwo, depthTwo-depthOne);
+        }
+    }
+
+    public static int getDescendant(BinaryTree descendant, BinaryTree topAncestor){
+        int depth = 0;
+        while (descendant!=topAncestor){
+            depth++;
+            descendant=descendant.ancestor;
+        }
+        return depth;
+    }
+
+    public static BinaryTree backtrackAncestralTree(BinaryTree lowerDescendant, BinaryTree higherDescendant, int diff){
+        while (diff>0){
+            lowerDescendant = lowerDescendant.ancestor;
+            diff--;
+        }
+        while (lowerDescendant!=higherDescendant){
+            lowerDescendant=lowerDescendant.ancestor;
+            higherDescendant=higherDescendant.ancestor;
+        }
+        return lowerDescendant;
+    }
+
+
     public static void main(String[] args) {
 
         YoungestCommonAncestor tree = new YoungestCommonAncestor();
