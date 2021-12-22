950f781a76bcf2f81dfec04b06f72a5a0e90f3df
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-21 21:45:49 -0700

    youngest common ancestor WIP

---
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/YoungestCommonAncestor.java
@@ -0,0 +1,19 @@
+package dafe.springframework.beerservice.dsAndAlgo;
+
+public class YoungestCommonAncestor {
+
+    static class BinaryTree{
+        public char name;
+        public BinaryTree ancestor;
+
+        public BinaryTree(char name){
+            this.name = name;
+            this.ancestor = null;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
