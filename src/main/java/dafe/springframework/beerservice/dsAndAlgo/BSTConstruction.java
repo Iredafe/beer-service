03e6d83a0a4ccab0f11e96e01f3213b00c85bc59
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-22 11:49:52 -0700

    BST construction  WIP

---
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BSTConstruction.java
@@ -0,0 +1,40 @@
+package dafe.springframework.beerservice.dsAndAlgo;
+
+public class BSTConstruction {
+
+    static class BST{
+       public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value){
+            this.value = value;
+        }
+
+        public BST insert(int value){
+            BST currentNode = this;
+           while (true) {
+               if (value < currentNode.value) {
+                   if (currentNode.left == null) {
+                       BST newNode = new BST(value);
+                       currentNode.left = newNode;
+                   } else {
+                       currentNode = currentNode.left;
+                   }
+               } else {
+                   if (value > currentNode.value) {
+                       if (currentNode.right == null) {
+                           BST newNode = new BST(value);
+                           currentNode.right = newNode;
+                       } else {
+                           currentNode = currentNode.right;
+                       }
+                   }
+               }
+               return this;
+           }
+        }
+    }
+
+
+}
