7dec20e7a8909c19f9d2a1da6a3fab6079059db6
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-23 16:04:21 -0700

    reconstruct BST  WIP

---
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ReconstructBST.java
@@ -0,0 +1,17 @@
+package dafe.springframework.beerservice.dsAndAlgo;
+
+public class ReconstructBST {
+
+    public static class BST{
+        int value;
+        BST left = null;
+        BST right = null;
+
+        public BST(int value){
+            this.value = value;
+        }
+
+    }
+
+
+}
