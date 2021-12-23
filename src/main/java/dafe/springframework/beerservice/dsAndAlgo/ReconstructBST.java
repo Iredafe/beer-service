6a58819c7e9c40e78bc409fbf8a1d69db3b9d1c9
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-23 16:05:53 -0700

    reconstruct BST  WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ReconstructBST.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ReconstructBST.java
@@ -1,5 +1,9 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ReconstructBST {
 
     public static class BST{
@@ -13,5 +17,10 @@ public class ReconstructBST {
 
     }
 
+    public static void main(String[] args) {
+        ArrayList <Integer> preOrderTraversalValues = new ArrayList<>(Arrays.asList(10, 4, 2, 1, 5, 17, 19, 18));
+
+
+    }
 
 }
