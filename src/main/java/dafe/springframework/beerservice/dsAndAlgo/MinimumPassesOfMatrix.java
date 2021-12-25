ecc673a510116897741cd10c937bb929b3836018
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-24 20:18:49 -0700

    minimum passes of matrix WIP VII

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinimumPassesOfMatrix.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinimumPassesOfMatrix.java
@@ -6,7 +6,7 @@ public class MinimumPassesOfMatrix {
 
     public static int minimumPassesOfMatrix(int [][] matrix){
         int passes = convertNegatives(matrix);
-        return passes;
+        return (!containsNegative(matrix) ? passes-1 : -1);
     }
 
     public static int convertNegatives(int [][]matrix){
@@ -16,7 +16,7 @@ public class MinimumPassesOfMatrix {
             ArrayList<int[]> currentPassQueue = nextPassQueue;
             nextPassQueue = new ArrayList<>();
 
-            while (currentPassQueue.size() > 0){
+         while (currentPassQueue.size() > 0){
                 int [] vals = currentPassQueue.remove(0);
                 int currentRow = vals[0];
                 int currentCol = vals[1];
@@ -33,28 +33,18 @@ public class MinimumPassesOfMatrix {
                             nextPassQueue.add(new int[]{row, col});
                         }
                 }
-
-            }
-
+         }
+                passes++;
         }
 
 
         return 1;
     }
 
-    public static ArrayList<int[]> getAdjacentPositions(int row, int col, int[][]matrix){
-        ArrayList<int []> adjacentPositions = new ArrayList<>();
-        if(row>0) adjacentPositions.add(new int[] {row-1, col});
-        if(row < matrix.length) adjacentPositions.add(new int[]{row+1, col});
-        if(col <0) adjacentPositions.add(new int[]{row, col-1});
-        if(col>matrix[0].length) adjacentPositions.add(new int[] {row, col+1});
-        return adjacentPositions;
-    }
-
     public static ArrayList<int[]> getAllPositivePositions( int[][] matrix){
         ArrayList<int[]> positivePositions = new ArrayList<>();
         for(int row=0; row < matrix.length; row++){
-            for(int col=0; col < matrix[0].length; col++){
+            for(int col=0; col < matrix[row].length; col++){
                 int value = matrix[row][col];
                 if(value > 0){
                     positivePositions.add(new int[]{row,col});
@@ -62,12 +52,20 @@ public class MinimumPassesOfMatrix {
             }
         }
         return positivePositions;
-         }
+    }
+
+    public static ArrayList<int[]> getAdjacentPositions(int row, int col, int[][]matrix){
+        ArrayList<int []> adjacentPositions = new ArrayList<>();
+        if(row>0) adjacentPositions.add(new int[] {row-1, col});
+        if(row < matrix.length-1) adjacentPositions.add(new int[]{row+1, col});
+        if(col>0) adjacentPositions.add(new int[]{row, col-1});
+        if(col<matrix[0].length-1) adjacentPositions.add(new int[] {row, col+1});
+        return adjacentPositions;
+    }
 
     public static boolean containsNegative(int [][] matrix){
-        for(int row=0; row < matrix.length; row++){
-            for(int col=0; col < matrix[0].length; col++){
-                int value = matrix[row][col];
+        for(int [] row : matrix){
+            for(int value : row){
                 if(value < 0) return true;
             }
         }
