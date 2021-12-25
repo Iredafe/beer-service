2bb22ca85571a6e1ff63c5fce537badcb8b77243
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-24 17:54:44 -0700

    minimum passes of matrix WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinimumPassesOfMatrix.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinimumPassesOfMatrix.java
@@ -1,4 +1,17 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class MinimumPassesOfMatrix {
+
+    public static int minimumPassesOfMatrix(int [][] matrix){
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        int [][] matrix =   {{0, -1, -3, 2, 0},
+                            {1, -2, -5, -1, -3},
+                            {3, 0, 0, -4, -1}};
+        System.out.println("This is the minimum passes of the given matrix : " + minimumPassesOfMatrix(matrix));
+    }
 }
