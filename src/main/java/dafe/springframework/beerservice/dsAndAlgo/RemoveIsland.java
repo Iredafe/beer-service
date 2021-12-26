9a7d131fb5f3373ad7c2d84810c7b3b4b03146de
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-12-25 19:55:29 -0700

    remove island WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/RemoveIsland.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/RemoveIsland.java
@@ -1,4 +1,22 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class RemoveIsland {
+
+    static int [][] removeIslands(int [][] matrix){
+
+        return matrix;
+    }
+
+
+    public static void main(String[] args) {
+        int [][] matrix =           {{1, 0, 0, 0, 0, 0},
+                                    {0, 1, 0, 1, 1, 1},
+                                    {0, 0, 1, 0, 1, 0},
+                                    {1, 1, 0, 0, 1, 0},
+                                    {1, 0, 1, 1, 0, 0},
+                                    {1, 0, 0, 0, 0, 1}};
+
+        System.out.println("This is the matrix after removing islands : " + removeIslands(matrix));
+                            };
+
 }
