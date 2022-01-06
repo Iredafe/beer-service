2a1e5c07de82b5bdafc86552bfed8279901ed98a
Iredafe Owolabi <iredafeowolabi@gmail.com>
2022-01-05 22:37:53 -0700

    lowest common ancestor WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LowestCommonAncestor.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LowestCommonAncestor.java
@@ -5,7 +5,10 @@ public class LowestCommonAncestor {
     private TreeNode result=null;
 
     public boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q){
-
+        if(currentNode == null) return false;
+        int left = this.recurseTree(currentNode.left, p, q) ? 1 : 0;
+        int right = this.recurseTree(currentNode.right, p, q) ? 1 : 0;
+        int mid = (currentNode == p || currentNode ==q) ? 1 : 0;
 
     }
 }
