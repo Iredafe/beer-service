29ffd7d0ccf45136dd67285bbc72ab71276afdf0
Iredafe Owolabi <iredafeowolabi@gmail.com>
2022-01-24 23:38:25 -0700

    Valid sudoku WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidSudoku.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidSudoku.java
@@ -2,9 +2,22 @@ package dafe.springframework.beerservice.dsAndAlgo;
 
 public class ValidSudoku {
 
-    public static boolean validSudoku(char [][] sudoku){
+    public static boolean validSudoku(char[][] sudoku) {
 
         return true;
     }
 
+    public static void main(String[] args) {
+
+        char[][] sudoku = {{'5' , '3' , '.' , '.' , '7' , '.' , '.' , '.' , '.'}
+                , {'6' , '.' , '.' , '1' , '9' , '5' , '.' , '.' , '.'}
+                , {'.' , '9' , '8' , '.' , '.' , '.' , '.' , '6' , '.'}
+                , {'8' , '.' , '.' , '.' , '6' , '.' , '.' , '.' , '3'}
+                , {'4' , '.' , '.' , '8' , '.' , '3' , '.' , '.' , '1'}
+                , {'7' , '.' , '.' , '.' , '2' , '.' , '.' , '.' , '6'}
+                , {'.' , '6' , '.' , '.' , '.' , '.' , '2' , '8' , '.'}
+                , {'.' , '.' , '.' , '4' , '1' , '9' , '.' , '.' , '5'}
+                , {'.' , '.' , '.' , '.' , '8' , '.' , '.' , '7' , '9'}};
+
+    }
 }
