4c4674387d864f2fb39bf2e1d4bf8623c2404fd3
Iredafe Owolabi <iredafeowolabi@gmail.com>
2022-01-26 22:34:46 -0700

    word search WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/WordSearch.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/WordSearch.java
@@ -1,4 +1,9 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class WordSearch {
+
+//loop through the board
+//check each position to see if the letter exists.
+// if it does not exists, return flase
+//else return true after traversing the board
 }
