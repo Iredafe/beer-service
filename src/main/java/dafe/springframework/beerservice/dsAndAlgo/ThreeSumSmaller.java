611ed3d4b4fffe30fa208e8c52de9392573e4b92
Iredafe Owolabi <iredafeowolabi@gmail.com>
2022-01-28 23:17:22 -0700

    3 sum WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ThreeSumSmaller.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ThreeSumSmaller.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class ThreeSumSmaller {
+
+    public static int threeSum(int [] nums , int target){
+        int sum =0;
+        for(int i=0; i<nums.length-2; i++){
+            sum+= twoSum(nums, i+1, target);
+        }
+        return sum;
+    }
+
 }
