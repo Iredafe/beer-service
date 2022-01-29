3634cf11cd22f70cdd9ec08c27096d4d9b516bf4
Iredafe Owolabi <iredafeowolabi@gmail.com>
2022-01-28 23:25:23 -0700

    3 sum smaller WIP II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ThreeSumSmaller.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ThreeSumSmaller.java
@@ -5,9 +5,13 @@ public class ThreeSumSmaller {
     public static int threeSumSmaller(int [] nums , int target){
         int sum =0;
         for(int i=0; i<nums.length-2; i++){
-            sum+= twoSum(nums, i+1, target-nums[i]);
+            sum+= binarySearch(nums, i+1, target-nums[i]);
         }
         return sum;
+    }
+
+    private static int binarySearch(int[] nums, int i, int j) {
+
     }
 
     public static void main(String[] args) {
