19d8378d37d2c271c088cd04cd07b8918f1dd295
Iredafe Owolabi <iredafeowolabi@gmail.com>
2022-01-29 23:42:48 -0700

    3 sum closest done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ThreeSumClosest.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ThreeSumClosest.java
@@ -3,16 +3,16 @@ package dafe.springframework.beerservice.dsAndAlgo;
 import java.util.Arrays;
 
 public class ThreeSumClosest {
-
     private static int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
+        int sum=0;
         int difference = Integer.MAX_VALUE;
         for(int i=0; i< nums.length; i++){
             int start = i+1;
             int end = nums.length - 1;
 
             while(start<end){
-                int sum = nums[i] + nums[start] + nums[end];
+                sum = nums[i] + nums[start] + nums[end];
                if(Math.abs(target-sum) < Math.abs(difference)){
                    difference = target-sum;
                }else if(sum<target){
