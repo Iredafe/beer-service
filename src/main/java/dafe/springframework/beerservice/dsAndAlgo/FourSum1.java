8f8bfbb9faaa36bc5ee6ece257cbe13fdd106753
Iredafe Owolabi <iredafeowolabi@gmail.com>
2022-01-30 15:29:10 -0700

    4 sum Working solution done.

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FourSum1.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FourSum1.java
@@ -6,41 +6,42 @@ import java.util.List;
 
 public class FourSum1 {
 
-    public static List<List<Integer>> fourSum(int [] nums, int target){
-
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        if(nums.length < 4) return result;
-        Arrays.sort(nums);
-        for(int i = 0; i< nums.length-3; i++){
-            if(nums[i] == nums[i+1]) continue;
-            for(int j= i+1; j < nums.length-2; j++){
-                if(nums[j] == nums[j+1]) continue;
-            int start = i+1;
-            int end = nums.length-1;
 
-            while(start < end){
-                int sum = nums[i] + nums[j] + nums[start] + nums[end];
-                if(sum == target){
-                    result.add(Arrays.asList(nums[i], nums[j], nums[start], nums[end]));
-                    start++;
-                    end--;
-                    while(start < end && nums[start] == nums[start+1]) start++;
-                    while(start<end && nums[end] == nums[end-1]) end--;
-                }
-                if(sum < target){
-                    start++;
-                }else{
-                    end--;
+        if(nums==null || nums.length < 4) return result;
+        Arrays.sort(nums);
+        for(int i=0; i< nums.length-3; i++){
+            if(i>0 && nums[i] == nums[i-1] ) continue;
+            for(int j=i+1; j<nums.length-2; j++){
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
+                int start = j+1;
+                int end = nums.length-1;
+                while(start < end){
+                    int sum = nums[i] + nums[j] + nums[start] + nums[end];
+                    if(sum == target){
+                        result.add(Arrays.asList(nums[i], nums[j], nums[start], nums[end]));
+                        start++;
+                        end--;
+                        while(start<end && nums[start] == nums[start-1]) start++;
+                        while(start<end && nums[end] == nums[end+1]) end--;
+                    }
+                    if(sum<target){
+                        start++;
+                    }else{
+                        end--;
+                    }
                 }
             }
-            }
+
         }
         return result;
+
     }
 
 
     public static void main(String[] args) {
-        int [] nums = {1,0,-1,0,-2,2};
+        int [] nums = {-3,-2,-1,0,0,1,2,3};
         int target = 0;
 
         System.out.println("This is the result for 4 Sum : " + fourSum(nums, target));
