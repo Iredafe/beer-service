10900ba7991055023bab4aeadcd66b3065a38834
Iredafe Owolabi <iredafeowolabi@gmail.com>
2022-01-29 22:44:59 -0700

    3 sum closest II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ThreeSumClosest.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ThreeSumClosest.java
@@ -1,2 +1,10 @@
-package dafe.springframework.beerservice.dsAndAlgo;public class ThreeSumClosest {
+package dafe.springframework.beerservice.dsAndAlgo;
+
+public class ThreeSumClosest {
+
+
+    public static void main(String[] args) {
+        int nums[] ={-1,2,1,-4};
+        int target = 1;
+    }
 }
