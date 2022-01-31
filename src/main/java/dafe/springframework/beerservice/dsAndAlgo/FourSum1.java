616adcfa3551f60ed764107ab5604bb4d4179248
Iredafe Owolabi <iredafeowolabi@gmail.com>
2022-01-30 23:54:58 -0700

    4 sum Working solution with map II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FourSum1.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FourSum1.java
@@ -38,7 +38,31 @@ public class FourSum1 {
     }
 
     public static List<List<Integer>> fourSumHashMap(int [] nums, int target){
-
+        Map<Integer, Set<List<Integer>>> map = new HashMap<>();
+        List<List<Integer>> result = new ArrayList<>();
+         for(int i=1; i< nums.length-1; i++){
+            for(int j=i+1; j <nums.length; j++ ){
+                int currentSum = nums[i] + nums[j];
+                int difference = target - currentSum;
+                if(map.containsKey(difference)){
+                    for(List<Integer> pair : map.get(difference)){
+                        result.add(Arrays.asList(nums[i], nums[j], pair.get(0), pair.get(1)));
+                    }
+                }
+            }
+            for(int k=0; k<i; k++){
+                int currentSum = nums[i] + nums[k];
+                List<Integer> pair = Arrays.asList(nums[i], nums[k]);
+                if(!map.containsKey(currentSum)){
+                    Set<List<Integer>> pairedList = new HashSet<>();
+                    pairedList.add(pair);
+                    map.put(currentSum,pairedList);
+                }else{
+                    map.get(currentSum).add(pair);
+                }
+            }
+        }
+         return result;
     }
 
     public static void main(String[] args) {
@@ -46,6 +70,6 @@ public class FourSum1 {
         int target = 0;
 
         System.out.println("This is the result for 4 Sum : " + fourSum(nums, target));
-        System.out.println("This is the result for 4 Sum with hashset: " + fourSumHashMap(nums, target));
+        System.out.println("This is the result for 4 Sum with hashMap: " + fourSumHashMap(nums, target));
     }
 }
