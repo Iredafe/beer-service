5cd62e635155543417ebcafe6053adf4a8dbca9d
Iredafe Owolabi <iredafeowolabi@gmail.com>
2022-01-30 23:08:40 -0700

    4 sum Working solution with map I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FourSum1.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FourSum1.java
@@ -37,26 +37,8 @@ public class FourSum1 {
 
     }
 
-    public static List<List<Integer>> fourSumHashSet(int [] nums, int target){
+    public static List<List<Integer>> fourSumHashMap(int [] nums, int target){
 
-        Set<List<Integer>> result = new HashSet<>();
-        for(int i=0; i<nums.length-2; i++){
-            for(int j= i+1; j<nums.length; j++){
-                int start = j+1, end = nums.length-1;
-                while(start < end){
-                    int sum = nums[i] + nums[j] + nums[start] + nums[end];
-                    if(sum == target){
-                        result.add(Arrays.asList(nums[i], nums[j], nums[start], nums[end]));
-                    }
-                    else if(sum < target){
-                        start++;
-                    }else{
-                        end--;
-                    }
-                }
-            }
-        }
-        return new ArrayList<>(result);
     }
 
     public static void main(String[] args) {
@@ -64,6 +46,6 @@ public class FourSum1 {
         int target = 0;
 
         System.out.println("This is the result for 4 Sum : " + fourSum(nums, target));
-        System.out.println("This is the result for 4 Sum with hashset: " + fourSumHashSet(nums, target));
+        System.out.println("This is the result for 4 Sum with hashset: " + fourSumHashMap(nums, target));
     }
 }
