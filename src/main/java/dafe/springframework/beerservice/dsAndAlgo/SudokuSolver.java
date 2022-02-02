d51538599598184caf4f0963919b9ea6b44eb417
Iredafe Owolabi <iredafeowolabi@gmail.com>
2022-02-01 22:50:41 -0700

    solve sudoku Wip I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SudokuSolver.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SudokuSolver.java
@@ -2,6 +2,14 @@ package dafe.springframework.beerservice.dsAndAlgo;
 
 public class SudokuSolver {
 
+    int boxSize = 3;
+    int rowSize = 9;
+    int rows[][] = new int [rowSize][rowSize+1];
+    int columns[][] = new int[rowSize][rowSize+1];
+    int boxes [][] = new int[rowSize][rowSize+1];
+    char[][] board;
+    boolean sudokuSolved = false;
+
     public void solveSudoku(char[][] board) {
 
     }
