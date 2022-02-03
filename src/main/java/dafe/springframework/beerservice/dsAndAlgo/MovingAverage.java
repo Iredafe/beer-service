4a39c9fc17aea3d5ab58a61da24b3d3a3d51205f
Iredafe Owolabi <iredafeowolabi@gmail.com>
2022-02-02 22:01:47 -0700

    moving average WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MovingAverage.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MovingAverage.java
@@ -1,15 +1,22 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MovingAverage {
 
     int size;
-
+    List<Integer> queue = new ArrayList<>();
     public MovingAverage(int size){
         this.size = size;
     }
 
     public double next(int val){
-
-        return 1.0;
+        queue.add(val);
+        int windowSum=0;
+        for(int i = Math.max(queue.size(), size); i< queue.size(); i++){
+            windowSum = (int)queue.get(i);
+        }
+        return windowSum * 1.0 / Math.min(queue.size(), size);
     }
 }
