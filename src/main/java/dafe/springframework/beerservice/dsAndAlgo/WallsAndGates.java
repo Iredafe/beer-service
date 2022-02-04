33aa78ec5106d8abd04c6d9b27641814296e0ea9
Iredafe Owolabi <iredafeowolabi@gmail.com>
2022-02-03 21:46:06 -0700

    walls and gates WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/WallsAndGates.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/WallsAndGates.java
@@ -1,4 +1,19 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class WallsAndGates {
+
+    public int[][] wallsAndGates(int [][] room){
+
+        return room;
+    }
+
+
+    public static void main(String[] args) {
+        int [][] input = {{2147483647,-1,0,2147483647},
+                        {2147483647,2147483647,2147483647,-1},
+                        {2147483647,-1,2147483647,-1},
+                        {0,-1,2147483647,2147483647}};
+
+
+    }
 }
