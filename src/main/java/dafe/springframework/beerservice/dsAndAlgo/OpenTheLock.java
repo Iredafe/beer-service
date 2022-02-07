0e345bcbda2d7001cdbec7ea6f313e19806add68
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-07 05:34:59 -0700

    open the lock WIP II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/OpenTheLock.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/OpenTheLock.java
@@ -1,9 +1,26 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
 public class OpenTheLock {
 
     public static int openTheLock(String [] deadends, String target){
+    int numberOfTurns = 0;
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        Set<String> deadendSet = new HashSet<>();
 
+        for(int i=0; i<deadends.length; i++){
+            deadendSet.add(deadends[i]);
+        }
+
+
+    while(!queue.isEmpty()){
+
+    }
         return -1;
     }
 
