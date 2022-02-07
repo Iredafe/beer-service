6babc58ddaaadcff2bb33a7ade15dae202336a57
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-07 05:27:38 -0700

    open the lock WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/OpenTheLock.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/OpenTheLock.java
@@ -2,8 +2,15 @@ package dafe.springframework.beerservice.dsAndAlgo;
 
 public class OpenTheLock {
 
-    public int openTheLock(String [] deadends, String target){
+    public static int openTheLock(String [] deadends, String target){
 
         return -1;
+    }
+
+    public static void main(String[] args) {
+        String [] deadends = {"0201","0101","0102","1212","2002"};
+        String target = "0202";
+
+        System.out.println("This is the depth : " + openTheLock(deadends, target));
     }
 }
