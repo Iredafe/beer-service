549f93ac79e59e191c4b37994e568ba7b29e3236
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-07 22:58:34 -0700

    perfect  number WIP II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/PerfectNumber.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/PerfectNumber.java
@@ -10,10 +10,15 @@ public class PerfectNumber {
     public static int perfectNumber(int n){
         Queue<Integer> queue = new LinkedList<>();
         Set<Integer> visited = new HashSet<>();
-        int count = 0;
+        queue.add(0);
+        visited.add(0);
+        int count = 1;
 
         while(!queue.isEmpty()){
+            int size = queue.size();
+            while(size > 0){
 
+            }
         }
         return count;
     }
