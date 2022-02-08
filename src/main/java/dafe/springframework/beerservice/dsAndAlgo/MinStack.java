7ce766d747eacdea79015351dd9f2124305c1d46
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-08 16:00:01 -0700

    minStack WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinStack.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinStack.java
@@ -1,4 +1,26 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.Stack;
+
 public class MinStack {
+    Stack<Integer> stack;
+    public MinStack() {
+        stack  = new Stack<>();
+    }
+
+    public void push(int val) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int getMin() {
+
+    }
 }
