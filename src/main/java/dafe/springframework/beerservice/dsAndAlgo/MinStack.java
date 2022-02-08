15a8968c578a72830a176f0ac13f71035640832a
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-08 16:31:20 -0700

    Implementation minStack WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinStack.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MinStack.java
@@ -3,13 +3,18 @@ package dafe.springframework.beerservice.dsAndAlgo;
 import java.util.Stack;
 
 public class MinStack {
-    Stack<Integer> stack;
+    Stack<int[]> stack;
     public MinStack() {
         stack  = new Stack<>();
     }
 
     public void push(int val) {
-
+        if(stack.isEmpty()){
+            stack.push(new int[]{val,val});
+            return;
+        }
+        int currentMin = stack.peek()[1];
+        stack.push(new int[]{val, Math.min(val,currentMin)});
     }
 
     public void pop() {
