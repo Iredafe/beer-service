5069f9a63067763e89e42f6214c1d2e07e672e1c
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-07 22:48:39 -0700

    perfect  number WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/PerfectNumber.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/PerfectNumber.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class PerfectNumber {
+
+    public static int perfectNumber(int n){
+
+        return n;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
