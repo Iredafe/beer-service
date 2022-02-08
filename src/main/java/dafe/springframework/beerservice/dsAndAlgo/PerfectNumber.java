a3fbf8c10ee5d1040c356ffabbab7731454bb674
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-07 22:57:24 -0700

    perfect  number WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/PerfectNumber.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/PerfectNumber.java
@@ -1,13 +1,25 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
 public class PerfectNumber {
 
     public static int perfectNumber(int n){
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        int count = 0;
 
-        return n;
+        while(!queue.isEmpty()){
+
+        }
+        return count;
     }
 
     public static void main(String[] args) {
 
+        System.out.println("This is the least perfect number required : " + perfectNumber(13));
     }
 }
