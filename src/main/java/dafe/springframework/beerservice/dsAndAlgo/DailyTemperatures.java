f52680b514c475ab975a3e423ef92e42704314d2
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-09 21:52:38 -0700

    daily temperatures WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/DailyTemperatures.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/DailyTemperatures.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class DailyTemperatures {
+
+    public static int[] dailyTemperatures(int [] temperatures){
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int [] temperatures = {73,74,75,71,69,72,76,73};
+    }
 }
