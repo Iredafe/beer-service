2e531b18de2d625143a97b5a7c9ea6e857f72d75
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-09 23:23:40 -0700

    daily temp WIP V

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/DailyTemperatures.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/DailyTemperatures.java
@@ -20,7 +20,14 @@ public class DailyTemperatures {
 
     public static int[] dailyTemp(int [] temperatures){
         int [] output = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
 
+        for(int i=0; i<temperatures.length; i++){
+            if(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]){
+                output[i] = i-stack.pop();
+            }
+            stack.push(i);
+        }
 
         return output;
     }
@@ -28,5 +35,6 @@ public class DailyTemperatures {
     public static void main(String[] args) {
         int [] temperatures = {73,74,75,71,69,72,76,73};
         System.out.println("This is the output : " + Arrays.toString(dailyTemperatures(temperatures)));
+        System.out.println("This is the output2 : " + Arrays.toString(dailyTemp(temperatures)));
     }
 }
