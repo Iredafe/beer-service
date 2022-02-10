aca5ee3e27a738bb5dab950b62dd93f8641629d5
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-09 21:11:31 -0700

    valid parenthesis WIP III

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidParentheses.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidParentheses.java
@@ -1,9 +1,18 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.Map;
+import java.util.Stack;
+
 public class ValidParentheses {
 
+    Map<Character, Character> map = Map.of('{','}','[',']','(',')');
     public static boolean isValid(String input){
 
+    Stack<Character> stack = new Stack<>();
+
+    for(int i=0; i<input.length(); i++){
+        char bracket = input.charAt(i);
+    }
         return false;
     }
 
