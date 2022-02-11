23f0901d7105931bd711efef1190c46fbc71d493
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-10 23:33:34 -0700

    evaluate reverse polish notation started

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/EvaluateReversePolishNotation.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/EvaluateReversePolishNotation.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class EvaluateReversePolishNotation {
+
+    public static int evaluate(String[] tokens){
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
