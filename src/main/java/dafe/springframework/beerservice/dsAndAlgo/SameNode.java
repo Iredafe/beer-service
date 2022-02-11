3a23fa7ca75eabfb82c68dc3cb9d8f1ee70c2195
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-10 21:36:28 -0700

    same node WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SameNode.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SameNode.java
@@ -1,4 +1,18 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class SameNode {
+
+    static class BinaryTree{
+    int value;
+    BinaryTree left=null;
+    BinaryTree right=null;
+
+    public BinaryTree(int value){
+        this.value = value;
+    }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
