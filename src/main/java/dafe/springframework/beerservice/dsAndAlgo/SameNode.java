34cffb38d667405c4490f48037be05eeaffd682e
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-10 21:49:36 -0700

    same node WIP III

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SameNode.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SameNode.java
@@ -28,7 +28,8 @@ public class SameNode {
         SameNode node2 = new SameNode();
         node1.root =  new BinaryTree(1);
         node1.root.left = new BinaryTree(2);
-        node2.root.right = new BinaryTree(3);        node1.root =  new BinaryTree(1);
+        node1.root.right = new BinaryTree(3);
+        node2.root =  new BinaryTree(1);
         node2.root.left = new BinaryTree(2);
         node2.root.right = new BinaryTree(3);
     }
