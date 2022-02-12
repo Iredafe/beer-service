fff6f228ebe21786e58cc561d7b8dd1b530f74ba
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-11 22:25:53 -0700

    next greater element WIP V

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NextGreaterElement1.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NextGreaterElement1.java
@@ -1,6 +1,8 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 public class NextGreaterElement1 {
@@ -27,7 +29,22 @@ public class NextGreaterElement1 {
 
     public static int[] nextGreaterElement(int nums1[], int nums2[]){
 
-        return new int[]{};
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i< nums2.length; i++){
+            map.put(nums2[i], i);
+        }
+        int j;
+        int result[] = new int [nums1.length];
+
+        for(int i=0; i< nums1.length; i++){
+            for(j=map.get(i) +1; j< nums2.length; j++){
+                if(nums2[j] > nums1[i]){
+                    result[i] = nums2[j];
+                }
+            }
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
