c6aa0e68a60d417ac1dfdfacfc1461e4251ff787
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-11 21:30:04 -0700

    next greater element WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NextGreaterElement1.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NextGreaterElement1.java
@@ -6,4 +6,10 @@ public class NextGreaterElement1 {
 
         return new int []{};
     }
+
+    public static void main(String[] args) {
+        int []nums1 = {4,1,2}, nums2 = {1,3,4,2};
+
+        System.out.println("Return the greater elements : " + greaterElement(nums1,nums2));
+    }
 }
