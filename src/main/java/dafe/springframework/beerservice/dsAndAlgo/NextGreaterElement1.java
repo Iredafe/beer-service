f293b130f690de8cea08cf9fdcd9bd3c475cc05b
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-11 21:43:01 -0700

    next greater element WIP II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NextGreaterElement1.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NextGreaterElement1.java
@@ -1,9 +1,21 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.Stack;
+
 public class NextGreaterElement1 {
 
     public static int[] greaterElement(int[] num1, int [] num2){
 
+        int[] result = new int[num1.length];
+        for(int i=0; i< num1.length; i++){
+            boolean found = false;
+            for(int j=0; j< num2.length; j++){
+                if(found && num2[j] > num1[i]){
+                    result[i] = num2[j];
+                }
+
+            }
+        }
         return new int []{};
     }
 
