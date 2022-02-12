1f0172370f5d8044adec87c8d03f16e56ad7c283
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-11 23:46:59 -0700

    next greater element with monotonic stack WIP II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NextGreaterElement1.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NextGreaterElement1.java
@@ -55,7 +55,14 @@ public class NextGreaterElement1 {
         Map<Integer, Integer> map = new HashMap<>();
 
         for(int i=0; i<nums2.length; i++){
+            while(!stack.isEmpty() && nums2[i] > stack.peek()){
+                map.put(stack.pop(), nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
 
+        while(!stack.isEmpty()){
+            map.put(stack.pop(), -1);
         }
         return result;
     }
