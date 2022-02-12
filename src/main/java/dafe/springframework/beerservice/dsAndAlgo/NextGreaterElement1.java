0e05c2757e723021cd78f712122705af9b130e46
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-11 21:28:39 -0700

    next greater element WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NextGreaterElement1.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NextGreaterElement1.java
@@ -1,4 +1,9 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class NextGreaterElement1 {
+
+    public static int[] greaterElement(int[] num1, int [] num2){
+
+        return new int []{};
+    }
 }
