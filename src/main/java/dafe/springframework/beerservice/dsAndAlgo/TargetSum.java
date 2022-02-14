4024d541a774f0c508d143d281ea6b240368f41a
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-13 23:25:02 -0700

    target sum Wip II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/TargetSum.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/TargetSum.java
@@ -1,21 +1,27 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class TargetSum {
+    static int count = 0;
 
     public static int targetSum(int [] nums, int target){
-    int count = 0;
-
-        return 0;
+        calculateTargetSum(nums, 0, 0, target);
+        return count;
     }
 
     public static void calculateTargetSum(int nums[], int i, int sum, int target){
         if(i== nums.length){
-
+            if(sum==target){
+                count++;
+            }
+        }else{
+            calculateTargetSum(nums, i+1, sum + nums[i], target);
+            calculateTargetSum(nums, i+1, sum - nums[i], target);
         }
     }
 
     public static void main(String[] args) {
         int [] nums = {1,1,1,1,1};
         int target = 3;
+        System.out.println("The number of times is : " + targetSum(nums, target));
     }
 }
