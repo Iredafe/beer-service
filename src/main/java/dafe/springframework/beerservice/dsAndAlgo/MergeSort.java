8f93e33d50de8cd090d3d43cfb3abbce58812e2a
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-13 21:18:43 -0700

    merge sort WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MergeSort.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MergeSort.java
@@ -1,9 +1,14 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.Arrays;
+
 public class MergeSort {
 
     public static int[] mergeSort(int [] array){
 
+        int mid = array.length/2;
+        int [] leftHalf = Arrays.copyOfRange(array, 0, mid);
+        int [] rightHalf = Arrays.copyOfRange(array, mid, array.length);
         return new int[]{};
     }
 
