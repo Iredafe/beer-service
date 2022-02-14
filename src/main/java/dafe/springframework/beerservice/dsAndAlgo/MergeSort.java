ffbcc6f2d1b39a3b7dc2f09f259288d80b15771e
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-13 21:34:05 -0700

    merge sort WIP II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MergeSort.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MergeSort.java
@@ -5,11 +5,19 @@ import java.util.Arrays;
 public class MergeSort {
 
     public static int[] mergeSort(int [] array){
-
+        if(array.length == 1) return array;
         int mid = array.length/2;
         int [] leftHalf = Arrays.copyOfRange(array, 0, mid);
         int [] rightHalf = Arrays.copyOfRange(array, mid, array.length);
-        return new int[]{};
+        return mergeBothArrays(mergeSort(leftHalf), mergeSort(rightHalf));
+    }
+
+    private static int[] mergeBothArrays(int[] leftHand, int [] rightHand) {
+    int i,j,k =0;
+        while(i<leftHand.length && j< rightHand.length ){
+
+        }
+
     }
 
     public static void main(String[] args) {
