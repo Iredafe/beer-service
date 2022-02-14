212680bd6a572e2422bcdf537aef22b42e80d1e4
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-13 23:10:52 -0700

    target sum Wip

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/TargetSum.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/TargetSum.java
@@ -1,4 +1,14 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class TargetSum {
+
+    public static int targetSum(int [] nums, int target){
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {1,1,1,1,1};
+        int target = 3;
+    }
 }
