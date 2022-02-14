fdad14a3c39329546521a02f933d588feb4c977d
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-13 18:56:05 -0700

    bubble sort WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BubbleSort1.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BubbleSort1.java
@@ -1,4 +1,37 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.List;
+
 public class BubbleSort1 {
+
+        public static List<Integer> sortList(List<Integer> unsortedList) {
+            int n = unsortedList.size();
+            for (int i = n - 1; i >= 0; i--) {
+                boolean swapped = false;
+                for (int j = 0; j < i; j++) {
+                    if (unsortedList.get(j) > unsortedList.get(j + 1)) {
+                        int temp = unsortedList.get(j);
+                        unsortedList.set(j, unsortedList.get(j + 1));
+                        unsortedList.set(j + 1, temp);
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                    return unsortedList;
+            }
+            return unsortedList;
+        }
+
+        public static List<String> splitWords(String s) {
+            return s.isEmpty() ? List.of() : Arrays.asList(s.split(" "));
+        }
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            List<Integer> unsortedList = splitWords(scanner.nextLine()).stream().map(Integer::parseInt).collect(Collectors.toList());
+            scanner.close();
+            List<Integer> res = sortList(unsortedList);
+            System.out.println(res.stream().map(String::valueOf).collect(Collectors.joining(" ")));
+        }
+    }
 }
