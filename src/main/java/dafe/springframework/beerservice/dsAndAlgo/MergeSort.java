3ade85683837a9500296923b9ae050971a5599b1
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-13 19:42:35 -0700

    merge sort WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MergeSort.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MergeSort.java
@@ -9,5 +9,7 @@ public class MergeSort {
 
     public static void main(String[] args) {
 
+        int [] input = {9,3,5,2,1,3,5,8,7};
+        System.out.println("This is the sorted result : " + mergeSort(input));
     }
 }
