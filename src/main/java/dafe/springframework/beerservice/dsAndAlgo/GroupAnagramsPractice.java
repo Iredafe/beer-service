c7ac5010b59f09e9ea2dfdbda3ea019d928b4aa4
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-13 23:41:36 -0700

    group anagrams practice WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/GroupAnagramsPractice.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/GroupAnagramsPractice.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class GroupAnagramsPractice {
+    public List<List<String>> groupAnagrams(List<String> words){
+
+        return new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList(new String[]{"yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"});
+    }
 }
