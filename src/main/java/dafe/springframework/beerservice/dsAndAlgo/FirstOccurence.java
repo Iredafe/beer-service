4307c67bf0056df1f291141e21b4c7cad59bf6fa
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-15 23:32:38 -0700

    first occurence - done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FirstOccurence.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FirstOccurence.java
@@ -8,21 +8,21 @@ public class FirstOccurence {
         int index = -1;
 
         while(left <= right){
-            int mid = left + right/2;
+            int mid = (left + right)/2;
             if(arr[mid] == target){
                 index = mid;
-            }else if(arr[mid] < target){
-                left = mid+1;
-            }else{
                 right = mid -1;
+            }else if(arr[mid] < target) {
+                left = mid + 1;
             }
         }
-
         return index;
     }
 
     public static void main(String[] args) {
         int [] arr ={1, 3, 3, 3, 3, 6, 10, 10, 10, 100};
         int target = 3;
+
+        System.out.println("This is the first occurrence : " + firstOccurence(arr, target));
     }
 }
