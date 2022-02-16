b2e24ca1798ea0e014223ada0db05bc91009f908
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-15 20:02:04 -0700

    isomorphic string WIP II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/IsoMorphicString.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/IsoMorphicString.java
@@ -10,5 +10,7 @@ public class IsoMorphicString {
     public static void main(String[] args) {
         String first = "egg";
         String second = "add";
+
+        System.out.println("These strings are Isomorphic : " + isomorphicString(first, second));
     }
 }
