d729fbf2f73572fd57a666b1386a71c267c1c1e8
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-15 23:03:43 -0700

    first element not smaller than target WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FirstElementNotSmallerThanTarget.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FirstElementNotSmallerThanTarget.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class FirstElementNotSmallerThanTarget {
+
+    public static int firstElementNotSmallerThanTarget(int [] arr, int target){
+        int left = 0;
+        int right = arr.length-1;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int input[] = {2, 3, 5, 7, 11, 13, 17, 19};
+        int target = 6;
+        System.out.println("This is the first element index : " + firstElementNotSmallerThanTarget(input, target));
+    }
 }
