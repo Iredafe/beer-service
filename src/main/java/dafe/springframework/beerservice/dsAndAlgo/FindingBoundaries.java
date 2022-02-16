549bfed005a1c155de38a4d47fd5da53e8fcf510
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-15 22:53:47 -0700

    finding boundaries done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FindingBoundaries.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FindingBoundaries.java
@@ -3,8 +3,23 @@ package dafe.springframework.beerservice.dsAndAlgo;
 public class FindingBoundaries {
 
     public static int findBoundary(boolean[] array){
+        int left = 0;
+        int right = array.length-1;
+        int boundaryIndex = -1;
+        while(left<=right){
+            int mid = (left+right)/2;
+        if(array[mid] == true){
+            boundaryIndex = mid;
+            right = mid-1;
+        }else if(array[mid] == false){
+            left = mid+1;
+        }
+        }
+        return boundaryIndex;
+    }
 
-
-        return -1;
+    public static void main(String[] args) {
+        boolean [] arr = {false,false, false, false, true, true, true};
+        System.out.println("This is the boundary : " + findBoundary(arr));
     }
 }
