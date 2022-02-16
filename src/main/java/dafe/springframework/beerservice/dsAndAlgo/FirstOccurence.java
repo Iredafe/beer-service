b32cb85cb22aec9848db34c6e559a49672c9f7ec
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-15 23:41:53 -0700

    first occurence - bug fixed

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FirstOccurence.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FirstOccurence.java
@@ -14,6 +14,8 @@ public class FirstOccurence {
                 right = mid -1;
             }else if(arr[mid] < target) {
                 left = mid + 1;
+            }else{
+                right = mid-1;
             }
         }
         return index;
@@ -21,8 +23,12 @@ public class FirstOccurence {
 
     public static void main(String[] args) {
         int [] arr ={1, 3, 3, 3, 3, 6, 10, 10, 10, 100};
+        int [] arr1 ={4, 6, 7, 7, 7, 20};
         int target = 3;
+        int target1 = 8;
 
-        System.out.println("This is the first occurrence : " + firstOccurence(arr, target));
+
+        //System.out.println("This is the first occurrence : " + firstOccurence(arr, target));
+        System.out.println("This is the first occurrence1 : " + firstOccurence(arr1, target1));
     }
 }
