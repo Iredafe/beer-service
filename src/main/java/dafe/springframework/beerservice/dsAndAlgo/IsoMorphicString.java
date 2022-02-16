8b19d36dcf3ff8879974a4f828c45c9f7d5935a7
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-15 19:58:20 -0700

    isomorphic string WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/IsoMorphicString.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/IsoMorphicString.java
@@ -1,4 +1,9 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class IsoMorphicString {
+
+    public static void main(String[] args) {
+        String first = "egg";
+        String second = "add";
+    }
 }
