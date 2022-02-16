f1793ad900b6afee9e4cb49919774cbd6fd3940f
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-15 20:08:44 -0700

    isomorphic string WIP II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/IsoMorphicString.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/IsoMorphicString.java
@@ -1,9 +1,16 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class IsoMorphicString {
 
     public static boolean isomorphicString(String s, String t){
-
+        if(s.length()!=t.length()) return false;
+        Map<Character, Character> map = new HashMap<>();
+        Set<Character> set = new HashSet<>();
         return false;
     }
 
