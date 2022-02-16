44740b9e953bb88b9fdf4e8259e360f0f905abc1
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-15 23:20:32 -0700

    first occurence - test created

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FirstOccurence.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FirstOccurence.java
@@ -1,4 +1,11 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class FirstOccurence {
+
+
+
+    public static void main(String[] args) {
+        int [] arr ={1, 3, 3, 3, 3, 6, 10, 10, 10, 100};
+        int target = 3;
+    }
 }
