8351c9b58cb8db2028c29039d410b497331dd90a
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-16 22:25:16 -0700

    square root WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SquareRoot.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SquareRoot.java
@@ -3,10 +3,16 @@ package dafe.springframework.beerservice.dsAndAlgo;
 public class SquareRoot {
 
     public static int squareRoot(int number){
-
-        return 1;
+        int left = 1; int right = number; int result = -1;
+        while(left <= right){
+            int mid = (left+right)/2;
+        }
+        return result;
     }
 
-
+    public static void main(String[] args) {
+        int input = 16;
+        System.out.println("This is the square root : " + squareRoot(input));
+    }
 
 }
