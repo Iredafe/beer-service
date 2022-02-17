7b0032344e3ae1f3c9258c1fbd7c36695d20e951
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-16 22:47:59 -0700

    find minimum in sorted array WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FindMinimumInRotatedSortedArray.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FindMinimumInRotatedSortedArray.java
@@ -1,4 +1,15 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class FindMinimumInRotatedSortedArray {
+
+    public static int findMinimum(int [] array){
+
+        return 1;
+    }
+
+
+    public static void main(String[] args) {
+        int [] nums ={3, 5, 7, 11, 13, 17, 19, 2};
+        System.out.println("This is the minimum number's index : " + findMinimum(nums));
+    }
 }
