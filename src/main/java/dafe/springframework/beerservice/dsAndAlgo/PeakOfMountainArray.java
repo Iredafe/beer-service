f0644a6e2fb2ebf650e39ec7194bb266c5238b8d
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-17 23:58:36 -0700

    peak of mountain array WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/PeakOfMountainArray.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/PeakOfMountainArray.java
@@ -2,6 +2,11 @@ package dafe.springframework.beerservice.dsAndAlgo;
 
 public class PeakOfMountainArray {
 
+    public static int peak(int [] arr){
+        int result = -1;
+
+        return result;
+    }
     public static void main(String[] args) {
 
     }
