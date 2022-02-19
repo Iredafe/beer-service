7ccf6beb94727b2f79ec693822bee7cff6fb242e
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-18 22:53:29 -0700

    two sum sorted WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/TwoSumSorted.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/TwoSumSorted.java
@@ -6,6 +6,12 @@ public class TwoSumSorted {
         int [] nums = {2, 3, 5, 8, 11, 15};
         int target = 5;
 
-        System.out.println();
+        System.out.println("These are the index that sum to the target : " + twoSumSorted(nums, target));
+    }
+
+    private static int[] twoSumSorted(int[] nums, int target) {
+        int[] indices = new int[2];
+
+        return indices;
     }
 }
