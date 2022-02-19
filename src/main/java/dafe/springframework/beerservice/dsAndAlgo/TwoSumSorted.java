437c4ba535b19c69a8bb2f3973a24551c0fcb7ad
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-18 22:51:22 -0700

    two sum sorted WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/TwoSumSorted.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/TwoSumSorted.java
@@ -1,4 +1,11 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class TwoSumSorted {
+
+    public static void main(String[] args) {
+        int [] nums = {2, 3, 5, 8, 11, 15};
+        int target = 5;
+
+        System.out.println();
+    }
 }
