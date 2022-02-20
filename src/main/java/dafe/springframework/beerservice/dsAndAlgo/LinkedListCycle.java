d7a77f39ee415e1a20509810930c41b4c63b1953
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-19 22:23:44 -0700

    linkedlist cycle WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LinkedListCycle.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LinkedListCycle.java
@@ -2,6 +2,13 @@ package dafe.springframework.beerservice.dsAndAlgo;
 
 public class LinkedListCycle {
 
+    static class Node{
+        int value;
+        int next;
+        public Node(int value){
+            this.value = value;
+        }
+    }
     public static boolean isCycle(Node head){}
 
 }
