bfa376513e68ad251a3c20216e7f3268243ab635
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-20 22:57:27 -0700

    middle of linkedlist WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MiddleOfLinkedList.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MiddleOfLinkedList.java
@@ -1,7 +1,7 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class MiddleOfLinkedList {
-
+ Node head;
     static class Node{
         int value;
         Node next;
@@ -9,5 +9,15 @@ public class MiddleOfLinkedList {
         public Node(int value){
             this.value = value;
         }
+    }
+
+
+    public static void main(String[] args) {
+        MiddleOfLinkedList middleOfLinkedList = new MiddleOfLinkedList();
+        middleOfLinkedList.head = new Node(1);
+        middleOfLinkedList.head.next = new Node(2);
+        middleOfLinkedList.head.next.next = new Node(3);
+        middleOfLinkedList.head.next.next.next = new Node(4);
+        middleOfLinkedList.head.next.next.next.next= new Node(5);
     }
 }
