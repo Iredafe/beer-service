fd8b01d575b20ca543a7ec3379eda5e71ac996d7
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-20 22:53:06 -0700

    middle of linkedlist WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MiddleOfLinkedList.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MiddleOfLinkedList.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class MiddleOfLinkedList {
+
+    static class Node{
+        int value;
+        Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+    }
 }
