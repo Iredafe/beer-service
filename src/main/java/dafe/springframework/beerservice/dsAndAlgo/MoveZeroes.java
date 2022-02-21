5fa3680bf381ec6119449eff4e43e3da7ca11969
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-21 13:42:49 -0700

    moving zeroes WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MoveZeroes.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MoveZeroes.java
@@ -1,4 +1,26 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class MoveZeroes {
+
+    public static void main(String[] args) {
+        int [] nums ={0,1,0,3,12};
+        System.out.println("This is the result : " + moveZeroes(nums));
+    }
+
+    private static int[] moveZeroes(int[] nums) {
+    int start=0; int end = nums.length-1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != 0){
+                nums[start++] = nums[i];
+            }
+        }
+
+        while(start < end){
+            if(nums[start] == 0){
+                nums[start++] = nums[end--];
+            }
+        }
+
+        return nums;
+    }
 }
