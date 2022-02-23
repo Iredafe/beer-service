82dee3deb7832cb4cfc81589e30389e94307ddcf
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-22 17:35:06 -0700

    valid anagrams I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidAnagrams.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidAnagrams.java
@@ -1,4 +1,15 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class ValidAnagrams {
+
+
+    public static void main(String[] args) {
+        String s = "anagram"; String t = "nagaram";
+
+        System.out.println("This is a valid anagram : " + validAnagram(s,t));
+    }
+
+    private static boolean validAnagram(String s, String t) {
+
+    }
 }
