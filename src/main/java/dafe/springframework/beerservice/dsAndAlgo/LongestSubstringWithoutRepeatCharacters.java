25db79adc34cc083568110db865c71216886ad8d
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-22 23:01:11 -0700

    longest substring w/o repeat xters WIP III

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LongestSubstringWithoutRepeatCharacters.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LongestSubstringWithoutRepeatCharacters.java
@@ -6,27 +6,23 @@ import java.util.Set;
 public class LongestSubstringWithoutRepeatCharacters {
 
     public static void main(String[] args) {
-        String input = " abccabcabcc";
+        String input = "abccabcabcc";
         System.out.println("The longest substring is : " + longest(input));
     }
 
     private static int longest(String input) {
-
         Set<Character> window = new HashSet<>();
-        int left = 0; int right = input.length()-1;
+        int left = 0; int right = 0;
         int result = 0;
-            while (left<right){
-                if(!window.contains(right)){
+            while (right<input.length()){
+                if(!window.contains(input.charAt(right))){
                     window.add(input.charAt(right));
                     right++;
-                    result++;
                 }else{
-                    window.remove(input.charAt(right));
+                    window.remove(input.charAt(left));
                     left++;
                 }
-
-                return Math.max(result, right-left);
-
+                result =  Math.max(result, right-left);
             }
 
         return result;
