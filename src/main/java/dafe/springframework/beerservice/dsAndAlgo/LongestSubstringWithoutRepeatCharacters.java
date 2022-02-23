b8c1424b3a2106726474a863691784250f03bfdb
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-22 22:33:31 -0700

    longest substring w/o repeat xters WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LongestSubstringWithoutRepeatCharacters.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LongestSubstringWithoutRepeatCharacters.java
@@ -1,4 +1,14 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class LongestSubstringWithoutRepeatCharacters {
+
+    public static void main(String[] args) {
+        String input = " abccabcabcc";
+        System.out.println("The longest substring is : " + longest());
+    }
+
+    private static int longest() {
+
+        return 1;
+    }
 }
