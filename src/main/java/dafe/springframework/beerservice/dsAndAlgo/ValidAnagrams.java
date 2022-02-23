e913dbf2590fd6e62b2282218ac18442643f929d
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-22 18:47:04 -0700

    valid anagrams IV

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidAnagrams.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ValidAnagrams.java
@@ -17,13 +17,13 @@ public class ValidAnagrams {
         Map<Character, Integer> map = new HashMap<>();
 
         for(int i=0; i<s.length(); i++){
-            map.put(s.charAt(i), map.getOrDefault(i, 0) +1);
-            map.put(t.charAt(i), map.getOrDefault(i, 0) -1);
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) +1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) -1);
 
         }
         for(char letter : map.keySet()){
-
+            if(map.get(letter) != 0 ) return false;
         }
-
+        return true;
     }
 }
