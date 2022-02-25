738defb1f50980507cfcfcfb4ef221db5a5acbf9
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-24 19:58:27 -0700

    lowest common ancestor WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LowestCommonAncestor.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LowestCommonAncestor.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 public class LowestCommonAncestor {
 
+    private TreeNode root;
+
     static class TreeNode{
         int val;
         TreeNode left;
@@ -14,9 +16,14 @@ public class LowestCommonAncestor {
         }
     }
 
-
+//[3,5,1,6,2,0,8,null,null,7,4]
 
     public static void main(String[] args) {
+        LowestCommonAncestor lowestCommonAncestor = new LowestCommonAncestor();
+        lowestCommonAncestor.root = new TreeNode(3);
+        lowestCommonAncestor.root.left = new TreeNode(5);
+        lowestCommonAncestor.root.right = new TreeNode(1);
+        lowestCommonAncestor.root.left.left = new TreeNode(6);
 
     }
 }
