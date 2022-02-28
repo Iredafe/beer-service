a8cd7860dd768a7426b7cf91c5f96a987ceb4a42
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-27 23:06:52 -0700

    find all anagrams WIP VI

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FindAllAnagrams.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FindAllAnagrams.java
@@ -1,10 +1,15 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FindAllAnagrams {
     public static List<Integer> findAnagrams(String s, String p) {
+
+        Map<Character, Integer> pCount = new HashMap();
+        Map<Character, Integer> sCount = new HashMap();
         List<Integer> result =  new ArrayList<>();
         return result;
     }
