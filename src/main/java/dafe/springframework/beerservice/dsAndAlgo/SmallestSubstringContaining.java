f5b0aace33fa1847735a183e22194a8da880a634
Dafe owolabi <iredafeowolabi@gmail.com>
2022-02-28 18:11:06 -0700

    smallest substring containing WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SmallestSubstringContaining.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SmallestSubstringContaining.java
@@ -2,6 +2,11 @@ package dafe.springframework.beerservice.dsAndAlgo;
 
 public class SmallestSubstringContaining {
 
+    public static String smallestSubstring(String bigString, String smallString){
+
+        return "";
+    }
+
     public static void main(String[] args) {
         String big = "abcd$ef$axb$c$";
         String small = "$$abf";
