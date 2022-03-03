cfb3650863f12fe809ad9cfd2ba5ae31eeb74d62
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-02 23:32:23 -0700

    clone graph WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/CloneGraph.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/CloneGraph.java
@@ -1,4 +1,15 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CloneGraph {
+
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
 }
