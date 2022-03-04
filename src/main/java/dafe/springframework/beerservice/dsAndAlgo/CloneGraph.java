9324b15540a0b82395fc2f2c3a039d2de9f57d7f
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-03 22:49:02 -0700

    clone graph WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/CloneGraph.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/CloneGraph.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class CloneGraph {
 
-    class Node {
+    static class Node {
         public int val;
         public List<Node> neighbors;
         public Node() {
@@ -30,6 +30,8 @@ public class CloneGraph {
 
         if(node == null) return null;
         if(visited.containsKey(node)) return visited.get(node);
+
+        Node cloneNode = new Node(node.val, new ArrayList<>());
         return null;
     }
 }
