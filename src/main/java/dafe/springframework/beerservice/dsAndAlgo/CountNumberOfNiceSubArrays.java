a885cbce0406dfb126885f8d2627bda9ce81a725
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-03 23:43:56 -0700

    count number of nice subarrays WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/CountNumberOfNiceSubArrays.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/CountNumberOfNiceSubArrays.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class CountNumberOfNiceSubArrays {
+
+
+    public static void main(String[] args) {
+        int k=3;
+        int nums[] = {1, 1, 2, 1, 1};
+
+        System.out.println("Count of nice sub arrays : " + countNumberOFNiceSubArray(nums, k));
+
+    }
 }
