b1db39a77f121c600660bfa26af1b9c7a1cb336c
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-05 17:14:33 -0700

    binary tree level order traversal WIP III

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BinaryTreeLevelOrderTraversal.java
@@ -17,5 +17,17 @@ public class BinaryTreeLevelOrderTraversal {
     public static void main(String[] args) {
 
          BinaryTreeLevelOrderTraversal tree = new BinaryTreeLevelOrderTraversal();
+
+        tree.root = new BinaryTree(1);
+        tree.root.left = new BinaryTree(2);
+        tree.root.right = new BinaryTree(3);
+        tree.root.left.left = new BinaryTree(4);
+        tree.root.left.right = new BinaryTree(5);
+        tree.root.right.left = new BinaryTree(6);
+        tree.root.right.right=new BinaryTree(7);
+        tree.root.left.left.left = new BinaryTree(8);
+        tree.root.left.left.right = new BinaryTree(9);
+        tree.root.left.right.left = new BinaryTree(10);
+
     }
 }
