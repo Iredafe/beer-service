cfe117d4241875341782584a616882695ab9c457
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-05 17:10:12 -0700

    binary tree level order traversal WIP II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BinaryTreeLevelOrderTraversal.java
@@ -16,5 +16,6 @@ public class BinaryTreeLevelOrderTraversal {
 
     public static void main(String[] args) {
 
+         BinaryTreeLevelOrderTraversal tree = new BinaryTreeLevelOrderTraversal();
     }
 }
