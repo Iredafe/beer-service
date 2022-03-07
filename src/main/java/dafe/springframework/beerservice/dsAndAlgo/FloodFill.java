724b3d48be76d71cd9c166b8c3f49efb60d9dee9
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-06 22:57:49 -0700

    floodfill WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/FloodFill.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/FloodFill.java
@@ -4,6 +4,11 @@ public class FloodFill {
 
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
 
-        return null;
+        return image;
+    }
+
+    public static void main(String[] args) {
+        int image[][] = {{1,1,1},{1,1,0},{1,0,1}};
+        int sr = 1; int sc = 1;
     }
 }
