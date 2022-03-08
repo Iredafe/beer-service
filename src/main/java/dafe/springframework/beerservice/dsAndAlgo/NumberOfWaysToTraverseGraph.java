c51407cae9e4278588a1b1f506accb7691972ba7
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-07 23:34:05 -0700

    unique path WIP done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/NumberOfWaysToTraverseGraph.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/NumberOfWaysToTraverseGraph.java
@@ -27,8 +27,8 @@ public class NumberOfWaysToTraverseGraph {
             Arrays.fill(array, 1);
         }
 
-        for(int i=0; i<height; i++){
-            for(int j=0; j<width; j++){
+        for(int i=1; i<height; i++){
+            for(int j=1; j<width; j++){
                 numberOfUniquePaths[i][j] = numberOfUniquePaths[i-1][j] + numberOfUniquePaths[i][j-1];
             }
         }
