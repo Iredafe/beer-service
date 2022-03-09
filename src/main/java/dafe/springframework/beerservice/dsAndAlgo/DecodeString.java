4bbe8854f125d7a78faa2b091e0a643e9f028380
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-08 23:47:54 -0700

    decode string WIP III

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/DecodeString.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/DecodeString.java
@@ -5,10 +5,13 @@ import java.util.Stack;
 public class DecodeString {
 
     public static String decodeString(String input){
-        Stack<String> stack = new Stack();
+        Stack<Character> stack = new Stack();
 
         for(int i=0; i<input.length(); i++){
-
+            char ch = input.charAt(i);
+            if(ch != ']'){
+                stack.push(ch);
+            }
         }
         return "";
     }
