289ae06d4a82a4b4484c426bf1e289075ba1301b
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-09 22:24:21 -0700

    interval list intersection WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/IntervalListIntersection.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/IntervalListIntersection.java
@@ -5,5 +5,7 @@ public class IntervalListIntersection {
     public static void main(String[] args) {
         int[][] first = {{0,2},{5,10},{13,23},{24,25}};
         int[][] second = {{1,5},{8,12},{15,24},{25,26}};
+
+        System.out.println("This is the intersection : " + findIntersection(first, second));
     }
 }
