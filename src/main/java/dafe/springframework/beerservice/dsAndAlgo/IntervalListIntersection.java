b40220ada851b6116cb3aa47358467cdfe853d49
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-09 22:33:42 -0700

    interval list intersection done

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/IntervalListIntersection.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/IntervalListIntersection.java
@@ -1,6 +1,7 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class IntervalListIntersection {
@@ -13,14 +14,14 @@ public class IntervalListIntersection {
             int low= Math.max(first[i][0], second[j][0]);
             int high= Math.max(first[i][1], second[j][1]);
 
-            if(low <= high){
+            if(low <= high) {
                 result.add(new int[]{low, high});
-
-            if(first[i][1] < second[j][i]){
+            }
+            if(first[i][1] < second[j][1]){
                 i++;
             }else j++;
+
             }
-        }
         return result.toArray(new int[result.size()][]);
     }
 
@@ -30,7 +31,7 @@ public class IntervalListIntersection {
         int[][] first = {{0,2},{5,10},{13,23},{24,25}};
         int[][] second = {{1,5},{8,12},{15,24},{25,26}};
 
-        System.out.println("This is the intersection : " + findIntersection(first, second));
+        System.out.println("This is the intersection : " + Arrays.deepToString(findIntersection(first, second)));
     }
 
 }
