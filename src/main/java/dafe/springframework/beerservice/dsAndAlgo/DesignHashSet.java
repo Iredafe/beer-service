15ca2105647d604321234c201659b5f4f5181570
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-11 22:12:01 -0700

    design hashset WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/DesignHashSet.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/DesignHashSet.java
@@ -2,6 +2,13 @@ package dafe.springframework.beerservice.dsAndAlgo;
 
 public class DesignHashSet {
 
+    public DesignHashSet() {
+
+    }
+
+    public void add(int key) {
+
+    }
 
     public static void main(String[] args) {
 
