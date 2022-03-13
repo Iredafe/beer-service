0b680dea8718f7125f1395a573d0baebcfee6c5f
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-13 16:44:47 -0700

    single number WIP VIII

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/IntersectionOf2Arrays.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/IntersectionOf2Arrays.java
@@ -1,4 +1,10 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class IntersectionOf2Arrays {
+
+
+    public static void main(String[] args) {
+        int [] nums1 = {4,9,5};
+        int [] nums2 = {9,4,9,8,4};
+    }
 }
