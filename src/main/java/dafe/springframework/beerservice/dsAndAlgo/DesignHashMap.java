2cf4960a34f7484b63721999bfb5be9eeb63ab1d
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-12 22:01:38 -0700

    design hashmap WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/DesignHashMap.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/DesignHashMap.java
@@ -1,4 +1,10 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class DesignHashMap {
+    private int key;
+    private int value;
+    private Bucket[] bucketArray;
+    public DesignHashMap(){
+
+    }
 }
