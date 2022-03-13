8de7b978d62f2d3d5aa05065dcdb70a2065208b4
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-13 15:15:24 -0700

    single number WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SingleNumber.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SingleNumber.java
@@ -4,5 +4,10 @@ public class SingleNumber {
 
     public static void main(String[] args) {
         int [] nums = {4,1,2,1,2};
+
+        System.out.println("This is the single number : " + singleNumber(nums));
+    }
+
+    private static String singleNumber(int[] nums) {
     }
 }
