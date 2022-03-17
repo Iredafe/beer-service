90fdb2e3a7560d09447fb25867d9471fed811560
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-16 22:21:19 -0700

    logger rate limiter WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LoggerRateLimiter.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LoggerRateLimiter.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import org.springframework.data.util.Pair;
+
+import java.util.LinkedList;
+
 public class LoggerRateLimiter {
+
+    private LinkedList<Pair<Integer, String >> msgQueue;
+     public LoggerRateLimiter(){
+         msgQueue = new LinkedList<>();
+    }
 }
