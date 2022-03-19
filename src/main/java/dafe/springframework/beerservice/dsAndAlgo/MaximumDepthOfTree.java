e14aeda35da49172bd3aa0998fac29e990db1cff
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-18 22:03:04 -0700

    max depth of tree WIP III

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/MaximumDepthOfTree.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/MaximumDepthOfTree.java
@@ -1,13 +1,19 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class MaximumDepthOfTree {
-    private TreeNode left;
-    private TreeNode right;
+
     static class TreeNode{
         int val;
-
+        private TreeNode left;
+        private TreeNode right;
         public TreeNode(int val){
             this.val = val;
         }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
     }
 }
