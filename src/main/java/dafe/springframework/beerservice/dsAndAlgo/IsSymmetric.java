2bd7b88739ba965e7fa97b382886d0a3507a9704
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-21 20:07:52 -0700

    is symmetric wip II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/IsSymmetric.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/IsSymmetric.java
@@ -2,6 +2,8 @@ package dafe.springframework.beerservice.dsAndAlgo;
 
 public class IsSymmetric {
 
+    private TreeNode _root;
+
     static class TreeNode{
         int val;
         TreeNode left;
@@ -17,8 +19,15 @@ public class IsSymmetric {
         return true;
     }
 
-
+//1,2,2,3,4,4,3
     public static void main(String[] args) {
-
+        IsSymmetric tree = new IsSymmetric();
+        tree._root = new TreeNode(1);
+        tree._root.left = new TreeNode(2);
+        tree._root.right = new TreeNode(2);
+        tree._root.left.left = new TreeNode(3);
+        tree._root.left.right = new TreeNode(4);
+        tree._root.right.left = new TreeNode(4);
+        tree._root.right.right = new TreeNode(3);
     }
 }
