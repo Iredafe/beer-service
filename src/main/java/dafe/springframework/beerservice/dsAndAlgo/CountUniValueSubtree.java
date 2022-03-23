8965536b0a9682f9f526abe04a1634b963384add
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-22 21:10:13 -0700

    count univalue subtree WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/CountUniValueSubtree.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/CountUniValueSubtree.java
@@ -4,6 +4,12 @@ public class CountUniValueSubtree {
 
     static class TreeNode{
         int val;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(){
+
+        }
     }
 
     public static void main(String[] args) {
