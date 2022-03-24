6c42f8832aba35f9b319b02a844661143b3ce680
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-23 22:33:32 -0700

    balanced binary tree WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BalanceBinaryTree.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BalanceBinaryTree.java
@@ -1,4 +1,12 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class BalanceBinaryTree {
+
+    static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+    }
+
 }
