4ed2edf706b3588193e50d28ea7d25c6e6ba82cb
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-27 16:32:04 -0700

    same bst WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SameBST.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SameBST.java
@@ -3,9 +3,17 @@ package dafe.springframework.beerservice.dsAndAlgo;
 import java.util.List;
 
 public class SameBST {
-
+    static class BST{
+        int val;
+        int left;
+        int right;
+    }
     public static boolean isSameBsts(List<Integer> arrayOne, List<Integer> arrayTwo){
 
         return true;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
