ac3d6bdf050e4a50030bdfe02e4be60f7debead9
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-27 16:43:51 -0700

    same bst WIP V

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/SameBST.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/SameBST.java
@@ -5,8 +5,29 @@ import java.util.List;
 
 public class SameBST {
     public static boolean isSameBsts(List<Integer> arrayOne, List<Integer> arrayTwo){
+        return areSameBSTs(arrayOne, arrayTwo, 0, 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private static boolean areSameBSTs(List<Integer> arrayOne,
+                                       List<Integer> arrayTwo,
+                                       int rootIdxOne,
+                                       int rootIdxTwo,
+                                       int minValue,
+                                       int maxValue) {
+
+
+        int leftRootIdxOne = getSmallerRootIdx(arrayOne, rootIdxOne, maxValue);
+        int leftRootIdxTwo = getSmallerRootIdx(arrayTwo, rootIdxTwo, maxValue);
+        int rightRootIdxOne = getBiggerOrEqual(arrayOne, minValue, rootIdxOne);
+        int rightRootIdxTwo = getBiggerOrEqual(arrayTwo, minValue, rootIdxTwo);
+
 
         return true;
+    }
+
+    private static int getBiggerOrEqual(List<Integer> arrayTwo, int minValue, int rootIdxTwo) {
+
+        return 1;
     }
 
     public static void main(String[] args) {
