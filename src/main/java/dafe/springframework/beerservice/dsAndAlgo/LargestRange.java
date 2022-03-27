7fa363ebe819c3b08de481c4514af02e29c2ffe4
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-26 22:15:22 -0700

    largest range WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LargestRange.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LargestRange.java
@@ -2,4 +2,8 @@ package dafe.springframework.beerservice.dsAndAlgo;
 
 public class LargestRange {
 
+    public static void main(String[] args) {
+        int [] input = {1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6};
+        System.out.println("This is the largest range : " + largestRange(input));
+    }
 }
