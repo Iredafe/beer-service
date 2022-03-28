5bb8b3498403d7b9dff60c74ff2da45a1f2ce675
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-27 22:20:51 -0700

    quick select WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/QuickSelect.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/QuickSelect.java
@@ -1,4 +1,8 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class QuickSelect {
+
+    public static void main(String[] args) {
+        System.out.println("This is the kth smallest number : " + quickselect(input));
+    }
 }
