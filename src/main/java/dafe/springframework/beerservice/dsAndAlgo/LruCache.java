0863bca88d87a86608b7b63c0446a5ce8f7e8a4d
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-28 22:45:22 -0700

    Lru cache WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LruCache.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LruCache.java
@@ -1,10 +1,15 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class LruCache {
+    int capacity;
     static class DoubleLinkedNode{
         int key;
         int value;
         DoubleLinkedNode next;
         DoubleLinkedNode prev;
+    }
+
+    public LruCache(int capacity){
+        this.capacity = capacity;
     }
 }
