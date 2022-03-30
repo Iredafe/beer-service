bd48fb52f8c67559b4b8036d6c92a3339f441be8
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-30 15:07:55 -0700

    construct bst from preorder WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/ConstructBstFromPreOrderArray.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/ConstructBstFromPreOrderArray.java
@@ -1,4 +1,14 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class ConstructBstFromPreOrderArray {
+
+    public static void main(String[] args) {
+        int [] preorder = {8,5,1,7,10,12};
+
+        System.out.println("This is the constructed bst : " + bstFromPreorder(preorder));
+    }
+
+    private static TreeNode bstFromPreorder(int[] preorder) {
+        return null;
+    }
 }
