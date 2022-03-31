c1f8692816661879a47c4fb44fe0257754da0890
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-30 22:41:55 -0700

    construct bst from preorder and inorder WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BSTFromInorderAndPreorder.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BSTFromInorderAndPreorder.java
@@ -1,4 +1,9 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class BSTFromInorderAndPreorder {
+
+    public static void main(String[] args) {
+        int [] preorder = {3,9,20,15,7};
+        int [] inorder = {9,3,15,20,7};
+    }
 }
