0dfa0d8e7e4da9859521e523e4591abfb23f7d60
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-30 22:43:07 -0700

    construct bst from preorder and inorder WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BSTFromInorderAndPreorder.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BSTFromInorderAndPreorder.java
@@ -5,5 +5,8 @@ public class BSTFromInorderAndPreorder {
     public static void main(String[] args) {
         int [] preorder = {3,9,20,15,7};
         int [] inorder = {9,3,15,20,7};
+
+        System.out.println("This is the binary tree constructed from inorder and preorder arrays : "
+                + arraysToTree(preorder, inorder));
     }
 }
