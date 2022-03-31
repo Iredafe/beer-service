6f30a0f21a1fb4c266fef26b03e409a70641be41
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-30 22:49:18 -0700

    construct bst from preorder and inorder WIP VI

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BSTFromInorderAndPreorder.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BSTFromInorderAndPreorder.java
@@ -1,6 +1,20 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BSTFromInorderAndPreorder {
+   static Map<Integer, Integer> map;
+
+    private static TreeNode arraysToTree(int left, int right, int[] preorder) {
+
+        return null;
+    }
+
+    private static TreeNode buildTree(int preorder[], int [] inorder){
+        map = new HashMap();
+        return arraysToTree(0, preorder.length-1, inorder);
+    }
 
     public static void main(String[] args) {
         int [] preorder = {3,9,20,15,7};
@@ -10,13 +24,5 @@ public class BSTFromInorderAndPreorder {
                 + buildTree( preorder, inorder));
     }
 
-    private static TreeNode arraysToTree(int left, int right, int[] preorder) {
 
-        return null;
-    }
-
-    private static TreeNode buildTree(int preorder[], int [] inorder){
-
-        return arraysToTree(0, preorder.length-1, inorder);
-    }
 }
