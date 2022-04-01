fcdedac3e44536af1a500b14edd6d4f0002f2ea8
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-31 21:07:03 -0700

    construct bst from postorder and inorder WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/BinaryTreeFromPostorderAndInorder.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/BinaryTreeFromPostorderAndInorder.java
@@ -1,4 +1,9 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class BinaryTreeFromPostorderAndInorder {
+
+    public TreeNode buildTree(int [] postoreder, int [] inorder){
+
+        return arrayToTree(0, postoreder.length-1, postoreder);
+    }
 }
