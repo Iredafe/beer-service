0116c5cb3507e8cd821c554b591d0deb289d8f08
Dafe owolabi <iredafeowolabi@gmail.com>
2022-03-31 22:38:32 -0700

    populate next right pointers in each node WIP I

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/PopulateNextRightPointersInEachNode.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/PopulateNextRightPointersInEachNode.java
@@ -1,9 +1,14 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class PopulateNextRightPointersInEachNode {
 
     public TreeNode populateNext(TreeNode root){
 
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
         return root;
     }
 }
