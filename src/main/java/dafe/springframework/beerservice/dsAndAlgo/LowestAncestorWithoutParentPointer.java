93836b7245d7b8cdbc8c3911b9d37ea2a8460deb
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-02 22:30:08 -0700

    lowest common ancestor without parent pointer WIP VI

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LowestAncestorWithoutParentPointer.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LowestAncestorWithoutParentPointer.java
@@ -32,6 +32,16 @@ public class LowestAncestorWithoutParentPointer {
 
             TreeNode node = stack.pop();
 
+            if(node.left!=null){
+                parent.put(node.left, node);
+                stack.push(node.left);
+            }
+
+            if(node.right!=null){
+                parent.put(node.right, node);
+                stack.push(node.right);
+            }
+
         }
 
 
