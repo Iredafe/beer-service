5c5a30bf8c51b25112aca84f9a5c24dba9d87b4b
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-02 22:17:35 -0700

    lowest common ancestor without parent pointer WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LCAWithoutParentPointer.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LCAWithoutParentPointer.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class LCAWithoutParentPointer {
+
+    static class TreeNode{
+        int val;
+        LowestCommonAncestor.TreeNode left;
+        LowestCommonAncestor.TreeNode right;
+        public TreeNode(int val){
+            this.val = val;
+        }
+    }
 }
