e967a800ef8a86dc5dbeb710152483e01ccd0983
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-04 20:17:28 -0700

    lowest common manager WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LowestCommonManager.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LowestCommonManager.java
@@ -1,4 +1,12 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class LowestCommonManager {
+
+    static class OrgChart {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
