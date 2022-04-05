f5950062e4d8c98db2d87769a2c9fb96d0cded66
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-04 20:21:41 -0700

    lowest common manager WIP II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/LowestCommonManager.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/LowestCommonManager.java
@@ -1,12 +1,18 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
+import org.aspectj.weaver.ast.Or;
+
 import java.util.List;
 
 public class LowestCommonManager {
-    static char name;
-    static List<OrgChart> directReports;
     static class OrgChart {
+        char name;
+        List<OrgChart> directReports;
 
+        OrgChart(char name, List<OrgChart> directReports){
+            this.name = name;
+            this.directReports = directReports;
+        }
     }
 
     public static void main(String[] args) {
