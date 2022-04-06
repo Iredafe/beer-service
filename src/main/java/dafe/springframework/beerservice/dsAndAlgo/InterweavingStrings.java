0f12c6de0ea05beb4a05509017412cb0718b1d54
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-05 22:16:50 -0700

    interweaving strings WIP II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/InterweavingStrings.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/InterweavingStrings.java
@@ -3,13 +3,24 @@ package dafe.springframework.beerservice.dsAndAlgo;
 public class InterweavingStrings {
 
     public static boolean isInterveavingStringRecursion(String first, String second, String third){
-
-        return false;
+        if(third.length() != first.length() + second.length()) return false;
+        return helper(first, second, third, 0, 0);
     }
 
     public static boolean helper(String first, String second, String third, int i, int j){
+        int k = i+ j;
 
-        return true;
+        if(k == third.length()) return true;
+
+        if(i<first.length() && first.charAt(i) == third.charAt(k)){
+            if(helper(first, second, third, i+1, j)) return true;
+        }
+
+        if(j<second.length() && second.charAt(j) == third.charAt(k)){
+            return helper(first, second, third, i, j+1);
+        }
+
+        return false;
     }
 
     public static void main(String[] args) {
