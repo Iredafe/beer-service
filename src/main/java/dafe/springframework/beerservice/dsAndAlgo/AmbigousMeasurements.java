e1794a90052911ade2668d83ee6509cff10d5405
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-06 22:36:17 -0700

    ambiguos measurements WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/AmbigousMeasurements.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/AmbigousMeasurements.java
@@ -1,4 +1,9 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class AmbigousMeasurements {
+
+    public static boolean ambiguousMeasurements(int [][] measuringCups, String low, String high){
+
+        return true;
+    }
 }
