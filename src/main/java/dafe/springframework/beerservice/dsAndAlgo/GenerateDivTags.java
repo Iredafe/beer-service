69cb556823fb38f77d612290e032d8a01bb94cc9
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-07 22:06:22 -0700

    generate div tags WIP II

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/GenerateDivTags.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/GenerateDivTags.java
@@ -5,8 +5,9 @@ import java.util.ArrayList;
 public class GenerateDivTags {
 
     private static ArrayList<String> generateDivTags(int numberOfTags) {
-
-        return null;
+        ArrayList<String> matchedDivTags = new ArrayList<>();
+        generateTagsFromPrefix(numberOfTags, numberOfTags, "", matchedDivTags);
+        return matchedDivTags;
     }
 
     public static void main(String[] args) {
