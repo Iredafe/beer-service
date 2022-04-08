6ed195ef36fddc4f39389e9a3d313d838bbc1b39
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-07 22:04:12 -0700

    generate div tags WIP

--- src/main/java/dafe/springframework/beerservice/dsAndAlgo/GenerateDivTags.java
+++ src/main/java/dafe/springframework/beerservice/dsAndAlgo/GenerateDivTags.java
@@ -1,4 +1,11 @@
 package dafe.springframework.beerservice.dsAndAlgo;
 
 public class GenerateDivTags {
+
+
+    public static void main(String[] args) {
+        int numberOfTags = 3;
+
+        System.out.println("These are the generated div tags :" + generateDivTags(numberOfTags));
+    }
 }
