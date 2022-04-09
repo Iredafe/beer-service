25a28608cf96c7426a2598175e223c95b1e2bafa
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-09 15:59:24 -0700

    license key formatter WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/LicenseKeyFormatter.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/LicenseKeyFormatter.java
@@ -1,4 +1,10 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class LicenseKeyFormatter {
+
+
+    public static void main(String[] args) {
+        String input1 = "5F3Z-2e-9-w"; int k1 = 4;
+        String input2 = "2-5g-3-J"; int k2 = 2;
+    }
 }
