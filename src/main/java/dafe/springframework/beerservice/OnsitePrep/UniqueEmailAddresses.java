155d6b956ccbac8c795c7af4c039f7c479937545
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-08 21:53:29 -0700

    unique email address WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/UniqueEmailAddresses.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/UniqueEmailAddresses.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class UniqueEmailAddresses {
+
+
+    public static void main(String[] args) {
+        String [] emails = {"test.email+alex@leetcode.com",
+                "test.e.mail+bob.cathy@leetcode.com",
+                "testemail+david@lee.tcode.com"};
+
+        System.out.println("This is the number of unique address : " + uniqueEmailAddress(emails) );
+    }
 }
