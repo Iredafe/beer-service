64104ce83d15f62d0c9d25a3f5ad817cfef73197
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-09 21:34:58 -0700

    fruit into basket WIP I

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/FruitsIntoBasket.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/FruitsIntoBasket.java
@@ -6,4 +6,10 @@ public class FruitsIntoBasket {
 
         return 0;
     }
+
+    public static void main(String[] args) {
+        int [] input = {1,2,3,2,2};
+
+        System.out.println("This is the number of fruit that got into the basket : " + fruitIntoBasket(input));
+    }
 }
