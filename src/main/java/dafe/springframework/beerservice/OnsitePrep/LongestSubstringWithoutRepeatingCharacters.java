fb5eb7b9a9c469c34e2d2ad80880e5b8573ca36e
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-10 12:39:49 -0700

    longest substring without repeating characters WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,4 +1,10 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubstringWithoutRepeatingCharacters {
+
+    int left=0; int right=0; int maxLength=0;
+    Map<Character, Integer> slidingWindow = new HashMap();
 }
