92d158458ecdf765fcc92fa921c2f2bceda97a11
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-09 21:11:07 -0700

    LongestSubstringWithAtMost2RepeatingCharacters WIP I

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/LongestSubstringWithAtMost2RepeatingCharacters.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/LongestSubstringWithAtMost2RepeatingCharacters.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class LongestSubstringWithAtMost2RepeatingCharacters {
+
+
+    private static String longestSubstringWith2RepeatingCharacters(String string) {
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String string = "ccaabbb";
+        System.out.println("The longest substring with at most 2 characters is of length : "
+                + longestSubstringWith2RepeatingCharacters(string));
+    }
 }
