66daf2f1de869a2026439b79fd95c97dfd38cb52
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-09 22:35:16 -0700

    simplify path WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/SimplifyPath.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/SimplifyPath.java
@@ -1,4 +1,11 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class SimplifyPath {
+
+
+    public static void main(String[] args) {
+        String directory = "/foo/../test/../test/../foo//bar/./baz";
+
+        System.out.println("This is the simplified version of the directory you entered : " + simplifyPath(directory));
+    }
 }
