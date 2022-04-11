95d427b8e4e8ea2d70062581a78ded2574f065e0
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-10 22:26:38 -0700

    3 sum WIP II

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/ThreeSum.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/ThreeSum.java
@@ -1,6 +1,7 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ThreeSum {
@@ -8,6 +9,13 @@ public class ThreeSum {
 
     public static List<List<Integer>> threeSum(int nums[]){
         List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for(int i=0; i< nums.length && nums[i] <=0 ; i++){
+            if(i==0 || nums[i] != nums[i-1]){
+                twoSum(i, nums, result);
+            }
+        }
         return result;
     }
 
