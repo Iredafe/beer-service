749e40c6df2e29a7e47ef1e4c9848e4ded5da732
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-10 22:24:21 -0700

    3 sum WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/ThreeSum.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/ThreeSum.java
@@ -2,5 +2,9 @@ package dafe.springframework.beerservice.OnsitePrep;
 
 public class ThreeSum {
 
+    public static void main(String[] args) {
+        int array[] = {-1,0,1,2,-1,-4};
+        System.out.println("These are the 3 numbers that sum up to zero: " + threeSum(array));
+    }
 
 }
