c372d0f823a8c4ae30e2f9805287fce31688f409
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-10 21:37:35 -0700

    container with most water WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/ContainerWithMostWater.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/ContainerWithMostWater.java
@@ -1,4 +1,9 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class ContainerWithMostWater {
+
+    public static void main(String[] args) {
+        int [] input = {1,8,6,2,5,4,8,3,7};
+        System.out.println("This is the maximum area : " + containerWithMostWater(input));
+    }
 }
