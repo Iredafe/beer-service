c6d838cbb78a8dacb4ff7b9b704cb00d63a279fa
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-10 22:54:59 -0700

    3 sum with hashset done

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/ThreeSum.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/ThreeSum.java
@@ -69,6 +69,7 @@ public class ThreeSum {
     public static void main(String[] args) {
         int array[] = {-1,0,1,2,-1,-4};
         System.out.println("These are the 3 numbers that sum up to zero: " + threeSum(array));
+        System.out.println("These are the 3 numbers that sum up to zero: " + threeSumWithHashSet(array));
     }
 
 }
