c20935e20494dd6961ef2df379da3d2dc4b8ef9b
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-10 17:41:55 -0700

    longest substring without repeating characters IV

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,8 +1,10 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
-
+//not working as expected
 public class LongestSubstringWithoutRepeatingCharacters {
 
     public static int longestSubstring(String input){
@@ -23,24 +25,28 @@ public class LongestSubstringWithoutRepeatingCharacters {
     }
 
     public static String longestSubstringWithoutRepeatingCharacters(String input){
-        Set<Character> slidingWindow = new HashSet();
+        HashMap<Character, Integer> slidingWindow = new HashMap<>();
         int left=0; int right=0; int max =0;
 
         while(right<input.length()){
-            if(slidingWindow.contains(input.charAt(right))){
-                slidingWindow.remove(input.charAt(left));
+
+            if(!slidingWindow.containsKey(input.charAt(right))){
+                slidingWindow.put(input.charAt(right), right);
+                right++;
+            }else {
+                int smallestIndex = Collections.min(slidingWindow.values());
+                slidingWindow.remove(input.charAt(smallestIndex));
+                slidingWindow.put(input.charAt(right), right);
                 left++;
-            }else{
-                slidingWindow.add(input.charAt(right));
-                max = Math.max(max, right-left+1);
                 right++;
             }
+
         }
-        return input.substring(left, max);
+        return input.substring(left+1, right-left);
     }
 
     public static void main(String[] args) {
-        String string = "pwwkew";
+        String string = "clementisacap";
         System.out.println("This is the length of longest non-repeating substring : " + longestSubstring(string));
         System.out.println("This is the length of longest non-repeating substring : "
                 + longestSubstringWithoutRepeatingCharacters(string));
