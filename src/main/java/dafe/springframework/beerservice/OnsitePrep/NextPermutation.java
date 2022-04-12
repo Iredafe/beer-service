f51b050d4c4730d05c5c7de9fc5c3a6533d8ff15
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-11 20:34:44 -0700

    next permutation done I

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/NextPermutation.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/NextPermutation.java
@@ -3,16 +3,16 @@ package dafe.springframework.beerservice.OnsitePrep;
 import java.util.Arrays;
 
 public class NextPermutation {
-
+//1,5,8,4,7,6,5,3,1
     public static int[] findNextPermutation(int [] inputArray){
         int i = inputArray.length - 2;
 
-        while(i>=0 && inputArray[i+1] < inputArray[i]){
+        while(i>=0 && inputArray[i+1] <= inputArray[i]){
             i--;
         }
         if(i>=0){
             int j = inputArray.length-1;
-            while(inputArray[j] < inputArray[i]){
+            while(inputArray[j] <= inputArray[i]){
                 j--;
             }
             swap(inputArray, i, j);
@@ -23,7 +23,11 @@ public class NextPermutation {
 
     private static void reverse(int [] array, int start){
         int i=start+1; int j=array.length-1;
-        swap(array, i, j);
+       while(i<j) {
+           swap(array, i, j);
+           i++;
+           j--;
+       }
     }
 
     private static void swap(int [] nums, int i, int j){
@@ -36,5 +40,6 @@ public class NextPermutation {
         int [] array = {1,2,3};
 
         System.out.println("This is the next permutation : " + Arrays.toString(findNextPermutation(array)));
+        System.out.println("This is the next permutation II: " + Arrays.toString(findNextPermutation(new int[]{1,5,8,4,7,6,5,3,1})));
     }
 }
