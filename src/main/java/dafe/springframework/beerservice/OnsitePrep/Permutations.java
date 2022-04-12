619b147a2c79cd3aabaa3caea2c764a3f336ad9a
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-11 22:01:57 -0700

    permutations WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/Permutations.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/Permutations.java
@@ -2,4 +2,8 @@ package dafe.springframework.beerservice.OnsitePrep;
 
 public class Permutations {
 
+    public static void main(String[] args) {
+        int [] nums = {1,2,3};
+        System.out.println("These are the permutations : " + permute(nums));
+    }
 }
