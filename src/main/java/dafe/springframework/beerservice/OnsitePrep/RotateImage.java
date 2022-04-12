26a00724e84a4ff25dd6d764aff93c3420650ef6
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-11 23:00:04 -0700

    rotate image WIP III

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/RotateImage.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/RotateImage.java
@@ -13,7 +13,9 @@ public class RotateImage {
     public static void transpose(int [][] matrix){
         for(int i=0; i< matrix.length; i++){
             for(int j=i; j<matrix.length; j++){
-
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
     }
