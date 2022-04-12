273e93985644405b75f2bfc42439e5215c205577
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-11 22:04:01 -0700

    permutations WIP I

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/Permutations.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/Permutations.java
@@ -1,6 +1,19 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Permutations {
+
+    public static List<List<Integer>> permute(int [] array){
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        for(int number : array){
+            list.add(number);
+        }
+        getAllPermutations(list, result, 0);
+        return result;
+    }
 
     public static void main(String[] args) {
         int [] nums = {1,2,3};
