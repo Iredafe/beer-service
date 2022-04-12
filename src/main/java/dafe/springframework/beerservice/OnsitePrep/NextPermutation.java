dbaad666668ad1e0eafa25396e19a17b3d481db8
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-11 19:19:47 -0700

    next permutation WIP I

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/NextPermutation.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/NextPermutation.java
@@ -2,7 +2,10 @@ package dafe.springframework.beerservice.OnsitePrep;
 
 public class NextPermutation {
 
-
+    public static int[] findNextPermutation(int [] inputArray){
+        int i = inputArray.length - 2;
+        return new int[]{};
+    }
 
     public static void main(String[] args) {
         int [] array = {1,2,3};
