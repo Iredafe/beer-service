6856c687bc18a3fb18db4374f5e1ad5373666d36
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-12 19:13:19 -0700

    jump game WIP I

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/JumpGame.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/JumpGame.java
@@ -2,6 +2,9 @@ package dafe.springframework.beerservice.OnsitePrep;
 
 public class JumpGame {
 
+    public static boolean canJump(int nums []){
+        canJumpFromPosition(nums, 0);
+    }
 
     public static void main(String[] args) {
         int [] array = {2,3,1,1,4};
