34c4a2f9fd209093428620a9edeedb108b54f334
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-12 22:16:12 -0700

    jump game done with top-bottom DP done

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/JumpGame.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/JumpGame.java
@@ -29,7 +29,7 @@ public class JumpGame {
             return memo[position] == Index.GOOD;
         }
         int furthestPosition = Math.min(position+nums[position] , nums.length-1);
-        for(int nextPosition = 0; nextPosition<=furthestPosition; nextPosition++){
+        for(int nextPosition = position+1; nextPosition<=furthestPosition; nextPosition++){
             if(jumpGameHelper(nextPosition, nums)){
                 memo[position] = Index.GOOD;
                 return true;
@@ -40,11 +40,13 @@ public class JumpGame {
  }
 
  public static boolean jumpGame(int [] nums){
-     Index[] memo = new Index[nums.length];
+      memo = new Index[nums.length];
 
      for(int i=0;i< memo.length; i++){
          memo[i] = Index.UNKNOWN;
      }
+     memo[nums.length-1] = Index.GOOD;
+     return jumpGameHelper(0, nums);
  }
     public static void main(String[] args) {
         int [] array = {2,3,1,1,4};
@@ -52,5 +54,7 @@ public class JumpGame {
 
         System.out.println("Can I jump to the end of the array? " +canJump(array));
         System.out.println("Can I jump to the end of the array2 ? " +canJump(array1));
+        System.out.println("Can I jump to the end of the array? " +jumpGame(array));
+        System.out.println("Can I jump to the end of the array2 ? " +jumpGame(array1));
     }
 }
