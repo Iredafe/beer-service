309941eb7bd17740aee0a824fbea285563a053af
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-13 20:49:17 -0700

    plus one WIP I

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/PlusOne.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/PlusOne.java
@@ -2,6 +2,21 @@ package dafe.springframework.beerservice.OnsitePrep;
 
 public class PlusOne {
 
+    private static int[] plusOne(int array[]){
+
+        for(int i= array.length-1; i>=0; i--){
+            if(array[i] == 9){
+                array[i] = 0;
+            }else{
+                array[i]++;
+                return array;
+            }
+        }
+        array = new int[array.length+1];
+        array[0]=1;
+        return array;
+    }
+
     public static void main(String[] args) {
         int [] digits = {1, 2, 3};
 
