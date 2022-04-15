6deab583ad80645ee7bbaa22da6b8dd6eae24f83
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-15 15:16:52 -0700

    read n characters given read4 done.

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/ReadNCharactersGivenRead4.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/ReadNCharactersGivenRead4.java
@@ -6,7 +6,7 @@ public class ReadNCharactersGivenRead4 {
         int copiedChar = 0; int readChars = 4;
         char [] buf4 = new char[4];
 
-        while(copiedChar < n && readChars<4){
+        while(copiedChar < n && readChars==4){
             readChars = read4(buf4);
 
             for(int i=0; i<readChars; i++){
