87279af1c9a1500bf23c7ef6a6fa2f0c4a7f85da
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-14 22:26:08 -0700

    minimum window substring WIP VIII

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/MinimumWindowSubstring.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/MinimumWindowSubstring.java
@@ -16,33 +16,35 @@ public class MinimumWindowSubstring {
         int requiredSize = map.size();
         HashMap<Character, Integer> slidingWindowMap = new HashMap<>();
         int left=0; int right=0; int formed = 0;
-        int answer[] = {-1, 0, 0};
+        int[] answer = {-1, 0, 0};
         while(right< longString.length()){
             char character = longString.charAt(right);
             int count = slidingWindowMap.getOrDefault(character, 0);
-            slidingWindowMap.put(character, count);
+            slidingWindowMap.put(character, count+1);
 
-            if(slidingWindowMap.containsKey(character) && map.get(character).equals(slidingWindowMap.get(character))){
+            if(map.containsKey(character) && map.get(character).equals(slidingWindowMap.get(character))){
                 formed++;
             }
 
-            while(left<right && formed == requiredSize){
-                char leftCharacter = longString.charAt(left);
+            while(left<=right && formed == requiredSize){
+               character = longString.charAt(left);
                if(answer[0] == -1 || (right-left+1) < answer[0]){
                    answer[0] = right-left+1;
                    answer[1] = left;
                    answer[2] = right;
                }
               slidingWindowMap.put(character, slidingWindowMap.get(character)-1);
-               if(slidingWindowMap.containsKey(leftCharacter) && slidingWindowMap.get(leftCharacter).intValue() < map.get(leftCharacter)){
+               if(map.containsKey(character) &&
+                       slidingWindowMap.get(character).intValue() < map.get(character)){
                    formed--;
                }
                left++;
             }
-        }
             right++;
+        }
 
-        return answer[0]==0?"":longString.substring(answer[1], answer[right]+1);
+
+        return answer[0]==-1?"":longString.substring(answer[1], answer[2]+1);
     }
 
     public static void main(String[] args) {
