b7d4977d0ccfaaf76e54200a2050b1ab01938024
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-14 21:41:00 -0700

    minimum window substring WIP I

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/MinimumWindowSubstring.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/MinimumWindowSubstring.java
@@ -2,8 +2,14 @@ package dafe.springframework.beerservice.OnsitePrep;
 
 public class MinimumWindowSubstring {
 
+    private static String findMinimumWindowSubstring(String longString, String shortString) {
+
+        return ""
+    }
+
     public static void main(String[] args) {
-        String string = "ADOBECODEBANC";
-        System.out.println("This is the minimum window : " + findMinimumWindowSubstring());
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+        System.out.println("This is the minimum window : " + findMinimumWindowSubstring(s, t));
     }
 }
