4c0d90c7c5084991e66c7e6148b2f84686559b27
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-14 21:45:51 -0700

    minimum window substring WIP II

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/MinimumWindowSubstring.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/MinimumWindowSubstring.java
@@ -1,10 +1,20 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
+import java.util.HashMap;
+
 public class MinimumWindowSubstring {
 
     private static String findMinimumWindowSubstring(String longString, String shortString) {
+        if(longString.length()==0 || shortString.length()==0) return "";
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<shortString.length(); i++){
+            char character = shortString.charAt(i);
+            int count = map.getOrDefault(character, 0);
+            map.put(character, count+1);
+        }
 
-        return ""
+        HashMap<Character, Integer> slidingWindowMap = new HashMap<>();
+        return "";
     }
 
     public static void main(String[] args) {
