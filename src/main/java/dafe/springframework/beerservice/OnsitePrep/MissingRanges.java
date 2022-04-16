3f2cd628d5c06a36a4246778be7f7174fd52c4b8
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-15 21:50:13 -0700

    missing ranges WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/MissingRanges.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/MissingRanges.java
@@ -1,4 +1,9 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class MissingRanges {
+
+    public static void main(String[] args) {
+        int [] array = {0,1,3,50,75};
+        System.out.println("These are the missing ranges : " + findMissingRanges(array));
+    }
 }
