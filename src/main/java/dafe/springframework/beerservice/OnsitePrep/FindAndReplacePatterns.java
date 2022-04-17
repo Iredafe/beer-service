bab469311ce81f14b71b548bfc5e4a827d4a34a4
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-16 21:55:13 -0700

    find and replace patterns WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/FindAndReplacePatterns.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/FindAndReplacePatterns.java
@@ -3,4 +3,11 @@ package dafe.springframework.beerservice.OnsitePrep;
 public class FindAndReplacePatterns {
 
 
+    public static void main(String[] args) {
+        String words []=new String[]{"abc", "deq", "mee", "aqq", "dkd", "ccc"};
+        String pattern = "abb";
+
+        System.out.println("These are the words that match the pattern : " + findAndReplace(words, pattern));
+
+    }
 }
