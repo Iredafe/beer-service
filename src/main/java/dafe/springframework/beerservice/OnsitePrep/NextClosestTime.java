da2dfd69a8510f82782a36280691a2f53c9dfa38
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-16 17:29:06 -0700

    next closest time WIP I

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/NextClosestTime.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/NextClosestTime.java
@@ -1,7 +1,9 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class NextClosestTime {
-
+    private static String nextClosestTime(String time) {
+        return null;
+    }
 
     public static void main(String[] args) {
         String time = "19:34";
