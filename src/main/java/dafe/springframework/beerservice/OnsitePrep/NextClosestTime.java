8d3d4e1b8ece4ce42b455903e049056701b7abf9
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-16 17:34:49 -0700

    next closest time WIP II

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/NextClosestTime.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/NextClosestTime.java
@@ -1,13 +1,29 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class NextClosestTime {
     private static String nextClosestTime(String time) {
+        int minutes = Integer.parseInt(time.substring(0,2)) * 60;
+        minutes+=Integer.parseInt(time.substring(3));
+
+        Set<Integer> set = new HashSet<>();
+        for(char character : time.toCharArray()){
+            set.add(character - '0');
+        }
+
+        while(true){
+            minutes++;
+
+        }
+
         return null;
     }
 
     public static void main(String[] args) {
         String time = "19:34";
-        System.out.println("The next closest time to " + time + "is : " + nextClosestTime(time));
+        System.out.println("The next closest time to " + time + " is : " + nextClosestTime(time));
     }
 
 }
