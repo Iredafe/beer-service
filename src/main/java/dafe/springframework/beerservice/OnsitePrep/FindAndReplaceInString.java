c7f3aa2e55c377eda0ff6fcf0c9fceec9e712ba0
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-16 22:43:10 -0700

    find and replace in string WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/FindAndReplaceInString.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/FindAndReplaceInString.java
@@ -1,4 +1,15 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class FindAndReplaceInString {
+
+
+    public static void main(String[] args) {
+        String wordString = "abcd";
+        int [] indices = {0, 2};
+        String [] target = {"eee", "ffff"};
+        String [] sources = {"a", "cd"};
+
+        System.out.println("This is the new string after replacement operation was done : "
+        + findAndReplaceInString(wordString, indices, sources, target));
+    }
 }
