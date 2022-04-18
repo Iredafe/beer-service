feb147a175e42d19ac4e127509145f5b0e04bc14
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-17 21:11:32 -0700

    maximum distance to closest person WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/MaximumDistanceToClosestPerson.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/MaximumDistanceToClosestPerson.java
@@ -1,4 +1,9 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class MaximumDistanceToClosestPerson {
+
+    public static void main(String[] args) {
+        int seats[] = {1,0,0,0,1,0,1};
+        System.out.println("This is the maximum distance to closest person : " + maximumDistanceToClosestPerson(seats));
+    }
 }
