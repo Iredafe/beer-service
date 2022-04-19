7b594756c2cf5d191947d5cb19257ac974ffca27
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-18 17:27:42 -0700

    merge 2 sorted lists WIP I

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/Merge2SortedLists.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/Merge2SortedLists.java
@@ -2,7 +2,7 @@ package dafe.springframework.beerservice.OnsitePrep;
 
 public class Merge2SortedLists {
 
-    static class ListNode{
+      static class ListNode{
         int value;
         ListNode next;
         ListNode previous;
@@ -10,5 +10,14 @@ public class Merge2SortedLists {
         public ListNode(int value){
             this.value = value;
         }
+
+        public static ListNode mergeTwoSortedLists(ListNode first, ListNode second){
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Merge2SortedLists node = new Merge2SortedLists();
+
     }
 }
