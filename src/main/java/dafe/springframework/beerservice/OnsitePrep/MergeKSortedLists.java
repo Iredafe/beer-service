906abae7dd03ae5f0cef89898cd13476ad0eaa3b
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-18 20:52:46 -0700

    merge k sorted lists WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/MergeKSortedLists.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/MergeKSortedLists.java
@@ -1,4 +1,27 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
+import java.util.List;
+
 public class MergeKSortedLists {
+
+    static class ListNode{
+        int val;
+        ListNode next;
+
+        public ListNode(int val){
+            this.val = val;
+        }
+    }
+
+    public static ListNode mergeTwoLists(ListNode first, ListNode second){
+
+        ListNode answer = new ListNode(-1);
+        ListNode previous = answer;
+
+        while(first!=null && second!=null){
+            if(first.val <= second.val){
+
+            }
+        }
+    }
 }
