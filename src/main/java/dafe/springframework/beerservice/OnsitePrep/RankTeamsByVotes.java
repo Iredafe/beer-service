d1edab34f39aa4bae00711ea8bcca27037a1566c
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-19 22:44:09 -0700

    rank teams by vote WIP I

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/RankTeamsByVotes.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/RankTeamsByVotes.java
@@ -1,7 +1,15 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RankTeamsByVotes {
 
+    private static String rankTeamByVote(String [] votes){
+        Map<Character, int[]> map = new HashMap<>();
+
+        return "";
+    }
 
     public static void main(String[] args) {
         String [] votes = {"ABC","ACB","ABC","ACB","ACB"};
