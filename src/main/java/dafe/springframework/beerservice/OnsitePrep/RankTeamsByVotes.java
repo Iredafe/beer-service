ce2c07c8746518731e8ffd0ea802978fa5546133
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-19 22:42:38 -0700

    rank teams by vote WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/RankTeamsByVotes.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/RankTeamsByVotes.java
@@ -1,4 +1,10 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class RankTeamsByVotes {
+
+
+    public static void main(String[] args) {
+        String [] votes = {"ABC","ACB","ABC","ACB","ACB"};
+        System.out.println("This is the teams vote rank : " + rankTeamByVote(votes));
+    }
 }
