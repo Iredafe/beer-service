d8ac7ad1a49b1f9f44bedbd6c9273626b11c5bce
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-19 22:40:39 -0700

    rank teams by vote started

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/TrappingRainWater.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/TrappingRainWater.java
@@ -4,10 +4,10 @@ public class TrappingRainWater {
 
     public static int trappedWater(int [] height){
         int n = height.length;
-        int left = 1;
+        int left = 0;
         int right = n-1;
         int leftMax = height[0];
-        int rightMax = height[n-2];
+        int rightMax = height[n-1];
         int trappedWaterVolume = 0;
 
         while (left <= right){
