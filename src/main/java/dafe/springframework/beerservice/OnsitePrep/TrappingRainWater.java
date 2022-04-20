0b126072fde19b28128944b6b6c535e502e5f3b0
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-19 21:01:47 -0700

    trapping rain water WIP II

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/TrappingRainWater.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/TrappingRainWater.java
@@ -3,7 +3,12 @@ package dafe.springframework.beerservice.OnsitePrep;
 public class TrappingRainWater {
 
     public static int trappedWater(int [] height){
-
+        int n = height.length;
+        int left = 1;
+        int right = n-1;
+        int leftMax = height[0];
+        int rightMax = height[n-2];
+        int trappedWaterVolume = 0;
 
     }
 
