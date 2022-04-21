22a822ed5ac85fd2275898c80d2b72e87a7971fa
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-20 21:55:12 -0700

    Kth Largest element in array WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/KthLargestElementInArray.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/KthLargestElementInArray.java
@@ -1,4 +1,12 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class KthLargestElementInArray {
+
+
+
+    public static void main(String[] args) {
+        int [] nums = {3,2,1,5,6,4}; int k = 2;
+
+        System.out.println("This is the kth largest number : " + findKthLargestNumber(nums));
+    }
 }
