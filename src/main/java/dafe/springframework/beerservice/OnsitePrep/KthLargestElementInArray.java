368fa69a4d75eb28daf71c457084ab5139451232
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-20 22:46:17 -0700

    Kth Largest element in array using quickselect started

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/KthLargestElementInArray.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/KthLargestElementInArray.java
@@ -1,12 +1,18 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class KthLargestElementInArray {
 
+    private static int kthLargestNumber(int [] array, int k){
+
+        return 0;
+    }
 
     private static int findKthLargestNumber(int [] array, int k){
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        PriorityQueue<Integer> heap =
+                new PriorityQueue<>(Comparator.comparingInt(n -> n));
 
         for(int number : array){
             heap.add(number);
@@ -20,6 +26,6 @@ public class KthLargestElementInArray {
     public static void main(String[] args) {
         int [] nums = {3,2,1,5,6,4}; int k = 2;
 
-        System.out.println("This is the kth largest number : " + findKthLargestNumber(nums));
+        System.out.println("This is the kth largest number : " + findKthLargestNumber(nums, k));
     }
 }
