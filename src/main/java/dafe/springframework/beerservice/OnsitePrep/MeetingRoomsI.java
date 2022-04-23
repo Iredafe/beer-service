03481bc8b47071588e867e72fd1c0c486973e775
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-22 20:13:49 -0700

    meeting rooms WIP II

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/MeetingRoomsI.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/MeetingRoomsI.java
@@ -1,9 +1,14 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
+import java.util.Arrays;
+
 public class MeetingRoomsI {
 
     public static boolean intervals(int[][] meetingRooms){
-
+        Arrays.sort(meetingRooms, (a, b)->Integer.compare(a[0], b[0]));
+        for(int i=0; i<meetingRooms.length; i++){
+            if(meetingRooms[i][0] <= meetingRooms[i+1][1]) return true;
+        }
         return false;
     }
 
