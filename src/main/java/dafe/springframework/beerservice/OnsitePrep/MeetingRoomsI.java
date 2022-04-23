fe0733d9cec3dd50d979daee3bbd74036aaf5bf2
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-22 20:20:57 -0700

    meeting rooms done

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/MeetingRoomsI.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/MeetingRoomsI.java
@@ -1,19 +1,20 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class MeetingRoomsI {
 
     public static boolean intervals(int[][] meetingRooms){
-        Arrays.sort(meetingRooms, (a, b)->Integer.compare(a[0], b[0]));
+        Arrays.sort(meetingRooms, (a,b)->Integer.compare(a[0],b[0]));
         for(int i=0; i<meetingRooms.length; i++){
-            if(meetingRooms[i][0] <= meetingRooms[i+1][1]) return true;
+            if(meetingRooms[i][1] > meetingRooms[i+1][0]) return false;
         }
-        return false;
+        return true;
     }
 
     public static void main(String[] args) {
-        int [][] meetingRooms = {{0,30},{5,10},{15,20}};
+        int [][] meetingRooms = {{5,10},{0,30},{15,20}};
         System.out.println("I can attend all meetings ? " + intervals(meetingRooms));
     }
 }
