0870121639b7a8db75b1d71e8a20781552ff258d
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-22 20:01:52 -0700

    meeting rooms WIP I

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/MeetingRoomsI.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/MeetingRoomsI.java
@@ -2,6 +2,11 @@ package dafe.springframework.beerservice.OnsitePrep;
 
 public class MeetingRoomsI {
 
+    public static boolean intervals(int[][] meetingRooms){
+
+        return false;
+    }
+
     public static void main(String[] args) {
         int [][] meetingRooms = {{0,30},{5,10},{15,20}};
         System.out.println("I can attend all meetings ? " + intervals(meetingRooms));
