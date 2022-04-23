c92f33d03adb636939c985c5ed01d5d402616555
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-22 22:01:51 -0700

    meeting room 2 WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/MeetingRoomII.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/MeetingRoomII.java
@@ -1,4 +1,15 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class MeetingRoomII {
+
+    public int minMeetingRooms(Interval[] intervals) {
+        int[] starts = new int[intervals.length];
+        int[] ends = new int[intervals.length];
+        for(int i=0; i<intervals.length; i++) {
+            starts[i] = intervals[i].start;
+            ends[i] = intervals[i].end;
+        }
 }
+
+    }
+    }
