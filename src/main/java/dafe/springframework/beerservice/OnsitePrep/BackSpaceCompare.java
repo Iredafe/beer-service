61b481abacadc2e0724ca53ea245ae98d3c83c19
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-23 20:48:55 -0700

    backspace compare WIP I

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/BackSpaceCompare.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/BackSpaceCompare.java
@@ -2,6 +2,16 @@ package dafe.springframework.beerservice.OnsitePrep;
 
 public class BackSpaceCompare {
 
+    private static boolean backSpaceCompare(String s, String t){
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        for(int i=0; i<s.length(); i++){
+            if()
+        }
+
+    }
+
     public static void main(String[] args) {
         String first = "ab#c";
         String second = "ad#c";
