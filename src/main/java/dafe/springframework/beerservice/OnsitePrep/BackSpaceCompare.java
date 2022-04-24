f66e9eccb8cdcc0ec35547385553521be2fc7026
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-23 20:42:05 -0700

    backspace compare WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/BackSpaceCompare.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/BackSpaceCompare.java
@@ -1,4 +1,11 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class BackSpaceCompare {
+
+    public static void main(String[] args) {
+        String first = "ab#c";
+        String second = "ad#c";
+
+        System.out.println("Both strings are same ? " + backSpaceCompare(first, second));
+    }
 }
