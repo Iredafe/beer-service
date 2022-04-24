2647eb3a27dc061644764d274ea935e2669da2da
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-23 21:17:22 -0700

    backspace compare WIP IV

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/BackSpaceCompare.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/BackSpaceCompare.java
@@ -5,32 +5,19 @@ import java.util.Stack;
 public class BackSpaceCompare {
 
     private static boolean backSpaceCompare(String s, String t){
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
 
-        for(int i=0; i<s.length(); i++){
-            char character = s.charAt(i);
-            if(character != '#'){
-                sb1.append(character);
-            }
-        }
-
-        for(int i=0; i<t.length(); i++){
-            char character = t.charAt(i);
-            if(character != '#') {
-                sb2.append(character);
-            }
-        }
-        return sb1.equals(sb2);
+        return buildString(s).equals(buildString(t));
     }
 
 
-    private static boolean buildString(String string){
+    private static String buildString(String string){
         Stack<Character> stack = new Stack<>();
 
         for(char character : string.toCharArray()){
-
+            if(character!='#') stack.push(character);
+            else stack.pop();
         }
+        return stack.toString();
     }
 
     public static void main(String[] args) {
