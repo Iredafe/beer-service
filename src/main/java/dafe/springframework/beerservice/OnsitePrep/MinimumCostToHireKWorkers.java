8a1f71e19758d8cdcd0515dcc696f2d8a7337085
Dafe owolabi <iredafeowolabi@gmail.com>
2022-04-24 21:35:47 -0700

    minimum cost to hire k workers WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/MinimumCostToHireKWorkers.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/MinimumCostToHireKWorkers.java
@@ -1,4 +1,9 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class MinimumCostToHireKWorkers {
+
+    public static void main(String[] args) {
+        int [] quality = {10,20,5};
+        int [] wage ={70,50,30};
+    }
 }
