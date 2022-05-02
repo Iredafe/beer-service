f22aa78e4d6c26abaf534ed7ca145191eb9b6b2d
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-01 20:04:06 -0700

    kth closest point to origin WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/KClosestPointToOrigin.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/KClosestPointToOrigin.java
@@ -1,4 +1,10 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class KClosestPointToOrigin {
+
+    public static void main(String[] args) {
+        int [] [] points = {{3,3},{5,-1},{-2,4}}; int k = 1;
+
+        System.out.println("This is the kth closest point to origin (0, 0) : " + kthClosestPoint(points, k));
+    }
 }
