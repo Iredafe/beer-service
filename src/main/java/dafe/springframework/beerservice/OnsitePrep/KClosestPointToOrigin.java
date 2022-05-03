de7d11adb07617063b679ac2f30cd04842d07278
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-02 21:58:07 -0700

    k closest point with quick select WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/KClosestPointToOrigin.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/KClosestPointToOrigin.java
@@ -38,7 +38,9 @@ public class KClosestPointToOrigin {
 /// using quickselect
 
     private static void swap(int i, int j, int [] array){
-
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
     public static void main(String[] args) {
         int [] [] points = {{3,3},{5,-1},{-2,4}}; int k = 1;
