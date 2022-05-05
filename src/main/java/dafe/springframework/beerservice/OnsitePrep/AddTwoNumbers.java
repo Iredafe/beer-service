f35af71f7a35e2e138e4ab9f563dbbbe21c0adfa
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-04 21:36:52 -0700

    add two numbers started

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/AddTwoNumbers.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/AddTwoNumbers.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class AddTwoNumbers {
+
+    static class ListNode{
+        int val;
+        ListNode next;
+
+        public ListNode(){
+
+        }
+    }
 }
