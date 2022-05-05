bde5dbdae2813a0640877552859136b76e51dc12
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-04 22:20:07 -0700

    remove nth node from end of list WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/RemoveNthNodeFromEndOfList.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/RemoveNthNodeFromEndOfList.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
+import java.util.List;
+
 public class RemoveNthNodeFromEndOfList {
+
+    static class ListNode{
+        int val;
+        ListNode next;
+
+        public ListNode (int val){
+            this.val = val;
+        }
+
+    }
 }
