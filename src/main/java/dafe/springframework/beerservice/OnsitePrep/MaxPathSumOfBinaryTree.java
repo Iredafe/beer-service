b12ea77462a7e798830b0cf344c7fd9b612e58e6
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-07 17:35:28 -0700

    max path sum WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/MaxPathSumOfBinaryTree.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/MaxPathSumOfBinaryTree.java
@@ -1,4 +1,14 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class MaxPathSumOfBinaryTree {
+
+    static class TreeNode{
+        int value;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int value){
+            this.value = value;
+        }
+    }
 }
