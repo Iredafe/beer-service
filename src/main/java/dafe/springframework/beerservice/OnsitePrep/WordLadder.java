b701791e4832219149a5c07a378899c336ef65d9
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-08 21:15:01 -0700

    word ladder WIP III

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/WordLadder.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/WordLadder.java
@@ -1,12 +1,16 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WordLadder {
 
     public static int ladderLength(String startWord, String endWord, List<String> listOfWordsToClimb){
 
+        Set<String> set = new HashSet<>(listOfWordsToClimb);
+        if(!set.contains(endWord)) return 0;
         return 0;
     }
     public static void main(String[] args) {
