696d433fce87b84f3fa376a8b7a193296c11d100
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-08 21:14:15 -0700

    word ladder WIP II

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/WordLadder.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/WordLadder.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 public class WordLadder {
 
-    public int ladderLength(String startWord, String endWord, List<String> listOfWordsToClimb){
+    public static int ladderLength(String startWord, String endWord, List<String> listOfWordsToClimb){
 
         return 0;
     }
     public static void main(String[] args) {
         String beginWord = "hit"; String endWord = "cog";
         List<String> wordList = Arrays.asList(new String[]{"hot", "dot", "dog", "lot", "log","cog"});
+
+        System.out.println("This is the shortest path to " + endWord+ " : " + ladderLength(beginWord, endWord, wordList));
     }
 }
