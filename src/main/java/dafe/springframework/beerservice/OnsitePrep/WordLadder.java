a89371d4b2422632ee857cef481798f706e92d5f
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-08 21:12:48 -0700

    word ladder WIP I

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/WordLadder.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/WordLadder.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class WordLadder {
 
+    public int ladderLength(String startWord, String endWord, List<String> listOfWordsToClimb){
+
+        return 0;
+    }
     public static void main(String[] args) {
         String beginWord = "hit"; String endWord = "cog";
         List<String> wordList = Arrays.asList(new String[]{"hot", "dot", "dog", "lot", "log","cog"});
