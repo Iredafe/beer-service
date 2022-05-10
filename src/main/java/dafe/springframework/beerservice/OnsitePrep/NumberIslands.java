4c27a11bc4bf71ffd35882f7f3f05f18f46e117a
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-09 21:09:08 -0700

    number islands WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/NumberIslands.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/NumberIslands.java
@@ -2,5 +2,14 @@ package dafe.springframework.beerservice.OnsitePrep;
 
 public class NumberIslands {
 
+    public static void main(String[] args) {
+        char [][] grid = {
+                {'1','1','1','1','0'},
+                {'1','1','0','1','0'},
+                {'1','1','0','0','0'},
+                {'0','0','0','0','0'}
+        };
 
+        System.out.println("This is the number of islands in the grid : " + numberOfIslandsBFS(grid));
+    }
 }
