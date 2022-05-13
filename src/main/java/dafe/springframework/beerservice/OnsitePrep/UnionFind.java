956592a64d8488eb62ae91eb541745b01f4059a8
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-12 22:19:07 -0700

    union find WIP I

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/UnionFind.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/UnionFind.java
@@ -5,6 +5,13 @@ public class UnionFind {
     private int[] root;
 
     public UnionFind(int size){
+        root = new int[size];
+        for(int i=0; i<size; i++){
+            root[i] = i;
+        }
+    }
 
+    public int find(int x){
+        return root[x];
     }
 }
