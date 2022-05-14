103c83e12295cfe38cb4c4c0a129f858791a54a4
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-13 20:46:06 -0700

    course schedule I WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/CourseSchedule1.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/CourseSchedule1.java
@@ -1,4 +1,10 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class CourseSchedule1 {
+
+    public static void main(String[] args) {
+        int numCourses = 4; int [][] prerequisites = {{1,0},{2,0},{3,1},{3,2}};
+
+        System.out.println("Can these courses be finished ? " + canFinish(numCourses, prerequisites) );
+    }
 }
