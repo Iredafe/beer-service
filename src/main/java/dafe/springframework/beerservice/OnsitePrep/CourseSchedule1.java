3aa63140a4e10233286ccf91dbc3d9fb3e5bc3e0
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-13 20:47:23 -0700

    course schedule I WIP II

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/CourseSchedule1.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/CourseSchedule1.java
@@ -1,11 +1,15 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class CourseSchedule1 {
 
 
     private static boolean canFinish(int numCourses, int [][] prerequisites){
 
-    }
+        HashMap<Integer, List<Integer>> dictionary = new HashMap<>();
+     }
 
     public static void main(String[] args) {
         int numCourses = 4; int [][] prerequisites = {{1,0},{2,0},{3,1},{3,2}};
