7f78f8443623c44c60b4cc6d142a6958961ae3e2
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-15 21:49:37 -0700

    count complete tree node WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/CountCompleteTreeNode.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/CountCompleteTreeNode.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class CountCompleteTreeNode {
+    static BinaryTree root;
+    static class BinaryTree{
+        int value; BinaryTree left; BinaryTree right;
+
+        public BinaryTree(int value){
+            this.value = value;
+        }
+    }
+
 }
