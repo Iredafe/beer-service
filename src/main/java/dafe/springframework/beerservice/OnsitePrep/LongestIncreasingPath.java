776dd469c2af567c9c21e3b3e588749fbdaea440
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-16 21:36:11 -0700

    longest increasing path WIP

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/LongestIncreasingPath.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/LongestIncreasingPath.java
@@ -1,4 +1,10 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
 public class LongestIncreasingPath {
+
+
+    public static void main(String[] args) {
+        int [][] matrix = {{9,9,4},{6,6,8},{2,1,1}};
+        System.out.println("The longest increasing path is : " + longestIncreasingPath(matrix));
+    }
 }
