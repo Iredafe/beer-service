b1de9ce82e9dff21328a2fbaa0f8ef2351bb95b8
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-16 21:50:31 -0700

    longest increasing path WIP V

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/LongestIncreasingPath.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/LongestIncreasingPath.java
@@ -6,6 +6,7 @@ public class LongestIncreasingPath {
     private static int longestIncreasingPath(int[][] matrix){
         int rowLength = matrix.length;
         int colLength = matrix[0].length;
+        if(rowLength==0) return 0;
         int [][] cache = new int [rowLength][colLength];
         int answer = 0;
 
@@ -18,8 +19,19 @@ public class LongestIncreasingPath {
     }
 
     private static int dfs(int [][]matrix, int i, int j, int[][] cache){
+        int rowLength = matrix.length;
+        int colLength= matrix[0].length;
 
-    }
+
+        for(int [] direction : directions){
+            int row = i + direction[0];
+            int col = j + direction[1];
+            if(row>=0 && col >=0 && row < rowLength && col < colLength && matrix[row][col] > matrix[i][j]){
+                cache[row][col] = Math.max(cache[i][j], dfs(matrix,row,col,cache));
+            }
+        }
+        return cache[i][j];
+        }
 
     public static void main(String[] args) {
         int [][] matrix = {{9,9,4},{6,6,8},{2,1,1}};
