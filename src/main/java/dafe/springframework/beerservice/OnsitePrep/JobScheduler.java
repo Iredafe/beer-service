c196ef64cb2463c131508981fe5f94aef7a9cc8d
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-18 21:27:43 -0700

    job scheduler WIP II

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/JobScheduler.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/JobScheduler.java
@@ -8,5 +8,10 @@ public class JobScheduler {
 
     public static void main(String[] args) {
         List<Integer> jobs = Arrays.asList(1,2,3,4);
+        List<Integer[]> dependencies = Arrays.asList(new Integer[]{1, 2},
+                new Integer[]{1, 3},
+                new Integer[]{3, 2},
+                new Integer[]{4, 2},
+                new Integer[]{4, 3});
     }
 }
