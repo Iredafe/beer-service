4218f75fc9d850bdb6e52d36276c9941b4c0127b
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-18 22:54:12 -0700

    decode string WIP II

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/DecodeString.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/DecodeString.java
@@ -1,7 +1,15 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
+import java.util.Stack;
+
 public class DecodeString {
 
+    private static String decodeString(String input){
+        Stack<Integer> countStack = new Stack<>();
+        Stack<Integer> stringStack = new Stack<>();
+        int k=0;
+        StringBuilder decodedString = new StringBuilder();
+    }
     public static void main(String[] args) {
         String s = "2[abc]3[cd]ef";
         System.out.println("This is the decoded string : " + decodeString(s));
