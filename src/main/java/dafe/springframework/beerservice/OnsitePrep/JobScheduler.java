2a49644b8e498c172eacb84f3ac57a941e804ab4
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-20 19:28:56 -0700

    job scheduler WIP XII

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/JobScheduler.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/JobScheduler.java
@@ -1,55 +1,59 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
 
 public class JobScheduler {
 
     private static List<Integer> findOrder(List<Integer> jobs, List<Integer[]> dependencies){
-        int size = jobs.size();
-        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+
+        List<List<Integer>>graph = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
         List<Integer> result = new ArrayList<>();
 
+        int size = jobs.size();
+        for(int i = 0; i< size; i++){
+            graph.add(new ArrayList<>());
+        }
+
         for(Integer [] dependency : dependencies){
-            if(graph.containsKey(dependency[0])){
-                graph.get(dependency[0]).add(dependency[1]);
-            }else{
-                List<Integer> nextJobs = new ArrayList<>();
-                nextJobs.add(dependency[1]);
-                graph.put(dependency[0], nextJobs);
-            }
+            graph.get(dependency[0]).add(dependency[1]);
         }
 
         boolean [] visited = new boolean[size];
         boolean [] checked = new boolean[size];
 
         for(int currentJob = 0; currentJob< size; currentJob++){
-            if(isCycle(graph, currentJob, checked, visited)){
+            if(isCycle(graph, currentJob, checked, visited, stack)){
                 return new ArrayList<>();
             }
         }
 
-        for(Integer order : graph.keySet()){
-            result.add(order);
+        for(int i=0; i<size; i++){
+            result.add(stack.pop());
         }
+
         return result;
     }
 
-    private static boolean isCycle(HashMap<Integer, List<Integer>> graph, int currentJob,
-                                   boolean [] checked, boolean [] visited){
+    private static boolean isCycle(List<List<Integer>> graph, int currentJob, boolean [] checked,
+                                   boolean [] visited, Stack<Integer> stack){
 
         if(checked[currentJob]) return false;
         if(visited[currentJob]) return true;
-        if(!graph.containsKey(currentJob)) return false;
 
         visited[currentJob] = true;
 
         for(Integer neighbor : graph.get(currentJob)){
-            if(isCycle(graph, neighbor, checked, visited)){
+            if(isCycle(graph, neighbor, checked, visited, stack)){
                 return true;
             }
         }
-        checked[currentJob] = true;
+        stack.push(currentJob);
         visited[currentJob] = false;
+        checked[currentJob] = true;
 
         return false;
     }
@@ -61,7 +65,5 @@ public class JobScheduler {
                 new Integer[]{3, 2},
                 new Integer[]{4, 2},
                 new Integer[]{4, 3});
-
-        System.out.println("This is the order in which the jobs must be done : " + findOrder(jobs, dependencies));
     }
 }
