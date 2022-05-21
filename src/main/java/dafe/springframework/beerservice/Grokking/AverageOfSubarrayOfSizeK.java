667957378bc0a6c9b1b3533546bad39dfb0c16d3
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-20 22:26:23 -0700

    average of subarray of size k WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/AverageOfSubarrayOfSizeK.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/AverageOfSubarrayOfSizeK.java
@@ -1,4 +1,11 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class AverageOfSubarrayOfSizeK {
+
+    public static void main(String[] args) {
+        double[] result = findAverages(5,
+                new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 });
+        System.out.println("Averages of subarrays of size K: " + Arrays.toString(result));
+    }
+
 }
