fa6505478c600f51d50191f36ac263316705ef9b
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-20 23:02:50 -0700

    maximum subarray of size k WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/MaximumSubarrayOfSizeK.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/MaximumSubarrayOfSizeK.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class MaximumSubarrayOfSizeK {
+
+    private static int findMaxSumSubArray(int k, int [] array){
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Maximum sum of a subarray of size K: "
+                + findMaxSumSubArray(3, new int[] { 2, 1, 5, 1, 3, 2 }));
+        System.out.println("Maximum sum of a subarray of size K: "
+                + findMaxSumSubArray(2, new int[] { 2, 3, 4, 1, 5 }));
+    }
 }
