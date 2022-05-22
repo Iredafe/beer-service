875dfafd69dfb4ea431b1b60e5997d058d3b1352
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-21 20:40:01 -0700

    diameter of a binary tree WIP I

--- src/main/java/dafe/springframework/beerservice/OnsitePrep/DiameterOfABinaryTree.java
+++ src/main/java/dafe/springframework/beerservice/OnsitePrep/DiameterOfABinaryTree.java
@@ -1,7 +1,20 @@
 package dafe.springframework.beerservice.OnsitePrep;
 
+import com.sun.source.tree.Tree;
+
 public class DiameterOfABinaryTree {
 
+    static class TreeNode{
+        int value;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int value){
+            this.value = value;
+        }
+
+
+    }
 
     public static void main(String[] args) {
 
