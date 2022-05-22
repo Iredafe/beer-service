fcd5bb8a951b986e510e348e6d0d1259efd4bf0b
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-21 22:27:07 -0700

    smallest subarray with greater sum WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/SmallestSubarrayWithGreaterSum.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/SmallestSubarrayWithGreaterSum.java
@@ -2,4 +2,12 @@ package dafe.springframework.beerservice.Grokking;
 
 public class SmallestSubarrayWithGreaterSum {
 
+    public static void main(String[] args) {
+        int result = findMinSubArray(7, new int[] { 2, 1, 5, 2, 3, 2 });
+        System.out.println("Smallest subarray length: " + result);
+        result = findMinSubArray(7, new int[] { 2, 1, 5, 2, 8 });
+        System.out.println("Smallest subarray length: " + result);
+        result = findMinSubArray(8, new int[] { 3, 4, 1, 1, 6 });
+        System.out.println("Smallest subarray length: " + result);
+    }
 }
