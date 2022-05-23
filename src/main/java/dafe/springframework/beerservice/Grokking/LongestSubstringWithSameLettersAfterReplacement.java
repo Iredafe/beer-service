1f5b6d1994291525451ed6c612a995e4f275ece7
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-23 15:04:03 -0700

    longest substring with same letters after replacement WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/LongestSubstringWithSameLettersAfterReplacement.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/LongestSubstringWithSameLettersAfterReplacement.java
@@ -1,4 +1,11 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class LongestSubstringWithSameLettersAfterReplacement {
+
+
+    public static void main(String[] args) {
+        System.out.println(findLength("aabccbb", 2));
+        System.out.println(findLength("abbcb", 1));
+        System.out.println(findLength("abccde", 1));
+    }
 }
