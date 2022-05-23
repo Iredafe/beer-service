dd770c8e028fcbda68cddd720a802e0c86af4348
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-22 21:42:35 -0700

    longest substring with distinct character WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/LongestSubstringWithDistinctCharacters.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/LongestSubstringWithDistinctCharacters.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class LongestSubstringWithDistinctCharacters {
+
+    public static void main(String[] args) {
+        System.out.println("Length of the longest substring: "
+                + findLength("aabccbb"));
+        System.out.println("Length of the longest substring: "
+                + findLength("abbbb"));
+        System.out.println("Length of the longest substring: "
+                + findLength("abccde"));
+    }
 }
