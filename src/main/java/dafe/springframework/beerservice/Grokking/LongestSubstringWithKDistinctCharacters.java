a9527f21648ff5813c52c2899dfd9b5183c50c8d
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-22 20:25:12 -0700

    longest substring with k distinct character WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/LongestSubstringWithKDistinctCharacters.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/LongestSubstringWithKDistinctCharacters.java
@@ -1,4 +1,14 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class LongestSubstringWithKDistinctCharacters {
+
+
+    public static void main(String[] args) {
+        System.out.println("Length of the longest substring: "
+                + findLength("araaci", 2));
+        System.out.println("Length of the longest substring: "
+                + findLength("araaci", 1));
+        System.out.println("Length of the longest substring: "
+                + findLength("cbbebi", 3));
+    }
 }
