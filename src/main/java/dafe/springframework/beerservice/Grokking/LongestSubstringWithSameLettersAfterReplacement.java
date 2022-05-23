56dcdb04cdb5393de349964e9bf56badb4bcb497
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-23 15:07:44 -0700

    longest substring with same letters after replacement WIP III

--- src/main/java/dafe/springframework/beerservice/Grokking/LongestSubstringWithSameLettersAfterReplacement.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/LongestSubstringWithSameLettersAfterReplacement.java
@@ -5,9 +5,17 @@ import java.util.Map;
 
 public class LongestSubstringWithSameLettersAfterReplacement {
 
-    private static int findLength(String string){
+    private static int findLength(String string, int k){
         int maxLength=0; int maxRepeatingLetterCount=0; int left=0;
         Map<Character, Integer> letterFrequencyMap = new HashMap<>();
+
+        for(int right=0; right<string.length(); right++){
+            char character = string.charAt(right);
+
+            letterFrequencyMap.put(character, letterFrequencyMap.getOrDefault(character,0)+1);
+            maxRepeatingLetterCount=Math.max(maxRepeatingLetterCount, letterFrequencyMap.get(character));
+            if()
+        }
     }
 
     public static void main(String[] args) {
