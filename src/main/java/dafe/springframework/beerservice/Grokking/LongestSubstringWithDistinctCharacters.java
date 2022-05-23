671967c4f7100a2c8b8775a39c50d6a3c26ddae2
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-22 22:17:14 -0700

    longest substring with distinct character WIP III

--- src/main/java/dafe/springframework/beerservice/Grokking/LongestSubstringWithDistinctCharacters.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/LongestSubstringWithDistinctCharacters.java
@@ -1,5 +1,6 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,21 +8,29 @@ public class LongestSubstringWithDistinctCharacters {
 
     private static int findLength(String input){
 
-        Set<Character> window = new HashSet<>();
+        HashMap<Character, Integer> window = new HashMap<>();
         int left=0;
         int length=0;
 
         for(int right=0; right<input.length(); right++){
             char character = input.charAt(right);
+
+            if(window.containsKey(character)){
+                left = Math.max(left, window.get(character)+1);
+            }
+            window.put(character, right);
+
+            length = Math.max(length, right-left+1);
         }
 
+        return length;
     }
 
     public static void main(String[] args) {
-        System.out.println("Length of the longest substring: "
-                + findLength("aabccbb"));
-        System.out.println("Length of the longest substring: "
-                + findLength("abbbb"));
+//        System.out.println("Length of the longest substring: "
+//                + findLength("aabccbb"));
+//        System.out.println("Length of the longest substring: "
+//                + findLength("abbbb"));
         System.out.println("Length of the longest substring: "
                 + findLength("abccde"));
     }
