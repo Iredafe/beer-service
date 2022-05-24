546b74d6d129c44044b9f0a143b598d2c548fab0
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-24 13:18:57 -0700

    permutation in a string WIP IV

--- src/main/java/dafe/springframework/beerservice/Grokking/PermutationInAString.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/PermutationInAString.java
@@ -7,10 +7,21 @@ public class PermutationInAString {
     private static boolean findPermutation(String word, String pattern){
 
         HashMap<Character, Integer> characterFrequencymap = new HashMap<>();
-        int left=0;
+        int left=0; int matched=0;
 
         for(char character : pattern.toCharArray()){
             characterFrequencymap.put(character, characterFrequencymap.getOrDefault(character, 0) +1);
+        }
+
+        for(int right=0; right< word.length(); right++){
+            char character = word.charAt(right);
+            if(characterFrequencymap.containsKey(character)){
+                characterFrequencymap.put(character, characterFrequencymap.get(character)-1);
+                if(characterFrequencymap.get(character)==0){
+
+                    matched--;
+                }
+            }
         }
         return false;
     }
