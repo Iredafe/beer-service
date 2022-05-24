7749389aa48c6310f71a6279264fdf7b9cca3b69
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-24 13:13:13 -0700

    permutation in a string WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/PermutationInAString.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/PermutationInAString.java
@@ -1,4 +1,15 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class PermutationInAString {
+
+    public static void main(String[] args) {
+        System.out.println("Permutation exist: "
+                + findPermutation("oidbcaf", "abc"));
+        System.out.println("Permutation exist: "
+                + findPermutation("odicf", "dc"));
+        System.out.println("Permutation exist: "
+                + findPermutation("bcdxabcdy", "bcdyabcdx"));
+        System.out.println("Permutation exist: "
+                + findPermutation("aaacb", "abc"));
+    }
 }
