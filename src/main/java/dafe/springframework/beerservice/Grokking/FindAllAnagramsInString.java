5b2620208c118966a2642c6e01ee9c07fc2a62f5
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-24 20:33:10 -0700

    find all anagrams in string WIP II

--- src/main/java/dafe/springframework/beerservice/Grokking/FindAllAnagramsInString.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/FindAllAnagramsInString.java
@@ -1,12 +1,19 @@
 package dafe.springframework.beerservice.Grokking;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class FindAllAnagramsInString {
 
     private static List<Integer> findStringAnagrams(String string, String pattern){
         List<Integer> result = new ArrayList<>();
+        HashMap<Character, Integer> characterFreqMap = new HashMap<>();
+        int matched=0; int windowStart=0;
+
+        for(char character : pattern.toCharArray()){
+            characterFreqMap.put(character, characterFreqMap.getOrDefault(character,0)+1);
+        }
 
         return result;
     }
