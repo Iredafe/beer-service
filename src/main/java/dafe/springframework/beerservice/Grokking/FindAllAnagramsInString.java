e38893e0489384877e39335548a7407be1a1b064
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-24 20:29:46 -0700

    find all anagrams in string WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/FindAllAnagramsInString.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/FindAllAnagramsInString.java
@@ -1,5 +1,8 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class FindAllAnagramsInString {
-
+    public static void main(String[] args) {
+        System.out.println(findStringAnagrams("ppqp", "pq"));
+        System.out.println(findStringAnagrams("abbcabc", "abc"));
+    }
 }
