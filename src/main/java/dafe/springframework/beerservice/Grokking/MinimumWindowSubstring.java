d4df218414454e723be16d1f76644ecfea6cd793
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-25 20:42:53 -0700

    minimum window substring WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/MinimumWindowSubstring.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/MinimumWindowSubstring.java
@@ -1,4 +1,12 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class MinimumWindowSubstring {
+
+
+    public static void main(String[] args) {
+        System.out.println(findSubstring("aabdec", "abc"));
+        System.out.println(findSubstring("aabdec", "abac"));
+        System.out.println(findSubstring("abdbca", "abc"));
+        System.out.println(findSubstring("adcad", "abc"));
+    }
 }
