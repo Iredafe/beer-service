96ac02a1e7459243c3e47d7e693317f93449e18b
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-25 20:46:02 -0700

    minimum window substring WIP I

--- src/main/java/dafe/springframework/beerservice/Grokking/MinimumWindowSubstring.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/MinimumWindowSubstring.java
@@ -1,7 +1,21 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.HashMap;
+
 public class MinimumWindowSubstring {
 
+    private static String findSubstring(String string, String pattern){
+        int matched=0; int windowStart=0;
+        String result = "";
+
+        HashMap<Character, Integer> characterFrequencyMap = new HashMap<>();
+
+        for(char letter : pattern.toCharArray()){
+            characterFrequencyMap.put(letter, characterFrequencyMap.getOrDefault(letter, 0) +1);
+        }
+
+        return result;
+    }
 
     public static void main(String[] args) {
         System.out.println(findSubstring("aabdec", "abc"));
