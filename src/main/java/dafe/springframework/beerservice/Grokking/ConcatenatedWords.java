81facf8f08e3e0a236c018030bacff5eaf7f96cc
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-26 22:02:51 -0700

    concatenated words WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/ConcatenatedWords.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/ConcatenatedWords.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class ConcatenatedWords {
+
+    public static void main(String[] args) {
+        List<Integer> result = findWordConcatenation(
+                "catfoxcat", new String[] { "cat", "fox" });
+        System.out.println(result);
+        result = findWordConcatenation(
+                "catcatfoxfox", new String[] { "cat", "fox" });
+        System.out.println(result);
+    }
 }
