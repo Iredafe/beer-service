247f45e7737a5a0527733f81dd0c0bc0d2d48fe4
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-26 20:51:12 -0700

    minimum window substring done..

--- src/main/java/dafe/springframework/beerservice/Grokking/MinimumWindowSubstring.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/MinimumWindowSubstring.java
@@ -29,7 +29,7 @@ public class MinimumWindowSubstring {
                     minLength=windowEnd-windowStart+1;
                     substringStart = windowStart;
 
-            }
+                }
 
 
                 character = string.charAt(windowStart++);
