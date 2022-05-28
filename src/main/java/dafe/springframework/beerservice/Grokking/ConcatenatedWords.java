68f81791bd2f851d3b351f481279eac8e1893533
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-27 21:06:46 -0700

    find concatenated words WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/ConcatenatedWords.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/ConcatenatedWords.java
@@ -1,15 +1,23 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConcatenatedWords {
 
-//    public static void main(String[] args) {
-//        List<Integer> result = findWordConcatenation(
-//                "catfoxcat", new String[] { "cat", "fox" });
-//        System.out.println(result);
-//        result = findWordConcatenation(
-//                "catcatfoxfox", new String[] { "cat", "fox" });
-//        System.out.println(result);
-//    }
+    private static List<Integer> findWordConcatenation(String string, String [] words){
+        List<Integer> resultIndices = new ArrayList<>();
+
+
+
+        return resultIndices;
+    }
+    public static void main(String[] args) {
+        List<Integer> result = findWordConcatenation(
+                "catfoxcat", new String[] { "cat", "fox" });
+        System.out.println(result);
+        result = findWordConcatenation(
+                "catcatfoxfox", new String[] { "cat", "fox" });
+        System.out.println(result);
+    }
 }
