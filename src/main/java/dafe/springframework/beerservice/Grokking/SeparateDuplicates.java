87efb73ea9e57bcb77638fd55991e1f4d6ed3a36
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-29 20:09:14 -0700

    separate duplicates done

--- src/main/java/dafe/springframework/beerservice/Grokking/SeparateDuplicates.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/SeparateDuplicates.java
@@ -2,6 +2,18 @@ package dafe.springframework.beerservice.Grokking;
 
 public class SeparateDuplicates {
 
+   static int remove(int [] array){
+        int nextNonDuplicates=1;
+
+        for(int i=0; i<array.length; i++){
+            if(array[nextNonDuplicates-1] != array[i]){
+                array[nextNonDuplicates] = array[i];
+                nextNonDuplicates++;
+            }
+        }
+        return nextNonDuplicates;
+    }
+
     public static void main(String[] args) {
         int[] arr = new int[] { 2, 3, 3, 3, 6, 9, 9 };
         System.out.println(remove(arr));
