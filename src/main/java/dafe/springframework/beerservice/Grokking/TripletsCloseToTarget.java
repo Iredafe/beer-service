18dc4738f6b440a96cee04e7ee02f5a42e71643f
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-30 16:09:06 -0700

    triplets close to target WIP I

--- src/main/java/dafe/springframework/beerservice/Grokking/TripletsCloseToTarget.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/TripletsCloseToTarget.java
@@ -1,9 +1,18 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.Arrays;
+
 public class TripletsCloseToTarget {
 
-    private static int searchTriplet(){
-
+    private static int searchTriplet(int [] array, int target){
+        Arrays.sort(array);
+        int sum=Integer.MAX_VALUE;
+        for (int i=0; i<array.length && array[i]<=0; i++){
+            if(i==0 || array[i-1] != array[i]){
+                getTripletsCloseToSum(array, i, sum);
+            }
+        }
+        return 0;
     }
     public static void main(String[] args) {
         System.out.println(searchTriplet(new int[] { -2, 0, 1, 2 }, 2));
