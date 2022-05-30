07a6f94167a58f07b313542334a5afc9c2fc584e
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-30 15:30:31 -0700

    triplets sum to zero WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/TripletSumToZero.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/TripletSumToZero.java
@@ -1,4 +1,10 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class TripletSumToZero {
+
+    public static void main(String[] args) {
+        System.out.println(TripletSumToZero.searchTriplets(
+                new int[] { -3, 0, 1, 2, -1, 1, -2 }));
+        System.out.println(TripletSumToZero.searchTriplets(new int[] { -5, 2, -1, -2, 3 }));
+    }
 }
