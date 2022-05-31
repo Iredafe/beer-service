20e573a692a7a5c5ac1d3e35dd8adbb502acaa75
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-30 21:23:25 -0700

    triplets close to target done

--- src/main/java/dafe/springframework/beerservice/Grokking/TripletsCloseToTarget.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/TripletsCloseToTarget.java
@@ -3,25 +3,23 @@ package dafe.springframework.beerservice.Grokking;
 import java.util.Arrays;
 
 public class TripletsCloseToTarget {
-    static int tripletsSumCloseToTarget;
+    static int difference;
     private static int searchTriplet(int [] array, int target){
-        tripletsSumCloseToTarget=Integer.MAX_VALUE;
+        difference =Integer.MAX_VALUE;
         Arrays.sort(array);
-        for (int i=0; i<array.length && array[i]<=0; i++){
+        for (int i = 0; i<array.length && difference !=0; i++){
             int left= i+1; int right = array.length-1;
             if(i==0 || array[i-1] != array[i]){
                 while(left < right){
                     int currentSum = array[i] + array[left] + array[right];
-                    int difference = Math.abs(currentSum-target);
-                    if(difference<tripletsSumCloseToTarget){
-                        tripletsSumCloseToTarget=difference;
-                        left++;
-                        right--;
-                    }
+                    if(Math.abs(target-currentSum)< difference){
+                        difference =target-currentSum;
+                    }else if(currentSum<target) left++;
+                    else right--;
                 }
             }
         }
-        return tripletsSumCloseToTarget;
+        return target-difference;
     }
 
 
