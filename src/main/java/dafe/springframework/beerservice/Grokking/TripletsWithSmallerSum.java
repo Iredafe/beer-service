d3172bc372ef816dda1d6d5b2d133dd34ccf11ab
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-30 21:25:59 -0700

    triplets with smaller sum WIP I

--- src/main/java/dafe/springframework/beerservice/Grokking/TripletsWithSmallerSum.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/TripletsWithSmallerSum.java
@@ -2,6 +2,10 @@ package dafe.springframework.beerservice.Grokking;
 
 public class TripletsWithSmallerSum {
 
+    private static int searchTriplets(int [] array, int target){
+
+        return 0;
+    }
 
     public static void main(String[] args) {
         System.out.println(searchTriplets(new int[] { -1, 0, 2, 3 }, 3));
