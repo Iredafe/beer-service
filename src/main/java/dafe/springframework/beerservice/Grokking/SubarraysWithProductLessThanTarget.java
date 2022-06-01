d3745f415671b24d77e3cbce3f2687626ec412a9
Dafe owolabi <iredafeowolabi@gmail.com>
2022-05-31 20:46:59 -0700

    subarrays with product less than target WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/SubarraysWithProductLessThanTarget.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/SubarraysWithProductLessThanTarget.java
@@ -1,4 +1,11 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class SubarraysWithProductLessThanTarget {
+
+    public static void main(String[] args) {
+        System.out.println(
+                findSubarrays(new int[] { 2, 5, 3, 10 }, 30));
+        System.out.println(
+                findSubarrays(new int[] { 8, 2, 6, 5 }, 50));
+    }
 }
