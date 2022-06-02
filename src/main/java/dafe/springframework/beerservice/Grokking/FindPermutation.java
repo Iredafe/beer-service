fd8670d08e207e871c3cb664753dfa81127a9783
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-01 22:32:21 -0700

    find permutations started

--- src/main/java/dafe/springframework/beerservice/Grokking/FindPermutation.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/FindPermutation.java
@@ -1,4 +1,11 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.List;
+
 public class FindPermutation {
+
+    public static void main(String[] args) {
+        List<List<Integer>> result = findPermutations(new int[] { 1, 3, 5 });
+        System.out.print("Here are all the permutations: " + result);
+    }
 }
