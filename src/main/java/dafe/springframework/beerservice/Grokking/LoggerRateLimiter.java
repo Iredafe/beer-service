733431d391d15e20dbedc903d8856a9cb0d55637
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-03 20:41:36 -0700

    rate limiter done

--- src/main/java/dafe/springframework/beerservice/Grokking/LoggerRateLimiter.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/LoggerRateLimiter.java
@@ -5,41 +5,39 @@ import org.springframework.data.util.Pair;
 import java.util.*;
 
 public class LoggerRateLimiter {
-
+    static Deque<Pair<Integer, String>> messageQueue = new LinkedList<>();
+    static Set<String> messageIndex = new HashSet<>();
 
     private static boolean shouldPrintMessage(Integer timestamp, String message){
-        Queue<Pair<Integer, String>> messageQueue = new LinkedList<>();
-        Set<String> messageIndex = new HashSet<>();
-
         while(messageQueue.size()>0){
-            Pair<Integer, String> streamEntry = messageQueue.poll();
+            Pair<Integer, String> streamEntry = messageQueue.getFirst();
          //clean up
             if(timestamp - streamEntry.getFirst()>=10){
                 messageQueue.remove();
                 messageIndex.remove(streamEntry.getSecond());
             }else break;
-
-            if(!messageIndex.contains(message)){
-                Pair<Integer, String> newEntry = Pair.of(timestamp, message);
-                messageIndex.add(message);
-                messageQueue.add(newEntry);
-                return true;
-            }
         }
-        return false;
+        if(!messageIndex.contains(message)){
+            Pair<Integer, String> newEntry = Pair.of(timestamp, message);
+            messageIndex.add(message);
+            messageQueue.add(newEntry);
+            return true;
+        }else return false;
     }
 
     public static void main(String[] args) {
         HashMap<Integer, String> streamMap= new HashMap<>();
             streamMap.put(1,"foo");
             streamMap.put(2,"bar");
-            streamMap.put(3, "foo");
+            streamMap.put(3,"foo");
             streamMap.put(8,"bar");
             streamMap.put(10,"foo");
             streamMap.put(11,"foo");
 
             for(Map.Entry<Integer, String> entry : streamMap.entrySet()){
-                System.out.println("We can print : " + shouldPrintMessage(entry.getKey(), entry.getValue()));
+                System.out.println("We can print {timestamp-- " +
+                        entry.getKey()+ ": message-- "+ entry.getValue()+ "} ->" +
+                        shouldPrintMessage(entry.getKey(), entry.getValue()));
             }
 
     }
