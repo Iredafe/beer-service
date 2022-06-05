d9b87ba11de43a8f31a1fde7a7a64ff88f262698
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-04 21:44:04 -0700

    rank team by votes WIP 1

--- src/main/java/dafe/springframework/beerservice/Grokking/RankTeamByVotes.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/RankTeamByVotes.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class RankTeamByVotes {
+
+    private static String rankTeam(String [] votes){
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
