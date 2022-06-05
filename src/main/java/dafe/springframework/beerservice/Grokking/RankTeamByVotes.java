9a4141ddd9293212c3568c47bdc7435367288589
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-04 21:46:39 -0700

    rank team by votes WIP 3

--- src/main/java/dafe/springframework/beerservice/Grokking/RankTeamByVotes.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/RankTeamByVotes.java
@@ -10,5 +10,7 @@ public class RankTeamByVotes {
     public static void main(String[] args) {
 
         String [] votes = {"ABC","ACB","ABC","ACB","ACB"};
+
+        System.out.println("The result of the vote is : " + rankTeam(votes));
     }
 }
