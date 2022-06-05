e6e6f9d27b4d91974c8c9f7cd07d7ebc53cf7241
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-04 21:52:27 -0700

    rank team by votes WIP 4

--- src/main/java/dafe/springframework/beerservice/Grokking/RankTeamByVotes.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/RankTeamByVotes.java
@@ -1,9 +1,19 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RankTeamByVotes {
 
     private static String rankTeam(String [] votes){
 
+        int length = votes[0].length();
+        Map<String, Integer> votingEngine = new HashMap<>();
+        for(String vote : votes){
+            for(int index=0; index<length; index++){
+
+            }
+        }
         return "";
     }
 
