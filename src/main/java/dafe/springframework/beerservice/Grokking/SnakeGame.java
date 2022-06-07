85a9aac81098b5f8b29fe1df5036a62432929c19
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-06 22:37:54 -0700

    WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/SnakeGame.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/SnakeGame.java
@@ -1,4 +1,11 @@
 package dafe.springframework.beerservice.Grokking;
 
+import org.springframework.data.util.Pair;
+
+import java.util.Deque;
+import java.util.HashMap;
+
 public class SnakeGame {
+    HashMap<Pair<Integer, Integer>, Integer> snakeMap;
+    Deque<Pair<Integer,Integer>> snake;
 }
