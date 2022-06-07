8a0d736a5ab5a797aa3e384ba2fa3ff2257e2771
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-06 22:44:35 -0700

    WIP IV

--- src/main/java/dafe/springframework/beerservice/Grokking/SnakeGame.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/SnakeGame.java
@@ -4,11 +4,13 @@ import org.springframework.data.util.Pair;
 
 import java.util.Deque;
 import java.util.HashMap;
+import java.util.LinkedList;
 
 public class SnakeGame {
     int[][]food;
     int width;
     int height;
+    int foodIndex;
     HashMap<Pair<Integer, Integer>, Integer> snakeMap;
     Deque<Pair<Integer,Integer>> snake;
 
@@ -16,6 +18,8 @@ public class SnakeGame {
         this.width = width;
         this.height=height;
         this.food=food;
+        snakeMap = new HashMap<>();
+        snake = new LinkedList<>();
     }
 
     public int move(String direction){
