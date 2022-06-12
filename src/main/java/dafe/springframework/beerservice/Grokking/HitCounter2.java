b428036e9bc3cbdf3cb290730fc984a7a50d91fa
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-11 22:23:05 -0700

    WIP II

--- src/main/java/dafe/springframework/beerservice/Grokking/HitCounter2.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/HitCounter2.java
@@ -3,18 +3,24 @@ package dafe.springframework.beerservice.Grokking;
 import org.springframework.data.util.Pair;
 
 import java.util.Deque;
-import java.util.HashMap;
+import java.util.LinkedList;
 
 public class HitCounter2 {
 
-    static Deque<Pair<Integer, Integer>> timeFrequencyMap;
+    static Deque<Pair<Integer, Integer>> timeFrequencyQueue;
 
 
     public HitCounter2(){
-        timeFrequencyMap = new HashMap<>();
+        timeFrequencyQueue = new LinkedList<>();
     }
 
     public static void hit(int timestamp){
-
+        if (timeFrequencyQueue.isEmpty() || timeFrequencyQueue.getLast().getFirst() != timestamp){
+            timeFrequencyQueue.offerLast(Pair.of(timestamp, 1));
+        }else{
+            int previousCount = timeFrequencyQueue.getFirst().getSecond();
+            timeFrequencyQueue.removeLast();
+            timeFrequencyQueue.add(Pair.of(timestamp, previousCount+1));
+        }
     }
 }
