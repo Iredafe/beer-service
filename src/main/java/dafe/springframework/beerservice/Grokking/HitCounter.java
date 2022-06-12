aad680ee1c7d546446dd5a049ef8a1b2c292ab52
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-11 21:42:48 -0700

    WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/HitCounter.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/HitCounter.java
@@ -1,4 +1,14 @@
 package dafe.springframework.beerservice.Grokking;
 
+import dafe.springframework.beerservice.simuduck.Quack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class HitCounter {
+
+    Queue<Integer> hits;
+    public HitCounter(){
+        hits = new LinkedList<>();
+    }
 }
