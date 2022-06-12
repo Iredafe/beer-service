ab5146704012e8d83dbdc61445db2179a0e5423c
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-11 21:58:25 -0700

    WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/HitCounter2.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/HitCounter2.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.HashMap;
+
 public class HitCounter2 {
+
+    HashMap<Integer, Integer> timeFrequencyMap;
+
+
+    public HitCounter2(){
+        timeFrequencyMap = new HashMap<>();
+    }
 }
