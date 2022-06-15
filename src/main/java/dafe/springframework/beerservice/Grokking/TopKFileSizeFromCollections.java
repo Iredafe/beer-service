b6b65be64e3d9371c83550df8e8d67317151ace5
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-14 21:07:29 -0700

    WIP I

--- src/main/java/dafe/springframework/beerservice/Grokking/TopKFileSizeFromCollections.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/TopKFileSizeFromCollections.java
@@ -4,5 +4,12 @@ public class TopKFileSizeFromCollections {
 
     public static void main(String[] args) {
         TopKFileSizeFromCollections topKFileSizeFromCollections = new TopKFileSizeFromCollections();
+
+
+        topKFileSizeFromCollections.addFileToDirectory("file1.txt", 100, "");
+        topKFileSizeFromCollections.addFileToDirectory("file2.txt", 100, "collection1");
+        topKFileSizeFromCollections.addFileToDirectory("file3.txt", 200, "collection1");
+        topKFileSizeFromCollections.addFileToDirectory("file4.txt", 300, "collection3");
+        topKFileSizeFromCollections.addFileToDirectory("file5.txt", 400, "collection4");
     }
 }
