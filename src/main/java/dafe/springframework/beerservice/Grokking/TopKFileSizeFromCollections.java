4cff3adea9387f7231d25c6a57e5bba576844a21
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-14 22:22:22 -0700

    WIP X

--- src/main/java/dafe/springframework/beerservice/Grokking/TopKFileSizeFromCollections.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/TopKFileSizeFromCollections.java
@@ -36,11 +36,11 @@ public class TopKFileSizeFromCollections {
         TopKFileSizeFromCollections topKFileSizeFromCollections = new TopKFileSizeFromCollections();
 
 
-        topKFileSizeFromCollections.addFileToDirectory("file1.txt", 100, "");
-        topKFileSizeFromCollections.addFileToDirectory("file2.txt", 100, "collection1");
-        topKFileSizeFromCollections.addFileToDirectory("file3.txt", 200, "collection1");
-        topKFileSizeFromCollections.addFileToDirectory("file4.txt", 300, "collection3");
-        topKFileSizeFromCollections.addFileToDirectory("file5.txt", 400, "collection4");
+        addFileToDirectory("file1.txt", 100, "");
+        addFileToDirectory("file2.txt", 100, "collection1");
+        addFileToDirectory("file3.txt", 200, "collection1");
+        addFileToDirectory("file4.txt", 300, "collection3");
+        addFileToDirectory("file5.txt", 400, "collection4");
     }
 
     static class File{
