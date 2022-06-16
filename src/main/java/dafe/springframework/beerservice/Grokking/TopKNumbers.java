9c617c7f711a8f3d310a241b56425900c096ca14
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-15 20:27:09 -0700

    WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/TopKNumbers.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/TopKNumbers.java
@@ -1,4 +1,23 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TopKNumbers {
+
+
+    private static List<Integer> findKLargestNumbers(int[] ints, int n) {
+
+        return new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> result =
+                findKLargestNumbers(new int[] { 3, 1, 5, 12, 2, 11 }, 3);
+        System.out.println("Here are the top K numbers: " + result);
+
+        result = findKLargestNumbers(new int[] { 5, 12, 11, -1, 12 }, 3);
+        System.out.println("Here are the top K numbers: " + result);
+    }
+
 }
