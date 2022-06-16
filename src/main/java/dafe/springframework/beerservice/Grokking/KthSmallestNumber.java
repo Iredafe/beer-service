55f65644fb8b4a681e53dcb41dbd41e7a9e80172
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-15 21:50:58 -0700

    WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/KthSmallestNumber.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/KthSmallestNumber.java
@@ -1,4 +1,22 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class KthSmallestNumber {
+
+    public static void main(String[] args) {
+        int result =
+                findKthSmallestNumber(new int[] { 1, 5, 12, 2, 11, 5 }, 3);
+        System.out.println("Kth smallest number is: " + result);
+
+        // since there are two 5s in the input array, our 3rd and 4th smallest numbers
+        // should be a '5'
+        result =
+                findKthSmallestNumber(new int[] { 1, 5, 12, 2, 11, 5 }, 4);
+        System.out.println("Kth smallest number is: " + result);
+
+        result =
+                findKthSmallestNumber(new int[] { 5, 12, 11, -1, 12 }, 3);
+        System.out.println("Kth smallest number is: " + result);  }
+
+
+}
 }
