0848de5577453820ec042854db3974d736e476a7
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-20 00:00:42 -0700

    WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/RateLimit2.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/RateLimit2.java
@@ -1,4 +1,15 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.HashMap;
+import java.util.Queue;
+
 public class RateLimit2 {
+
+    public int REQUEST_LIMIT = 100;
+    public int TIME_LIMIT = 300;
+    HashMap<String, HitCounter> clientHitMap = new HashMap<>();
+
+    class HitCounter{
+        Queue<Integer> requestQueue;
+    }
 }
