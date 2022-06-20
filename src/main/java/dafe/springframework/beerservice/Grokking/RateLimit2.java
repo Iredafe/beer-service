19ff6a44bcbe3dc8c38c56445c079afa0195e7d5
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-20 00:09:46 -0700

    WIP IV

--- src/main/java/dafe/springframework/beerservice/Grokking/RateLimit2.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/RateLimit2.java
@@ -5,31 +5,35 @@ import java.util.Queue;
 
 public class RateLimit2 {
 
-    public int REQUEST_LIMIT = 100;
-    public int TIME_LIMIT = 300;
-    HashMap<String, HitCounter> clientHitMap = new HashMap<>();
-
-    class HitCounter{
-        Queue<Integer> requestQueue;
-
-        public boolean makeRequest(int timestamp){
-            //clean up
-            while (!requestQueue.isEmpty()){
-                if(timestamp - requestQueue.peek() >= TIME_LIMIT){
-                    requestQueue.remove();
-                }else break;
-            }
-
-            if(requestQueue.size() < REQUEST_LIMIT){
-                requestQueue.add(timestamp);
-                return true;
-            }
-            return false;
-        }
-    }
+    static HashMap<String, HitCounter> clientHitMap = new HashMap<>();
 
 
     public static boolean isRequestAllowed(String clientID, int currentTime){
+        if(!clientHitMap.containsKey(clientID)){
+            RequestCounter requestCounter = new RequestCounter();
+        }
+    }
+}
 
+
+class RequestCounter{
+
+    public static int REQUEST_LIMIT = 100;
+    public static int TIME_LIMIT = 300;
+    static Queue<Integer> requestQueue;
+
+    public static boolean makeRequest(int timestamp){
+        //clean up
+        while (!requestQueue.isEmpty()){
+            if(timestamp - requestQueue.peek() >= TIME_LIMIT){
+                requestQueue.remove();
+            }else break;
+        }
+
+        if(requestQueue.size() < REQUEST_LIMIT){
+            requestQueue.add(timestamp);
+            return true;
+        }
+        return false;
     }
 }
