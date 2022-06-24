3e1a8dcf98c3fbc19a041e922d6c3d7a6673f9d0
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-23 20:55:42 -0700

    WIP VII

--- src/main/java/dafe/springframework/beerservice/FileSystem.java
+++ src/main/java/dafe/springframework/beerservice/FileSystem.java
@@ -3,10 +3,12 @@ package dafe.springframework.beerservice;
 import io.micrometer.core.instrument.util.AbstractPartition;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class FileSystem  {
 
+    HashMap<String, File> fileCollectionMap = new HashMap<>();
     class FileCollection{
         String collectionName;
         int size;
@@ -20,10 +22,18 @@ public class FileSystem  {
 
     class File{
         String fileName;
+        int size;
+        FileCollection fileCollection;
     }
+
     private static List<FileCollection> addFile(String fileName, int size, String fileCollection){
 
         return new ArrayList<>();
+    }
+
+    private static int totalSizeOfFiles(){
+
+        return 0;
     }
 
     private static List<FileCollection> getTopNFiles(int numberLimit){
@@ -42,7 +52,7 @@ public class FileSystem  {
         System.out.println("Test that files can exist without a collection : " + addFile(fileName3, 120, ""));
         System.out.println("test Number of files added successfully to collection : " + addFile(fileName1, 200, fileCollection1));
         System.out.println("test Number of files added successfully to collection : " + addFile(fileName2, 200, fileCollection2));
-
+        System.out.println("test total number of files are correctly computed : " + totalSizeOfFiles());
         System.out.println("Test top n files in collection : " + getTopNFiles(n));
         /*
         file2.txt(size: 200) in collection "collection1"
@@ -55,6 +65,20 @@ public class FileSystem  {
 
         test that the total no of files is accurate - red->green
         test that the top n files are return accurately by the function
+
+        *solution plan
+        create a map that can hold key-value pairs of fileName -> File && collectionName -> FileCollection
+        create a maxHeap to compute the top n files
+        check if the input has a collection name
+            - if it does not have a collection name, then it is a file with no collection
+                * store it in the map as a file object
+            - if the input has a collection name, then it is a file that belongs to a collection
+                * store the mapping of collectionName -> collection in the map.
+
+            add the collections that have been built up inside the maxheap
+            -the maxheap will sort the files in descending order of size
+            -we can pop out the smallest collections in the heap and leave only n number of collections inside the heap.
+            -compute the total size by adding all the sizes of both files in a collection and files not in a collection.
 
         */
     }
