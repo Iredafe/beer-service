e79fd0e0f01c2882544e3fb4c92f0e6153b03bcd
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-23 20:41:50 -0700

    WIP VI

--- src/main/java/dafe/springframework/beerservice/FileSystem.java
+++ src/main/java/dafe/springframework/beerservice/FileSystem.java
@@ -1,49 +1,45 @@
 package dafe.springframework.beerservice;
 
+import io.micrometer.core.instrument.util.AbstractPartition;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileSystem  {
 
-
-    class File{
-        String fileName;
+    class FileCollection{
+        String collectionName;
         int size;
-        FileCollection collection;
-
-        public File(String fileName, int size){
-            this.fileName = fileName;
-            this.size=size;
-        }
-
-        public File(String fileName, int size, FileCollection collection){
-            this.fileName=fileName;
-            this.size=size;
-            this.collection=collection;
-        }
+        List<File> files;
     }
 
-    class FileCollection{
-     String collectionName;
-     int size;
-     List<File> files;
+    private static List<FileCollection> addFile(String fileName, int size, FileCollection fileCollection){
 
-     public FileCollection(String collectionName){
-         this.collectionName=collectionName;
-     }
+        return new ArrayList<>();
+    }
+
+    private static List<FileCollection> getTopNFiles(int numberLimit){
+
+        return new ArrayList<>();
     }
 
     public static void main(String[] args) {
         System.out.println("Hello World");
         String fileName1 = "file1.txt";
         String fileName2 = "file2.txt";
-        System.out.println("Number of files in collection : " + addFile(fileName1, 200, collection1));
-        System.out.println("Number of files in collection : " + addFile(fileName2, 200, collection1));
+        String fileName3 = "file3.txt";
+        String fileCollection1 = "collection1";
+        String fileCollection2 = "collection2";
+
+        System.out.println("Test that files can exist without a collection : " + addFile(fileName3, 120, ""));
+        System.out.println("test Number of files added successfully to collection : " + addFile(fileName1, 200, fileCollection1));
+        System.out.println("test Number of files added successfully to collection : " + addFile(fileName2, 200, fileCollection2));
 
         System.out.println("Test top n files in collection : " + getTopNFiles(n));
         /*
-file2.txt(size: 200) in collection "collection1"
-file3.txt(size: 200) in collection "collection1"
-file4.txt(size: 300) in collection "collection2"
+        file2.txt(size: 200) in collection "collection1"
+        file3.txt(size: 200) in collection "collection1"
+        file4.txt(size: 300) in collection "collection2"
 
         *test cases
         test that files can be added to the collection successfully
