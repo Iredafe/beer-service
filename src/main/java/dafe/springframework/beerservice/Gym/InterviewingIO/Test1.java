ff5146767f087377bac55ab03c942ed498f37d0c
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-25 21:43:50 -0700

    WIP I

--- src/main/java/dafe/springframework/beerservice/Gym/InterviewingIO/Test1.java
+++ src/main/java/dafe/springframework/beerservice/Gym/InterviewingIO/Test1.java
@@ -1,4 +1,35 @@
 package dafe.springframework.beerservice.Gym.InterviewingIO;
+/*
+ Spend a gift card on food items for a meal at a restaurant
 
+Constraints/Hints:
+- must use 100% of the gift card value
+- any item can be purchased an unlimited number of times
+
+Inputs:
+- $0 < all money values <= $1,000
+- 1 <= menu size <= 100
+- menu names/prices are unique to each other
+
+
+Part One:
+
+- write a method that processes a single gift card amount at a time
+- return an array/list of strings indicating the menu item names which spend 100% of the gift card
+- return an empty array/list if you cannot find an answer.
+- more than one answer might be possible, stop and return the FIRST one you find
+
+Example:
+    menu:
+    $15 full dinner
+    $4 sandwich
+    $3 milkshake
+
+purchase_food(10.00)
+    ... would return ['sandwich', 'milkshake', 'milkshake']
+purchase_food(5.00)
+    ... would return []
+
+ */
 public class Test1 {
 }
