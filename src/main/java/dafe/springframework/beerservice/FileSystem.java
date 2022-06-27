babe91caf12dd17cf0fcb3a156f67430f4d5a7ed
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-26 22:14:56 -0700

    WIP IX

--- src/main/java/dafe/springframework/beerservice/FileSystem.java
+++ src/main/java/dafe/springframework/beerservice/FileSystem.java
@@ -1,4 +1,15 @@
 package dafe.springframework.beerservice;
 
 public class FileSystem {
+
+    private static void addFileToSystem(String fileName, int fileSize, String collectionName){
+
+    }
+
+
+    public static void main(String[] args) {
+       // file2.txt(size: 200) in collection "collection1"
+    addFileToSystem("file2.txt", 200, "collection1");
+
+    }
 }
