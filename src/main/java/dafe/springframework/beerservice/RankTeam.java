3fac669c06057451472fbbbb83ed7474a35cfcae
Dafe owolabi <iredafeowolabi@gmail.com>
2022-06-26 17:48:43 -0700

    WIP II

--- src/main/java/dafe/springframework/beerservice/RankTeam.java
+++ src/main/java/dafe/springframework/beerservice/RankTeam.java
@@ -1,5 +1,7 @@
 package dafe.springframework.beerservice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class RankTeam {
@@ -33,5 +35,17 @@ public class RankTeam {
 
      */
 
+    public static void main(String [] args){
+        List<String> voteList = new ArrayList<>();
+        voteList.add("XYZ");
+        voteList.add("XZY");
+        voteList.add("XZY");
+        voteList.add("XYZ");
+        voteList.add("XZY");
+        voteList.add("XYZ");
+        List<List<String>> votes = Arrays.asList(voteList);
+
+        System.out.println("This is the winner of vote in order of 1st, 2nd and 3rd : " + rankTeam(votes));
+    }
 
 }
