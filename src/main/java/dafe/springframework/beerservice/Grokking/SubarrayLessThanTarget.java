a78d97b10bd127e474dd344d9c7184695176626a
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-06 20:35:39 -0700

    WIP II

--- src/main/java/dafe/springframework/beerservice/Grokking/SubarrayLessThanTarget.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/SubarrayLessThanTarget.java
@@ -12,5 +12,7 @@ public class SubarrayLessThanTarget {
     public static void main(String[] args) {
         System.out.println(
               findSubarrays(new int[] { 2, 5, 3, 10 }, 30));
+        System.out.println(
+                findSubarrays(new int[] { 8, 2, 6, 5 }, 50));
     }
 }
