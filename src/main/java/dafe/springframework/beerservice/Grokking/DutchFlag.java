2493f20150d6a9281ce9a25250df96cdfad5bd81
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-06 21:47:53 -0700

    WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/DutchFlag.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/DutchFlag.java
@@ -2,6 +2,10 @@ package dafe.springframework.beerservice.Grokking;
 
 public class DutchFlag {
 
+    private static int[] sort(int [] array){
+
+        return new int[]{};
+    }
 
     public static void main(String[] args) {
         int[] arr = new int[] { 1, 0, 2, 1, 0 };
