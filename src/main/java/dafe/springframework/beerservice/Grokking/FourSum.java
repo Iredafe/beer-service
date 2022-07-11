d923307352e6f5f7fe83507e5ddb48416dab63ec
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-10 21:30:50 -0700

    WIP II

--- src/main/java/dafe/springframework/beerservice/Grokking/FourSum.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/FourSum.java
@@ -4,5 +4,6 @@ public class FourSum {
 
     public static void main(String[] args) {
         System.out.println(searchQuadruplets(new int[] { 4, 1, 2, -1, 1, -3 }, 1));
+        System.out.println(searchQuadruplets(new int[] { 2, 0, -1, 1, -2, 2 }, 2));
     }
 }
