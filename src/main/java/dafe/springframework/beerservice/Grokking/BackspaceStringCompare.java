3484ef934522ed21e48b83e6d8ed2855076a9b77
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-11 22:45:02 -0700

    WIP III

--- src/main/java/dafe/springframework/beerservice/Grokking/BackspaceStringCompare.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/BackspaceStringCompare.java
@@ -2,7 +2,13 @@ package dafe.springframework.beerservice.Grokking;
 
 public class BackspaceStringCompare {
     private static boolean compare(String firstString, String secondString){
+        int index1 = firstString.length()-1;
+        int index2 = secondString.length()-1;
 
+        while(index1 >=0 || index2 >= 0){
+            int validCharIndex1 = getValidCharIndex(index1, firstString);
+            int validCharIndex2 = getValidCharIndex(index2, secondString);
+        }
         return false;
     }
 
