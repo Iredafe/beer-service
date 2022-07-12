d9e120dc8324f2db989077e8e93279f6e3a3a43b
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-11 22:38:04 -0700

    WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/BackspaceStringCompare.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/BackspaceStringCompare.java
@@ -1,4 +1,12 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class BackspaceStringCompare {
+
+    public static void main(String[] args) {
+
+        System.out.println(compare("xy#z", "xzz#"));
+        System.out.println(compare("xy#z", "xyz#"));
+        System.out.println(compare("xp#", "xyz##"));
+        System.out.println(compare("xywrrmp", "xywrrmu#p"));
+    }
 }
