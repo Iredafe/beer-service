e555b479ab3bfe565f8168524ad98b473c327627
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-12 22:27:54 -0700

    minimum window sort WIP III

--- src/main/java/dafe/springframework/beerservice/Grokking/MinimumWindowSort.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/MinimumWindowSort.java
@@ -4,6 +4,7 @@ public class MinimumWindowSort {
 
     public static void main(String[] args) {
         System.out.println(sort(new int[] { 1, 2, 5, 3, 7, 10, 9, 12 }));
+        System.out.println(sort(new int[] { 1, 3, 2, 0, -1, 7, 10 }));
 
     }
 }
