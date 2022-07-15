30136098bcccc8c651ff87e0ab2b64a397e69178
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-14 22:37:16 -0700

    WIP VII

--- src/main/java/dafe/springframework/beerservice/Grokking/LinkedListCycle.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/LinkedListCycle.java
@@ -23,5 +23,6 @@ public class LinkedListCycle {
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
     }
 }
