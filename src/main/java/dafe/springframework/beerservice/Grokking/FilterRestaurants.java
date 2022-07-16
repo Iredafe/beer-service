7ca0ca76376bbbd567552476ad6cbc521986d673
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-15 22:44:37 -0700

    Filter Restaurants V

--- src/main/java/dafe/springframework/beerservice/Grokking/FilterRestaurants.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/FilterRestaurants.java
@@ -15,5 +15,7 @@ public class FilterRestaurants {
                                         {3,8,1,30,4},
                                         {4,10,0,10,3},
                                         {5,1,1,15,1}};
+
+    int veganFriendly = 1, maxPrice = 50, maxDistance = 10;
     }
 }
