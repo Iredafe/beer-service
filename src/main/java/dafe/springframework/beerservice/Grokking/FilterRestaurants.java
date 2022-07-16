e3df5a303e7dda751e88afb8bf84cca788121eb6
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-15 23:41:22 -0700

    Filter Restaurants VIII

--- src/main/java/dafe/springframework/beerservice/Grokking/FilterRestaurants.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/FilterRestaurants.java
@@ -1,13 +1,20 @@
 package dafe.springframework.beerservice.Grokking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FilterRestaurants {
 
     private static List<Integer> filterRestaurants(int[][] restaurants, int veganFriendly, int maxPrice, int maxDistance){
-
-        return new ArrayList<>();
+        return Arrays.stream(restaurants).filter(s->s[2]>=veganFriendly && s[3]<= maxPrice && s[4]<=maxDistance)
+                .sorted((a,b)-> {
+                    if(a[1] == b[1]) return b[0]-a[0];
+                    else return b[1]-a[1];
+                })
+                .map(i->i[0])
+                .collect(Collectors.toList());
     }
     public static void main(String[] args) {
     int [][] restaurants = new int[][]{{1,4,1,40,10},
@@ -18,6 +25,6 @@ public class FilterRestaurants {
 
     int veganFriendly = 1, maxPrice = 50, maxDistance = 10;
 
-    filterRestaurants(restaurants, veganFriendly, maxPrice, maxDistance);
+        System.out.println(filterRestaurants(restaurants, veganFriendly, maxPrice, maxDistance));
     }
 }
