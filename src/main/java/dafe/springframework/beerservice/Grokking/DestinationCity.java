ba5d79e5f82eecc8efb6ab3a5e8946600d6bd2bc
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-18 22:46:04 -0700

    destination city WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/DestinationCity.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/DestinationCity.java
@@ -1,8 +1,12 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class DestinationCity {
 
     public static void main(String[] args) {
-
+        List<List<String>> paths = Arrays.asList(Arrays.asList("London","New York"),Arrays.asList("New York","Lima"),
+                Arrays.asList("Lima","Sao Paulo"));
     }
 }
