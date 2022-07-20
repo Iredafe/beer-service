6338c3eac7d7b44921c8ab6dfb75364fa8195da5
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-19 21:54:29 -0700

    start of linkedlist cycle WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/StartOfLinkedListCycle.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/StartOfLinkedListCycle.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class StartOfLinkedListCycle {
+
+    static class ListNode{
+        int value=0;
+        ListNode next;
+
+        public ListNode(ListNode next){
+            this.next = next;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
 }
