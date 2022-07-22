e8033c676545fd0319a11c1e4263c7ea37a0ebc7
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-21 22:12:28 -0700

    implement prefix tree WIP XI

--- src/main/java/dafe/springframework/beerservice/Grokking/ImplementPrefixTree.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/ImplementPrefixTree.java
@@ -2,42 +2,48 @@ package dafe.springframework.beerservice.Grokking;
 
 public class ImplementPrefixTree {
 
-     class TrieNode{
-         public static final int R=26;
-         public TrieNode[] links;
-         public boolean isEnd;
+    static class TrieNode {
+        public static final int R = 26;
+        public TrieNode[] links;
+        public boolean isEnd;
 
-         public TrieNode() {
-             links = new TrieNode[R];
-         }
-
-        public void put(char ch, TrieNode node){
-            links[ch-'a']=node;
+        public TrieNode() {
+            links = new TrieNode[R];
         }
 
-        public void setEnd(){
-            isEnd=true;
+        public void put(char ch, TrieNode node) {
+            links[ch - 'a'] = node;
         }
 
-        public TrieNode get(char ch){
-            return links[ch-'a'];
+        public void setEnd() {
+            isEnd = true;
         }
 
-        public boolean containsKey(char ch){
-            return links[ch-'a'] != null;
+        public TrieNode get(char ch) {
+            return links[ch - 'a'];
         }
 
-        public boolean isEnd(){
-             return isEnd;
+        public boolean containsKey(char ch) {
+            return links[ch - 'a'] != null;
         }
-     }
+
+        public boolean isEnd() {
+            return isEnd;
+        }
     }
 
-    class Trie {
+
+    static class Trie {
+        public TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
 
         public static void insert(String word) {
-
+            if ()
         }
+
         public static boolean search(String word) {
 
         }
@@ -45,4 +51,6 @@ public class ImplementPrefixTree {
         public static boolean startsWith(String prefix) {
 
         }
+
+    }
 }
