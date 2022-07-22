b02cf8836fe1f60ae53e5139e0adc989a0b6fd3d
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-21 21:24:56 -0700

    implement prefix tree WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/ImplementPrefixTree.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/ImplementPrefixTree.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class ImplementPrefixTree {
+    class Trie {
+
+        public Trie() {
+
+        }
+
+        public void insert(String word) {
+
+        }
 }
