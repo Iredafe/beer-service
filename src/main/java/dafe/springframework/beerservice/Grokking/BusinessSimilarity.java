281b17140c4d64d682d2b5a3113141878b41a5fa
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-22 22:04:20 -0700

    business similiarity WIP VI

--- src/main/java/dafe/springframework/beerservice/Grokking/BusinessSimilarity.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/BusinessSimilarity.java
@@ -1,12 +1,7 @@
 package dafe.springframework.beerservice.Grokking;
 
-import org.apache.tomcat.util.json.JSONParser;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class BusinessSimilarity {
     static class PositiveReview {
@@ -32,31 +27,6 @@ public class BusinessSimilarity {
 
 
     public static void main(String[] args) {
-
-        String str ="";
-        Scanner sc=new Scanner(System.in);
-
-        while(sc.hasNextLine()){ // reading file till EOF
-            str+=sc.nextLine();
-        }
-        try{
-            JSONParser parser = new JSONParser(str); // parsing input as json object
-            JSONObject json = (JSONObject) parser.parse();
-            JSONArray lis1 = (JSONArray)json.get("positive_reviews"); //
-            List<PositiveReview> lis = new ArrayList<>(); // making list from input json fields
-            for (int i = 0; i < lis1.length(); i++) {
-                JSONObject js = (JSONObject) lis1.get(i);
-                PositiveReview ps = new PositiveReview(Integer.parseInt(js.get("user_id").toString()),Integer.parseInt(js.get("business_id").toString()));
-                lis.add(ps);
-            }
-            System.out.println(findMostSimilarBusiness(Integer.parseInt(json.get("business_of_interest_id").toString()),lis));
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
-
-
 
     }
 }
