74f377d1830faead7d177b3061fea66e68c2274d
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-24 21:58:11 -0700

    prefix based search WIP I

--- src/main/java/dafe/springframework/beerservice/Grokking/PrefixBasedSearch.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/PrefixBasedSearch.java
@@ -4,6 +4,7 @@ public class PrefixBasedSearch {
 
 
     public static void main(String[] args) {
-
+        String business_names[] = { "burger king", "McDonald's", "super duper burger's", "subway", "pizza hut"};
+        String searchTerm = "duper bur";
     }
 }
