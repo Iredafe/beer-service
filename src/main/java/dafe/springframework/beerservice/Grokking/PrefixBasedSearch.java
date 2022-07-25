622cbc1cd6b053f7bde0d174549e6877c6cb025d
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-24 21:59:33 -0700

    prefix based search WIP II

--- src/main/java/dafe/springframework/beerservice/Grokking/PrefixBasedSearch.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/PrefixBasedSearch.java
@@ -4,7 +4,13 @@ public class PrefixBasedSearch {
 
 
     public static void main(String[] args) {
-        String business_names[] = { "burger king", "McDonald's", "super duper burger's", "subway", "pizza hut"};
-        String searchTerm = "duper bur";
+        String[] input = {"burger king", "McDonald's", "super duper burger's", "subway", "pizza hut"};
+        String searchTerm1 = "bur";
+        String searchTerm2 = "duper bur";
+        String searchTerm3 = "bur duper";
+        PrefixSearch ps = new PrefixSearch();
+        System.out.println(ps.getRelatedString(Arrays.asList(input), searchTerm1));
+        System.out.println(ps.getRelatedString(Arrays.asList(input), searchTerm2));
+        System.out.println(ps.getRelatedString(Arrays.asList(input), searchTerm3));
     }
 }
