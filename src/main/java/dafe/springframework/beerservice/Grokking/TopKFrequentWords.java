f119df20b226af66794bbd3a7218dfef66a4646f
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-28 22:48:38 -0700

    top k frequent words WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/TopKFrequentWords.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/TopKFrequentWords.java
@@ -4,5 +4,7 @@ public class TopKFrequentWords {
 
     public static void main(String[] args) {
         String [] words = {"i","love","leetcode","i","love","coding"}; int k=2;
+
+        System.out.println("Top " + k + " frequent words are : " + getTopKFrequentWords(words, k));
     }
 }
