d9cbf51a227b29fa0c9f52deb478a62901255a0a
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-29 18:56:59 -0700

    top k frequent words 1 WIP V

--- src/main/java/dafe/springframework/beerservice/Grokking/TopKFrequentWords1.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/TopKFrequentWords1.java
@@ -1,22 +1,21 @@
 package dafe.springframework.beerservice.Grokking;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class TopKFrequentWords1 {
     private static List<String> topKFrequentWords(String [] wordArray, int k){
-
         HashMap<String , Integer> wordFrequencyMap = new HashMap<>();
         List<String> result = new ArrayList<>();
         for(String word : wordArray){
             if(!wordFrequencyMap.containsKey(word)){
                 wordFrequencyMap.put(word, 1);
             }else{
-                wordFrequencyMap.put(word,wordFrequencyMap.get(word)+1)
+                wordFrequencyMap.put(word,wordFrequencyMap.get(word)+1);
             }
         }
 
+        PriorityQueue<Map.Entry<String,Integer>> heap = new PriorityQueue<>((a,b) -> a.getValue()==b.getValue()
+                ? a.getKey().compareTo(b.getKey()) : a.getValue()-b.getValue());
         return result;
     }
 
