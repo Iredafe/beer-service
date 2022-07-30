80e210a9a970c0e1ab70c7ffdee5c517dc8afcf6
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-29 20:42:38 -0700

    vegan friendly WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/VeganFriendly.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/VeganFriendly.java
@@ -1,4 +1,10 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class VeganFriendly {
+
+
+    public static void main(String[] args) {
+        int [][] restaurant = new int [][] {{1,4,1,40,10},{2,8,0,50,5},{3,8,1,30,4},{4,10,0,10,3},{5,1,1,15,1}};
+        int veganFriendly = 1, maxPrice = 50, maxDistance = 10;
+    }
 }
