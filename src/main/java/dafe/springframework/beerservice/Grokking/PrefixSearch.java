441086980a156973b85058db48fa55a8e53e5be6
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-29 22:12:54 -0700

    prefix search WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/PrefixSearch.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/PrefixSearch.java
@@ -1,4 +1,11 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.HashMap;
+
 public class PrefixSearch {
+
+    static class TrieNode{
+        boolean isEnd = false;
+        HashMap<Character, TrieNode> dictionary = new HashMap<>();
+    }
 }
