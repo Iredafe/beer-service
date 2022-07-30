3fdad4a3e51b34312950382ab78967bcfc47fa2a
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-29 18:41:54 -0700

    top k frequent words 1 WIP I

--- src/main/java/dafe/springframework/beerservice/Grokking/TopKFrequentWords1.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/TopKFrequentWords1.java
@@ -1,7 +1,13 @@
 package dafe.springframework.beerservice.Grokking;
 
-public class TopKFrequentWords1 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class TopKFrequentWords1 {
+    private static List<String> topKFrequentWords(String [] wordArray, int k){
+
+        return new ArrayList<>();
+    }
 
     public static void main(String[] args) {
         String [] words = {"the","day","is","sunny","the","the","the","sunny","is","is"};
