f4bff316dc88bb00e6b7e6cd1f48225585f6eaf9
Dafe owolabi <iredafeowolabi@gmail.com>
2022-07-31 22:56:36 -0700

    are sentences similar done

--- src/main/java/dafe/springframework/beerservice/Grokking/AreSentencesSimilar.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/AreSentencesSimilar.java
@@ -18,12 +18,10 @@ public class AreSentencesSimilar {
 
         for(int i=0; i< sentence1.length; i++){
             if(sentence1[i].equals(sentence2[i])) continue;
+            if(!map.containsKey(sentence1[i])) return false;
             if(!map.get(sentence1[i]).contains(sentence2[i])) return false;
         }
-
-
-
-        return false;
+        return true;
     }
 
     public static void main(String[] args) {
