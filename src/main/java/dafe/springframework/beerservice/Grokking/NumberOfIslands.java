ba1b2de5d14fb7d602aea1b911aedda45404f1a7
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-01 21:07:35 -0700

    number of islands WIP V

--- src/main/java/dafe/springframework/beerservice/Grokking/NumberOfIslands.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/NumberOfIslands.java
@@ -5,16 +5,21 @@ public class NumberOfIslands {
     private static int countIslands(int [][] grid) {
         int rowLength = grid.length;
         int colLength = grid[0].length;
-
-        for(int row=0; row<rowLength; row++) {
+        int totalIslands = 0;
+        for (int row = 0; row < rowLength; row++) {
             for (int col = 0; col < colLength; col++) {
-                if(grid[row][col]==1){
-                    visitIslands(grid,row,col);
+                if (grid[row][col] == 1) {
+                    visitIslands(grid, row, col);
                     totalIslands++;
+                }
             }
         }
-        return 0;
+        return totalIslands;
     }
+        private static void visitIslands(int [][] matrix, int row, int col){
+            if(row<0||row>= matrix.length||col<0||col>= matrix[0].length) return;
+            if(matrix[row][col] == 0) continue;
+        }
 
     public static void main(String[] args) {
         System.out.println(" The total number Islands in the grid is " +countIslands(
