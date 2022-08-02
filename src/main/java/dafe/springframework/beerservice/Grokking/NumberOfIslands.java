9c131d062c9b59e97552e191f919758c382d7b6c
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-01 21:19:29 -0700

    number of islands BFS WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/NumberOfIslands.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/NumberOfIslands.java
@@ -9,23 +9,28 @@ public class NumberOfIslands {
         for (int row = 0; row < rowLength; row++) {
             for (int col = 0; col < colLength; col++) {
                 if (grid[row][col] == 1) {
-                    visitIslands(grid, row, col);
+                    visitIslandsDFS(grid, row, col);
                     totalIslands++;
                 }
             }
         }
         return totalIslands;
     }
-        private static void visitIslands(int [][] matrix, int row, int col){
+        private static void visitIslandsDFS(int [][] matrix, int row, int col){
             if(row<0||row>= matrix.length||col<0||col>= matrix[0].length) return;
             if(matrix[row][col] == 0) return;
             matrix[row][col]=0;
-            visitIslands(matrix, row+1, col);
-            visitIslands(matrix, row-1,col);
-            visitIslands(matrix, row, col-1);
-            visitIslands(matrix, row, col+1);
+            visitIslandsDFS(matrix, row+1, col);
+            visitIslandsDFS(matrix, row-1,col);
+            visitIslandsDFS(matrix, row, col-1);
+            visitIslandsDFS(matrix, row, col+1);
         }
 
+
+        private int getNumberOfIslands(int matrix){
+
+        return numberOfIslands;
+        }
     public static void main(String[] args) {
         System.out.println(" The total number Islands in the grid is " +countIslands(
                 new int[][] {
