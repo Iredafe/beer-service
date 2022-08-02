e53f0ff0ee4b1f780d141d0251b9a092d2180324
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-01 21:01:49 -0700

    number of islands WIP II

--- src/main/java/dafe/springframework/beerservice/Grokking/NumberOfIslands.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/NumberOfIslands.java
@@ -2,6 +2,11 @@ package dafe.springframework.beerservice.Grokking;
 
 public class NumberOfIslands {
 
+    private static int countIslands(int [][] grid) {
+
+        return 0;
+    }
+
     public static void main(String[] args) {
         System.out.println(" The total number Islands in the grid is " +countIslands(
                 new int[][] {
