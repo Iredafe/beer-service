21d65ba6699219534fff328e82f8706e5a936b0f
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-02 22:18:08 -0700

    flood fill done

--- src/main/java/dafe/springframework/beerservice/Grokking/FloodFill.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/FloodFill.java
@@ -15,8 +15,15 @@ public class FloodFill {
     private static void fillDFS(int [][] matrix, int i, int j, int oldColor, int newColor){
 
         if(i<0 || i>=matrix.length || j<0 || j>=matrix[0].length){
-
+            return;
         }
+        if(matrix[i][j] != oldColor) return;
+        matrix[i][j]=newColor;
+
+        fillDFS(matrix, i+1, j,oldColor, newColor);
+        fillDFS(matrix, i-1, j,oldColor, newColor);
+        fillDFS(matrix, i, j+1,oldColor, newColor);
+        fillDFS(matrix, i, j-1,oldColor, newColor);
 
     }
 
