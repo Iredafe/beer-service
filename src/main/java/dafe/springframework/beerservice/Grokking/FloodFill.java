1c080bfbf4b00f67ec2c1a0ed47a24f2e8b5fe7a
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-02 21:26:15 -0700

    flood fill started

---
+++ src/main/java/dafe/springframework/beerservice/Grokking/FloodFill.java
@@ -0,0 +1,24 @@
+package dafe.springframework.beerservice.Grokking;
+
+public class FloodFill {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(floodFill(
+                new int[][] {
+                        { 0, 1, 1, 1, 0 },
+                        { 0, 0, 0, 1, 1 },
+                        { 0, 1, 1, 1, 0 },
+                        { 0, 1, 1, 0, 0 },
+                        { 0, 0, 0, 0, 0 }
+                }, 1, 3, 2)));
+
+        System.out.println(Arrays.deepToString(floodFill(
+                new int[][] {
+                        { 0, 0, 0, 0, 0 },
+                        { 0, 0, 0, 0, 0 },
+                        { 0, 0, 1, 1, 0 },
+                        { 0, 0, 1, 0, 0 },
+                        { 0, 0, 1, 0, 0 }
+                }, 3, 2, 5)));
+    }
+}
