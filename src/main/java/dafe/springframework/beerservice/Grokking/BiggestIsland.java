72d46b90a51d074bfae7c5501a14ffef86954893
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-02 20:38:50 -0700

    biggest island WIP IV

--- src/main/java/dafe/springframework/beerservice/Grokking/BiggestIsland.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/BiggestIsland.java
@@ -15,9 +15,7 @@ public class BiggestIsland {
     }
 
     private static int maxAreaOfIsland(int[][] matrix) {
-
         int biggestIsland=0;
-
         for(int i=0; i<matrix.length; i++){
             for (int j=0; j<matrix[0].length; j++){
                 if(matrix[i][j]==1){
@@ -28,15 +26,19 @@ public class BiggestIsland {
         return biggestIsland;
     }
 
-
     private static int visitIslandDFS(int [][] matrix, int row, int col){
         int area=1;
-
         if(row<0|| row>=matrix.length || col <0 || col >=matrix[0].length){
             return 0;
         }
         if(matrix[row][col] == 0) return 0;
 
         matrix[row][col] =0;
+        area+=visitIslandDFS(matrix, row+1, col);
+        area+=visitIslandDFS(matrix,row-1,col);
+        area+=visitIslandDFS(matrix,row, col+1);
+        area+=visitIslandDFS(matrix, row, col-1);
+
+        return area;
     }
 }
