91af5b39aba05969f437bbc4f617da8daa35c706
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-02 20:17:38 -0700

    biggest island WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/BiggestIsland.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/BiggestIsland.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class BiggestIsland {
+
+
+    public static void main(String[] args) {
+        System.out.println(maxAreaOfIsland(
+                new int[][] {
+                        { 1, 1, 1, 0, 0 },
+                        { 0, 1, 0, 0, 1 },
+                        { 0, 0, 1, 1, 0 },
+                        { 0, 1, 1, 0, 0 },
+                        { 0, 0, 1, 0, 0 }
+                }));
+    }
 }
