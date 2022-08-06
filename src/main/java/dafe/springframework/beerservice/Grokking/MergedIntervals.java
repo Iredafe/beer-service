aefffda9bc58048604d6fd23ba3177e6feb8065d
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-05 20:25:42 -0700

    merged intervals WIP I

--- src/main/java/dafe/springframework/beerservice/Grokking/MergedIntervals.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/MergedIntervals.java
@@ -4,6 +4,13 @@ public class MergedIntervals {
 
 
     public static void main(String[] args) {
-
+        List<Interval> input = new ArrayList<Interval>();
+        input.add(new Interval(1, 4));
+        input.add(new Interval(2, 5));
+        input.add(new Interval(7, 9));
+        System.out.print("Merged intervals: ");
+        for (Interval interval : merge(input))
+            System.out.print("[" + interval.start + "," + interval.end + "] ");
+        System.out.println();
     }
 }
