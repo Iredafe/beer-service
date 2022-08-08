5f68e7b02c1981bd0bcb700a89c9001f99aa060e
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-07 22:14:25 -0700

    top frequent I

--- src/main/java/dafe/springframework/beerservice/Grokking/TopKFrequentElement.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/TopKFrequentElement.java
@@ -1,4 +1,14 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class TopKFrequentElement {
+
+    private static int[] topKFrequentElement(int [] array, int k){
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int [] array = {1,1,1,2,2,3};
+        int k = 3;
+    }
 }
