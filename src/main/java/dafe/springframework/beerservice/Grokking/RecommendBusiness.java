8dd55017b286671a13ddac1f5b7844f7be28cc82
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-09 21:11:45 -0700

    recommend business WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/RecommendBusiness.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/RecommendBusiness.java
@@ -1,4 +1,21 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RecommendBusiness {
+    class Business{
+        String name;
+        Map<Business, Integer> nearbyBusinesses = new HashMap<>();
+
+        public Business(String name){
+            this.name = name;
+        }
+
+        public String getName(){
+            return this.name;
+        }
+
+
+    }
 }
