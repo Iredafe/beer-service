624ca65938fd3b5f2c211374c48c2dfe9de5d5b0
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-10 14:14:26 -0700

    started I

--- src/main/java/dafe/springframework/beerservice/Grokking/ActiveBusiness.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/ActiveBusiness.java
@@ -1,4 +1,20 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.Arrays;
+
 public class ActiveBusiness {
+
+    public static void main(String[] args) {
+        List<Businesses> list = Arrays.asList(
+                new Businesses(EventTypes.ADS, 7, 3),
+                new Businesses(EventTypes.ADS, 8, 2),
+                new Businesses(EventTypes.ADS, 5, 1),
+                new Businesses(EventTypes.PAGE_VIEWS, 11, 2),
+                new Businesses(EventTypes.PAGE_VIEWS, 12, 3),
+                new Businesses(EventTypes.PHOTO_VIEWS, 10, 3),
+                new Businesses(EventTypes.REVIEWS, 7, 2)
+        );
+
+        System.out.println(findActiveBusinesses(list));
+    }
 }
