10942fd3138a6b633b7236b5103a2048c9a31649
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-10 10:25:11 -0700

    started II

--- src/main/java/dafe/springframework/beerservice/Grokking/FindSimilarBusinesses.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/FindSimilarBusinesses.java
@@ -1,13 +1,19 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.List;
+
 public class FindSimilarBusinesses {
 
-    class Business{
+   static class Reviews{
         int userId;
         int businessId;
-        public Business(int userId, int businessId){
+        public Reviews(int userId, int businessId){
             this.userId = userId;
             this.businessId = businessId;
         }
+    }
+
+    private static int findSimilarBusinesses(int businessOfInterestId, List<Reviews> reviewsList){
+
     }
 }
