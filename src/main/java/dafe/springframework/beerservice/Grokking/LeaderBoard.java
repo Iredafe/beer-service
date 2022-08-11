8852d6abbdc87ce60a0ed2889f207f44c2b0be90
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-11 10:17:22 -0700

    started V

--- src/main/java/dafe/springframework/beerservice/Grokking/LeaderBoard.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/LeaderBoard.java
@@ -1,7 +1,6 @@
 package dafe.springframework.beerservice.Grokking;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class LeaderBoard {
 
@@ -19,11 +18,22 @@ public class LeaderBoard {
 
     private static List<String> leaderboard(List<Entry> entryList){
         HashMap<String, Integer> receiverFrequencyCount = new HashMap<>();
+        List<String> result=new ArrayList<>();
 
         for (Entry entry: entryList){
             receiverFrequencyCount.put(entry.to, receiverFrequencyCount.getOrDefault(entry.to, 0)+1);
         }
 
+        PriorityQueue<Map.Entry<String, Integer>> heap = new PriorityQueue<>((a, b)->b.getValue()- a.getValue());
+        PriorityQueue<Map.Entry<String, Integer>> heap1 = new PriorityQueue<>((a, b)-> Integer.compare(b.getValue(), a.getValue()));
+
+        for(Map.Entry<String,Integer> entry : receiverFrequencyCount.entrySet()){
+            heap.add(entry);
+        }
+
+        for(int i=0; i<heap.size(); i++ ){
+            result.add(heap.poll().getKey());
+        }
         return result;
     }
 
