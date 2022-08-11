953defe7cbba9882042fa7f9b31726e0e7c88396
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-11 09:49:27 -0700

    started I

--- src/main/java/dafe/springframework/beerservice/Grokking/LeaderBoard.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/LeaderBoard.java
@@ -1,4 +1,19 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class LeaderBoard {
+
+    class Entry{
+        String from;
+        String to;
+        String message;
+
+        public Entry(String from, String to, String message){
+            this.from=from;
+            this.to=to;
+            this.message=message;
+        }
+    }
+
+
+
 }
