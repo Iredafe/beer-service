f50de7463a53c8295750c03386debe9f5acd0e9b
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-13 20:56:43 -0700

    started VI

--- src/main/java/dafe/springframework/beerservice/Grokking/SimilarityIndex.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/SimilarityIndex.java
@@ -1,10 +1,18 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SimilarityIndex {
 
     private static double similarityIndex(String bizName1, String bizName2){
 
         double jacardSimilarityIndex=0;
+        Set<String> set1 = new HashSet<>();
+        Set<String> set2 = new HashSet<>();
+
+        String [] bizNameArray1 = bizName1.split(" ");
+        String [] bizNameArray2 = bizName2.split(" ");
 
         return jacardSimilarityIndex;
     }
