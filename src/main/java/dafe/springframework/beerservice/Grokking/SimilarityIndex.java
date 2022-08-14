42cafbdfd5d1f3e71ecb07b8bcbccc327e791c7b
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-13 21:35:56 -0700

    started XIII

--- src/main/java/dafe/springframework/beerservice/Grokking/SimilarityIndex.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/SimilarityIndex.java
@@ -1,5 +1,6 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -10,23 +11,27 @@ public class SimilarityIndex {
         double jacardSimilarityIndex=0;
         Set<String> set1 = new HashSet<>();
         Set<String> set2 = new HashSet<>();
+        HashMap<String, Integer> map = new HashMap<>();
 
         String [] bizNameArray1 = bizName1.replace("?","").split(" ");
         String [] bizNameArray2 = bizName2.replace("?","").split(" ");
 
         for(String word : bizNameArray1){
             set1.add(word);
+            map.put(word, map.getOrDefault(word, 0)+1);
         }
 
         for (String word : bizNameArray2){
             set2.add(word);
+            map.put(word, map.getOrDefault(word, 0)+1);
         }
 
         set1.retainAll(set2);
         double intersectionSize = set1.size();
-        double union = bizNameArray1.length+bizNameArray2.length-intersectionSize;
+        double union1 = map.size();
+//        double union = bizNameArray1.length+bizNameArray2.length-intersectionSize;
 
-        jacardSimilarityIndex= intersectionSize/union;
+        jacardSimilarityIndex= intersectionSize/union1;
         return jacardSimilarityIndex;
     }
     public static void main(String[] args) {
