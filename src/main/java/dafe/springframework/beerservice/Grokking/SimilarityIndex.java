1252ba3dbf9d5617e3a49cc5a68141986c2ae87a
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-13 20:48:43 -0700

    started II

--- src/main/java/dafe/springframework/beerservice/Grokking/SimilarityIndex.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/SimilarityIndex.java
@@ -2,6 +2,12 @@ package dafe.springframework.beerservice.Grokking;
 
 public class SimilarityIndex {
 
+    private static double similarityIndex(String bizName1, String bizName2){
+
+        double jacardSimilarityIndex=0;
+
+        return jacardSimilarityIndex;
+    }
     public static void main(String[] args) {
 
     }
