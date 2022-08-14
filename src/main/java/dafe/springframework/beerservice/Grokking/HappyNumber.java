ca41a78e8a0b5b7a97cfd8b51e461c02510d872f
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-13 21:45:07 -0700

    started IX

--- src/main/java/dafe/springframework/beerservice/Grokking/HappyNumber.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/HappyNumber.java
@@ -2,7 +2,16 @@ package dafe.springframework.beerservice.Grokking;
 
 public class HappyNumber {
 
+    private static boolean find(int number){
+
+
+        return false;
+    }
+
+
     public static void main(String[] args) {
 
+        System.out.println(find(23));
+        System.out.println(find(12));
     }
 }
