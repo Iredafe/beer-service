27b8cb6baa1e151ea2f020cbe87501a8a23c92a2
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-15 19:13:15 -0700

    started I

--- src/main/java/dafe/springframework/beerservice/Grokking/DetectAndOrderBusiness.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/DetectAndOrderBusiness.java
@@ -1,4 +1,9 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class DetectAndOrderBusiness {
+
+    static class
+    public static void main(String[] args) {
+        Business business= new Business();
+    }
 }
