a07078e13dfc7ff1e0894d0fb362964a360263de
Dafe owolabi <iredafeowolabi@gmail.com>
2022-08-16 22:21:03 -0700

    started I

--- src/main/java/dafe/springframework/beerservice/Grokking/MatchTeam.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/MatchTeam.java
@@ -1,4 +1,13 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.Arrays;
+
 public class MatchTeam {
+
+    public static void main(String[] args) {
+        String[][] users =  {{"Alex","Infra"},{"Kevin","Search"},{"Jill","HR"},{"Jane","Infra"},{"Justin","Search"},{"Michael","HR"}};
+        List<String[]> lst = getMatchResult(users);
+        for(String[] res : lst)
+            System.out.println(Arrays.toString(res));
+    }
 }
