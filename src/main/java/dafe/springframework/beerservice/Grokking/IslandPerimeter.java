bde2c625b1e3e12136b7b22bbc596a2a6491288a
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-05 21:33:12 -0700

    island perimeter done

--- src/main/java/dafe/springframework/beerservice/Grokking/IslandPerimeter.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/IslandPerimeter.java
@@ -2,15 +2,37 @@ package dafe.springframework.beerservice.Grokking;
 
 public class IslandPerimeter {
 
-    private static int findIslandPerimeter(int [][] grid){
+    private static int getIslandPerimeter(int [][] grid){
 
-        int islandPerimeter = 0;
+        for(int i=0; i<grid.length; i++){
+            for (int j=0; j<grid[0].length; j++){
+                boolean[][] visited = new boolean[grid.length][grid[0].length];
+                if(grid[i][j]==1 && !visited[i][j]){
+                    return getIslandPerimeterDFS(grid, i, j, visited);
+                }
+            }
+        }
+        return 0;
+    }
 
-        return islandPerimeter;
+    private static int getIslandPerimeterDFS(int[][] matrix, int row, int col, boolean[][] isVisited){
+        if(row < 0 || row >= matrix.length || col < 0 || col >= matrix.length) return 1;
+        if(matrix[row][col] == 0) return 1;
+        if(isVisited[row][col]) return 0;
+        isVisited[row][col] = true;
+
+        int count = 0;
+
+        count+= getIslandPerimeterDFS(matrix, row+1, col, isVisited);
+        count+= getIslandPerimeterDFS(matrix, row-1, col, isVisited);
+        count+= getIslandPerimeterDFS(matrix, row, col+1, isVisited);
+        count+= getIslandPerimeterDFS(matrix, row, col-1, isVisited);
+
+        return count;
     }
 
     public static void main(String[] args) {
-    System.out.println(findIslandPerimeter(
+    System.out.println(getIslandPerimeter(
             new int[][] {
         { 1, 1, 0, 0, 0 },
         { 0, 1, 0, 0, 0 },
@@ -19,7 +41,7 @@ public class IslandPerimeter {
         { 0, 0, 0, 0, 0 }
     }));
 
-    System.out.println(findIslandPerimeter(
+    System.out.println(getIslandPerimeter(
             new int[][] {
         { 0, 0, 0, 0 },
         { 0, 1, 0, 0 },
