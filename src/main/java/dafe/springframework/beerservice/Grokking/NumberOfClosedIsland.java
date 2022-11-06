e352a0cc80d17ec80abda4d0a03cc801f65f6cfb
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-05 17:29:25 -0700

    adding method I

--- src/main/java/dafe/springframework/beerservice/Grokking/NumberOfClosedIsland.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/NumberOfClosedIsland.java
@@ -5,8 +5,23 @@ public class NumberOfClosedIsland {
     private static int countClosedIslands(int[][] matrix){
 
         int numberOfClosedIsland = 0;
+
+        int rowLength = matrix.length;
+        int colLength = matrix[0].length;
+
+        for(int row = 0; row< rowLength; row++){
+            for (int col = 0; col< colLength; col++){
+
+            }
+        }
+
         return numberOfClosedIsland;
     }
+
+    private static void dfs(int [][] matrix, int row, int col){
+
+    }
+
     public static void main(String[] args) {
         System.out.println(countClosedIslands(
                 new int[][] {
