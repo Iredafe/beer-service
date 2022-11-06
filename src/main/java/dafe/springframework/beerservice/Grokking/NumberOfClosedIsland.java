dbb1d7cf8aa387253695721c8c9f45bead1bfae1
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-05 18:05:57 -0700

    adding method III

--- src/main/java/dafe/springframework/beerservice/Grokking/NumberOfClosedIsland.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/NumberOfClosedIsland.java
@@ -2,17 +2,21 @@ package dafe.springframework.beerservice.Grokking;
 
 public class NumberOfClosedIsland {
 
+    static int rowLength;
+    static int colLength;
+
     private static int countClosedIslands(int[][] matrix){
-
+        rowLength =matrix.length;
+        colLength = matrix[0].length;
         int numberOfClosedIsland = 0;
+        boolean[][]visited = new boolean[][]{};
 
-        int rowLength = matrix.length;
-        int colLength = matrix[0].length;
 
         for(int row = 0; row< rowLength; row++){
             for (int col = 0; col< colLength; col++){
-                if(matrix[row][col] == 1){
-                    dfs(matrix, row, col);
+                if(matrix[row][col] == 1 && !visited[row][col]){
+                    if(isClosedIsland(matrix, row, col, visited));
+                    numberOfClosedIsland++;
                 }
             }
         }
@@ -20,8 +24,15 @@ public class NumberOfClosedIsland {
         return numberOfClosedIsland;
     }
 
-    private static void dfs(int [][] matrix, int row, int col){
+    private static boolean isClosedIsland(int [][] matrix, int row, int col, boolean [][]visited){
+        if (row<0 || row >= rowLength || col < 0 || col >= colLength)
+            return false;
 
+            visited[row][col]=true;
+            isClosedIsland(matrix,row+1,col,visited);
+            isClosedIsland(matrix,row-1,col,visited);
+            isClosedIsland(matrix,row,col+1,visited);
+            isClosedIsland(matrix,row,col-1,visited);
     }
 
     public static void main(String[] args) {
