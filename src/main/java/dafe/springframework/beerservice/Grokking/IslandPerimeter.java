15c02083e9cf2b22365de6f4a7337dcd6a5df44a
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-05 21:13:05 -0700

    island perimeter WIP

--- src/main/java/dafe/springframework/beerservice/Grokking/IslandPerimeter.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/IslandPerimeter.java
@@ -1,4 +1,32 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class IslandPerimeter {
+
+    private static int findIslandPerimeter(int [][] grid){
+
+        int islandPerimeter = 0;
+
+        return islandPerimeter;
+    }
+
+    public static void main(String[] args) {
+    System.out.println(findIslandPerimeter(
+            new int[][] {
+        { 1, 1, 0, 0, 0 },
+        { 0, 1, 0, 0, 0 },
+        { 0, 1, 0, 0, 0 },
+        { 0, 1, 1, 0, 0 },
+        { 0, 0, 0, 0, 0 }
+    }));
+
+    System.out.println(findIslandPerimeter(
+            new int[][] {
+        { 0, 0, 0, 0 },
+        { 0, 1, 0, 0 },
+        { 0, 1, 0, 0 },
+        { 0, 1, 1, 0 },
+        { 0, 1, 0, 0 }
+    }));
 }
+}
+
