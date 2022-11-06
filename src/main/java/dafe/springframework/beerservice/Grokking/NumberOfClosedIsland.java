63c95ec50c7410ca50c5ff767d365c8e30095a96
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-05 17:19:37 -0700

    add test

--- src/main/java/dafe/springframework/beerservice/Grokking/NumberOfClosedIsland.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/NumberOfClosedIsland.java
@@ -1,4 +1,24 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class NumberOfClosedIsland {
+
+    public static void main(String[] args) {
+        System.out.println(countClosedIslands(
+                new int[][] {
+                        { 1, 1, 0, 0, 0 },
+                        { 0, 1, 0, 0, 0 },
+                        { 0, 0, 1, 1, 0 },
+                        { 0, 1, 1, 0, 0 },
+                        { 0, 0, 0, 0, 0 }
+                }));
+
+        System.out.println(countClosedIslands(
+                new int[][] {
+                        { 0, 0, 0, 0 },
+                        { 0, 1, 0, 0 },
+                        { 0, 1, 0, 0 },
+                        { 0, 0, 1, 0 },
+                        { 0, 0, 0, 0 }
+                }));
+    }
 }
