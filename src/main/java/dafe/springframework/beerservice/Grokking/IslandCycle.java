49d4e6fa40fead90ff47bc78c0679cdbc8806989
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-06 21:50:24 -0700

    island cycle tests I

--- src/main/java/dafe/springframework/beerservice/Grokking/IslandCycle.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/IslandCycle.java
@@ -2,6 +2,19 @@ package dafe.springframework.beerservice.Grokking;
 
 public class IslandCycle {
 
+    private static boolean hasCycle(char [][] matrix){
+        boolean [][] visited = new boolean[matrix.length][matrix[0].length];
+        for(int row=0; row < matrix.length; row++){
+            for(int col =0; col<matrix[0].length; col++){
+                if(visited[row][col]) {
+                   return islandContainsCycle(matrix, visited, matrix[row][col],row, col, -1, -1);
+                }
+            }
+        }
+        return false;
+    }
+
+
     public static void main(String[] args) {
         System.out.println(hasCycle(
                 new char[][] {
