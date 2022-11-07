492bc9e3c532a3f5aeeb604531f5edc047432320
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-06 19:10:40 -0700

    distinct island done

--- src/main/java/dafe/springframework/beerservice/Grokking/NumberOfDistinctIsland.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/NumberOfDistinctIsland.java
@@ -24,15 +24,37 @@ public class NumberOfDistinctIsland {
     }
 
     private static int findDistinctIslandsDFS(int[][] grid) {
-        HashSet<StringBuilder> islandSet = new HashSet<>();
+        HashSet<String> islandSet = new HashSet<>();
+
         boolean [][] visited = new boolean[grid.length][grid[0].length];
         for(int row=0; row< grid.length; row++){
             for(int col=0; col<grid[0].length; col++){
                 if(grid[row][col] == 1 && !visited[row][col]){
-                    dfs(grid, visited, row, col, islandSet, "O");
+                    StringBuilder coordinates = new StringBuilder();
+                    dfs(grid, visited, row, col, coordinates, "O");
+                    islandSet.add(coordinates.toString());
                 }
             }
         }
-        return 0;
+        return islandSet.size();
+    }
+
+    private static void dfs(int [][] matrix,
+                            boolean [][] isVisited,
+                            int row,
+                            int col,
+                            StringBuilder cordinates,
+                            String direction){
+        if(row<0 || row >= matrix.length || col < 0 || col >= matrix[0].length) return;
+        if(matrix[row][col] == 0 || isVisited[row][col]) return;
+
+        isVisited[row][col] = true;
+        cordinates.append(direction);
+        dfs(matrix, isVisited, row+1, col, cordinates, "D");
+        dfs(matrix, isVisited, row-1, col, cordinates, "U");
+        dfs(matrix, isVisited, row, col+1, cordinates, "R");
+        dfs(matrix, isVisited, row, col-1, cordinates, "L");
+
+        cordinates.append("B");
     }
 }
