5f9ce906021c5b01b5d03e18be7472cb6ea1a729
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-06 21:43:21 -0700

    island cycle tests added

--- src/main/java/dafe/springframework/beerservice/Grokking/IslandCycle.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/IslandCycle.java
@@ -1,4 +1,31 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class IslandCycle {
+
+    public static void main(String[] args) {
+        System.out.println(hasCycle(
+                new char[][] {
+                        { 'a', 'a', 'a', 'a' },
+                        { 'b', 'a', 'c', 'a' },
+                        { 'b', 'a', 'c', 'a' },
+                        { 'b', 'a', 'a', 'a' }
+                }));
+
+        System.out.println(hasCycle(
+                new char[][] {
+                        { 'a', 'a', 'a', 'a' },
+                        { 'a', 'b', 'b', 'a' },
+                        { 'a', 'b', 'a', 'a' },
+                        { 'a', 'a', 'a', 'c' }
+                }));
+
+        System.out.println(hasCycle(
+                new char[][] {
+                        { 'a', 'b', 'e', 'b' },
+                        { 'b', 'b', 'b', 'b' },
+                        { 'b', 'c', 'c', 'd' },
+                        { 'c', 'c', 'd', 'd' }
+                }));
+
+    }
 }
