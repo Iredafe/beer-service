e864dc53d5110eda00fddacadfd9c95b6a2e17f5
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-06 17:56:22 -0700

    distinct island III

--- src/main/java/dafe/springframework/beerservice/Grokking/NumberOfDistinctIsland.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/NumberOfDistinctIsland.java
@@ -1,5 +1,7 @@
 package dafe.springframework.beerservice.Grokking;
 
+import java.util.HashSet;
+
 public class NumberOfDistinctIsland {
     public static void main(String[] args) {
         System.out.println(findDistinctIslandsDFS(
@@ -22,11 +24,12 @@ public class NumberOfDistinctIsland {
     }
 
     private static int findDistinctIslandsDFS(int[][] grid) {
-
+        HashSet<StringBuilder> islandSet = new HashSet<>();
+        boolean [][] visited = new boolean[grid.length][grid[0].length];
         for(int row=0; row< grid.length; row++){
             for(int col=0; col<grid[0].length; col++){
                 if(grid[row][col] == 1 && !visited[row][col]){
-
+                    dfs(grid, visited, row, col, islandSet, "O");
                 }
             }
         }
