ce2aed1dc3fec7d1f2b73269d80c3519ebbb26e3
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-06 17:26:22 -0700

    distinct island I

--- src/main/java/dafe/springframework/beerservice/Grokking/NumberOfDistinctIsland.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/NumberOfDistinctIsland.java
@@ -1,5 +1,29 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class NumberOfDistinctIsland {
+    public static void main(String[] args) {
+        System.out.println(findDistinctIslandsDFS(
+                new int[][] {
+                        { 1, 1, 0, 1, 1 },
+                        { 1, 1, 0, 1, 1 },
+                        { 0, 0, 0, 0, 0 },
+                        { 0, 1, 1, 0, 1 },
+                        { 0, 1, 1, 0, 1 }
+                }));
 
+        System.out.println(findDistinctIslandsDFS(
+                new int[][] {
+                        { 1, 1, 0, 1 },
+                        { 0, 1, 1, 0 },
+                        { 0, 0, 0, 0 },
+                        { 1, 1, 0, 0 },
+                        { 0, 1, 1, 0 }
+                }));
+    }
+
+    private static int findDistinctIslandsDFS(int[][] grid) {
+
+
+        return 0;
+    }
 }
