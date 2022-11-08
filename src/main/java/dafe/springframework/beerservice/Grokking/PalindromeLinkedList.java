d68586f610d7b7c67b1769fda7f5c8e9196238d3
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-07 22:21:38 -0700

    palindrome linkedlist I

--- src/main/java/dafe/springframework/beerservice/Grokking/PalindromeLinkedList.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/PalindromeLinkedList.java
@@ -1,4 +1,16 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class PalindromeLinkedList {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(2);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(6);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(2);
+        System.out.println("Is palindrome: " + isPalindrome(head));
+
+        head.next.next.next.next.next = new ListNode(2);
+        System.out.println("Is palindrome: " + isPalindrome(head));
+    }
 }
