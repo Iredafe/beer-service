0b9c2bbd47650ef84c5d89e48e6e80e25fc544a8
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-07 19:06:04 -0700

    island cycle done

--- src/main/java/dafe/springframework/beerservice/Grokking/IslandCycle.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/IslandCycle.java
@@ -6,12 +6,33 @@ public class IslandCycle {
         boolean [][] visited = new boolean[matrix.length][matrix[0].length];
         for(int row=0; row < matrix.length; row++){
             for(int col =0; col<matrix[0].length; col++){
-                if(visited[row][col]) {
+                if(!visited[row][col]) {
                    return islandContainsCycle(matrix, visited, matrix[row][col],row, col, -1, -1);
                 }
             }
         }
         return false;
+    }
+
+    private static boolean islandContainsCycle(char [][] grid,
+                                               boolean [][] isVisited,
+                                               char startingCharacter,
+                                               int row,
+                                               int col,
+                                               int previousRow,
+                                               int previousCol){
+        if(row<0 || row >= grid.length || col < 0 || col >= grid[0].length) return false;
+        if(isVisited[row][col]) return true;
+        if(grid[row][col] != startingCharacter) return false;
+
+        isVisited[row][col] = true;
+
+        if(row+1 != previousRow && islandContainsCycle(grid, isVisited, grid[row][col],row+1, col, row,col)) return true;
+        if(row-1 != previousRow && islandContainsCycle(grid, isVisited, grid[row][col],row-1, col, row,col)) return true;
+        if(col+1 != previousCol && islandContainsCycle(grid, isVisited, grid[row][col],row, col+1, row,col)) return true;
+        if(col-1 != previousCol && islandContainsCycle(grid, isVisited, grid[row][col],row, col-1, row,col)) return true;
+
+    return false;
     }
 
 
