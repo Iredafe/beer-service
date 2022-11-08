81f095a968f436e4929253ac0d39fe91598d1044
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-07 19:11:24 -0700

    middle of linkedlist I

--- src/main/java/dafe/springframework/beerservice/Grokking/MiddleOfLinkedList.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/MiddleOfLinkedList.java
@@ -1,4 +1,20 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class MiddleOfLinkedList {
+
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        System.out.println("Middle Node: " + findMiddle(head).value);
+
+        head.next.next.next.next.next = new ListNode(6);
+        System.out.println("Middle Node: " + findMiddle(head).value);
+
+        head.next.next.next.next.next.next = new ListNode(7);
+        System.out.println("Middle Node: " + findMiddle(head).value);
+    }
 }
