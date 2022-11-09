a5f9df61975c2fcebf41a69ff92854124b8ddd8a
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-08 23:12:01 -0700

    rearrange linkedlist test setup

--- src/main/java/dafe/springframework/beerservice/Grokking/RearrangeLinkedList.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/RearrangeLinkedList.java
@@ -1,4 +1,18 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class RearrangeLinkedList {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(2);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(6);
+        head.next.next.next = new ListNode(8);
+        head.next.next.next.next = new ListNode(10);
+        head.next.next.next.next.next = new ListNode(12);
+        reorder(head);
+        while (head != null) {
+            System.out.print(head.value + " ");
+            head = head.next;
+        }
+    }
 }
