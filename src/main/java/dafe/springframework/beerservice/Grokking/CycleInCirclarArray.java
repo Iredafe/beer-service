b9bb3a5804e795e072eb04f75b4cf7b85a97c21b
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-09 22:53:10 -0700

    cycle in circular array test setup

--- src/main/java/dafe/springframework/beerservice/Grokking/CycleInCirclarArray.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/CycleInCirclarArray.java
@@ -1,4 +1,10 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class CycleInCirclarArray {
+
+    public static void main(String[] args) {
+        System.out.println(loopExists(new int[] { 1, 2, -1, 2, 2 }));
+        System.out.println(loopExists(new int[] { 2, 2, -1, 2 }));
+        System.out.println(loopExists(new int[] { 2, 1, -1, -2 }));
+    }
 }
