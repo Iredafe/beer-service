ed01f881c4d7d90f9023b0a4bafb7b25c3c3f140
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-11 22:07:21 -0700

    cycle in circular array IV

--- src/main/java/dafe/springframework/beerservice/Grokking/CycleInCirclarArray.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/CycleInCirclarArray.java
@@ -2,12 +2,12 @@ package dafe.springframework.beerservice.Grokking;
 
 public class CycleInCirclarArray {
 
+    public static boolean loopExists(int [] array){
+
+        return true;
+    }
+
     public static void main(String[] args) {
-
-        public static boolean loopExists(){
-
-        }
-
         System.out.println(loopExists(new int[] { 1, 2, -1, 2, 2 }));
         System.out.println(loopExists(new int[] { 2, 2, -1, 2 }));
         System.out.println(loopExists(new int[] { 2, 1, -1, -2 }));
