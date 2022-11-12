7db261494ef1661bf2a4e705621b2084dd42b55f
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-11 22:11:38 -0700

    cycle in circular array V

--- src/main/java/dafe/springframework/beerservice/Grokking/CycleInCirclarArray.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/CycleInCirclarArray.java
@@ -3,7 +3,20 @@ package dafe.springframework.beerservice.Grokking;
 public class CycleInCirclarArray {
 
     public static boolean loopExists(int [] array){
+        for(int i=0; i<array.length; i++){
+            int slow=i, fast=i;
 
+            boolean isForward = array[i] >=0;
+
+            do{
+                slow = findNextIndex(array, isForward, slow);
+                fast = findNextIndex(array, isForward, fast);
+
+                if(fast!=-1){
+                    fast = findNextIndex(array, isForward, fast);
+                }
+            }while ()
+        }
         return true;
     }
 
