231f8ec8a74db03d1c636b8bb54eb622c6f4e7fb
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-13 19:53:40 -0700

    reverse sublist 1

--- src/main/java/dafe/springframework/beerservice/Grokking/ReverseSublist.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/ReverseSublist.java
@@ -3,5 +3,19 @@ package dafe.springframework.beerservice.Grokking;
 public class ReverseSublist {
 
 
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ListNode result = reverse(head, 2, 4);
+        System.out.print("Nodes of the reversed LinkedList are: ");
+        while (result != null) {
+            System.out.print(result.value + " ");
+            result = result.next;
+        }
+    }
 
 }
