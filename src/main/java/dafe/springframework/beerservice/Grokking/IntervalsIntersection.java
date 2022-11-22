285055e3d41099ac81d0c4d4899c4028d43d65ff
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-21 20:52:55 -0700

    intervals intersection II

--- src/main/java/dafe/springframework/beerservice/Grokking/IntervalsIntersection.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/IntervalsIntersection.java
@@ -1,6 +1,6 @@
 package dafe.springframework.beerservice.Grokking;
 
-public class IntervalsIntersection {
+public class {
 
 
 
@@ -8,7 +8,7 @@ public class IntervalsIntersection {
         Interval[] input1 = new Interval[] { new Interval(1, 3), new Interval(5, 6),
                 new Interval(7, 9) };
         Interval[] input2 = new Interval[] { new Interval(2, 3), new Interval(5, 7) };
-        Interval[] result = IntervalsIntersection.merge(input1, input2);
+        Interval[] result = merge(input1, input2);
         System.out.print("Intervals Intersection: ");
         for (Interval interval : result)
             System.out.print("[" + interval.start + "," + interval.end + "] ");
@@ -17,7 +17,7 @@ public class IntervalsIntersection {
         input1 = new Interval[] { new Interval(1, 3), new Interval(5, 7),
                 new Interval(9, 12) };
         input2 = new Interval[] { new Interval(5, 10) };
-        result = IntervalsIntersection.merge(input1, input2);
+        result = merge(input1, input2);
         System.out.print("Intervals Intersection: ");
         for (Interval interval : result)
             System.out.print("[" + interval.start + "," + interval.end + "] ");
