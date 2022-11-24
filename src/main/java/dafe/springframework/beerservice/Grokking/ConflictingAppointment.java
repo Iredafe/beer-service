03a4bc2f19ba7e63c382b2d51c8c90d3ab518eb7
Dafe owolabi <iredafeowolabi@gmail.com>
2022-11-23 20:47:00 -0700

    conflicting appointment II

--- src/main/java/dafe/springframework/beerservice/Grokking/ConflictingAppointment.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/ConflictingAppointment.java
@@ -5,17 +5,17 @@ public class ConflictingAppointment {
     public static void main(String[] args) {
         Interval[] intervals = { new Interval(1, 4), new Interval(2, 5),
                 new Interval(7, 9) };
-        boolean result = ConflictingAppointments.canAttendAllAppointments(intervals);
+        boolean result = canAttendAllAppointments(intervals);
         System.out.println("Can attend all appointments: " + result);
 
         Interval[] intervals1 = { new Interval(6, 7), new Interval(2, 4),
                 new Interval(8, 12) };
-        result = ConflictingAppointments.canAttendAllAppointments(intervals1);
+        result = canAttendAllAppointments(intervals1);
         System.out.println("Can attend all appointments: " + result);
 
         Interval[] intervals2 = { new Interval(4, 5), new Interval(2, 3),
                 new Interval(3, 6) };
-        result = ConflictingAppointments.canAttendAllAppointments(intervals2);
+        result = canAttendAllAppointments(intervals2);
         System.out.println("Can attend all appointments: " + result);
     }
 }
