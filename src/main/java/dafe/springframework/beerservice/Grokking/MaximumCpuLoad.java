7fed3806dc1ab54537c890166c33ea5c158e19cb
Dafe owolabi <iredafeowolabi@gmail.com>
2022-12-02 21:50:50 -0700

    maximum cpu load I

--- src/main/java/dafe/springframework/beerservice/Grokking/MaximumCpuLoad.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/MaximumCpuLoad.java
@@ -1,4 +1,22 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class MaximumCpuLoad {
+
+
+    public static void main(String[] args) {
+        List<Job> input = new ArrayList<Job>(Arrays.asList(new Job(1, 4, 3),
+                new Job(2, 5, 4), new Job(7, 9, 6)));
+        System.out.println("Maximum CPU load at any time: " +
+                findMaxCPULoad(input));
+
+        input = new ArrayList<Job>(Arrays.asList(new Job(6, 7, 10), new Job(2, 4, 11),
+                new Job(8, 12, 15)));
+        System.out.println("Maximum CPU load at any time: " +
+                findMaxCPULoad(input));
+
+        input = new ArrayList<Job>(Arrays.asList(new Job(1, 4, 2), new Job(2, 4, 1),
+                new Job(3, 6, 5)));
+        System.out.println("Maximum CPU load at any time: " +
+                findMaxCPULoad(input));
+    }
 }
