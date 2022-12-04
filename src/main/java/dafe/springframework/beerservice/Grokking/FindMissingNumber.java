24eec35cdab54ebebe8d22cf8a479fbf64a9d12a
Dafe owolabi <iredafeowolabi@gmail.com>
2022-12-03 21:24:15 -0700

    find missing number I

--- src/main/java/dafe/springframework/beerservice/Grokking/FindMissingNumber.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/FindMissingNumber.java
@@ -1,4 +1,15 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class FindMissingNumber {
+
+    private static int[] findMissingNumber(int [] array){
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findMissingNumber(new int[] { 4, 0, 3, 1 }));
+        System.out.println(findMissingNumber(
+                new int[] { 8, 3, 5, 2, 4, 6, 0, 1 }));
+    }
 }
