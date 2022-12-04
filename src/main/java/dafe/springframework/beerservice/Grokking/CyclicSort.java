4c2e25c4f0cff75c8d2655ccffabd8668cbc86c9
Dafe owolabi <iredafeowolabi@gmail.com>
2022-12-03 20:17:14 -0700

    cyclic sort I

--- src/main/java/dafe/springframework/beerservice/Grokking/CyclicSort.java
+++ src/main/java/dafe/springframework/beerservice/Grokking/CyclicSort.java
@@ -1,4 +1,25 @@
 package dafe.springframework.beerservice.Grokking;
 
 public class CyclicSort {
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[] { 3, 1, 5, 4, 2 };
+        sort(arr);
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+
+        arr = new int[] { 2, 6, 4, 3, 1, 5 };
+        sort(arr);
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+
+        arr = new int[] { 1, 5, 6, 4, 3, 2 };
+        sort(arr);
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+    }
 }
